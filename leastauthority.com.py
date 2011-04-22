e573e59c91b6007e1709e3ec54f5be6df92f39c6
nejucomo <nejucomo@gmail.com>
2011-04-21 22:15:27 -0700

    Add config file; require purchase_url config option; redirect /signup -> the purchase url.
    Ignore-this: 9f1bb2469c75779cd5a90b393eddf7a0
    
    darcs-hash:20110422051527-8c0c7-3d39eb6f18a14df7f173aa0fa3ff8a9baf3a6a2e

--- leastauthority.com.py
+++ leastauthority.com.py
@@ -1,8 +1,40 @@
+#! /usr/bin/env python
+
+import os
+import json
+
 from twisted.web.server import Site
 from twisted.web.static import File
+from twisted.web.util import Redirect
 from twisted.internet import reactor
 
-resource = File('content')
-factory = Site(resource)
-reactor.listenTCP(80, factory)
-reactor.run()
+def main():
+    config = Config()
+
+    resource = File('content')
+    resource.putChild('signup', Redirect( config.purchase_url ))
+
+    factory = Site(resource)
+    reactor.listenTCP(80, factory)
+    reactor.run()
+
+
+class Config (object):
+    def __init__(self):
+        d = self._load_config_json()
+        self.purchase_url = str( d.pop('purchase_url') )
+        if d:
+            print 'Warning: Unknown config items: %r' % (d.keys(),)
+
+    @staticmethod
+    def _load_config_json():
+        path = os.path.expanduser('~/lae_website_config.json')
+        f = open(path, 'r')
+        try:
+            return json.load(f)
+        finally:
+            f.close()
+
+
+if __name__ == '__main__':
+    main()
