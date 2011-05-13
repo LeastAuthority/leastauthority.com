44648093f40b682bcec4d111697a904f4b433fe6
nejucomo <nejucomo@gmail.com>
2011-05-12 21:20:26 -0700

    Test loading config from a temp file to get 100% branch coverage.
    Ignore-this: 739d54f5ec9c02a1f2c9ecfa5cb02a86
    
    darcs-hash:20110513042026-8c0c7-50f1334e5f33e241f731a4ce0dc001fe692a9ebb

--- lae_site/test/test_config.py
+++ lae_site/test/test_config.py
@@ -10,10 +10,8 @@ class ConfigTests (TestCase):
     def test_unexceptional_load(self):
         config = self._load_from_string( VALID_CONFIG )
 
-        self.assertEqual( VALID_PURCHASE_URL, config.devpay_purchase_url )
-        self.assertEqual( VALID_ACCESS_KEY, config.aws_creds.access_key )
-        self.assertEqual( VALID_SECRET_KEY, config.aws_creds.secret_key )
-        
+        self._assert_valid_config(config)
+
 
     def test_missing_required_key(self):
         for vector in [INVALID_EMPTY_CONFIG, INVALID_WRONG_KEY_CONFIG]:
@@ -29,13 +27,30 @@ class ConfigTests (TestCase):
                     pprint.pformat(config),
                     )
 
+
     def test_unknown_options(self):
         config = self._load_from_string( UNKNOWN_OPTION_CONFIG )
 
         expected = { UNKNOWN_OPTION_NAME: UNKNOWN_OPTION_VALUE }
 
         self.assertEqual(expected, config.unknown_options)
-    
+
+
+    def test_load_file(self):
+        temp = self.mktemp()
+
+        with file(temp, 'w') as f:
+            f.write(VALID_CONFIG)
+
+        self._assert_valid_config(Config(temp))
+
+
+    def _assert_valid_config(self, config):
+        self.assertEqual( VALID_PURCHASE_URL, config.devpay_purchase_url )
+        self.assertEqual( VALID_ACCESS_KEY, config.aws_creds.access_key )
+        self.assertEqual( VALID_SECRET_KEY, config.aws_creds.secret_key )
+
+
     @staticmethod
     def _load_from_string(text):
         return Config( StringIO(text) )
