b34c51b95d23f29dfa78e0fed0bf7994e8485b18
david-sarah <david-sarah@jacaranda.org>
2011-10-29 18:46:28 -0700

    Make the web server support SSL/TLS.
    Ignore-this: d0b6e358a63908ae1f8951a083f4fbc6
    
    darcs-hash:20111030014628-93fa1-ab8ce68658587bec3e320b52fc0e03416bb6b131

--- lae_site/main.py
+++ lae_site/main.py
@@ -1,19 +1,27 @@
 #! /usr/bin/env python
 
-import sys
+import sys, os
 import logging
 
-from twisted.internet import reactor
+from twisted.internet import ssl, reactor
 
 from lae_site.config import Config
 from lae_site.handlers import make_site
 
 # Test comment by sirvaliance, remove me
 def main():
-    port = 80
+    default_port = 443
+    port = None
+    ssl_enabled = True
     for arg in sys.argv:
         if arg.startswith('--port='):
             port = int(arg[len('--port='):])
+        if arg == '--nossl':
+            ssl_enabled = False
+            default_port = 80
+
+    if port is None:
+        port = default_port
 
     config = Config()
 
@@ -30,7 +38,19 @@ def main():
     site = make_site(config)
 
     logging.info('Listening on port %d...' % port)
-    reactor.listenTCP(port, site)
+    if ssl_enabled:
+        logging.info('SSL/TLS is enabled (start with --nossl to disable).')
+        KEYFILE = 'keys/server.key'
+        CERTFILE = 'keys/server.crt'
+        assert os.path.exists(KEYFILE), "Private key file %s not found" % (KEYFILE,)
+        assert os.path.exists(CERTFILE), "Certificate file %s not found" % (CERTFILE,)
+
+        # http://twistedmatrix.com/documents/current/core/howto/ssl.html
+        sslcontext = ssl.DefaultOpenSSLContextFactory(KEYFILE, CERTFILE).getContext()
+        reactor.listenSSL(port, site, sslcontext)
+    else:
+        logging.info('SSL/TLS is disabled.')
+        reactor.listenTCP(port, site)
     reactor.run()
 
 
