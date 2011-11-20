7443575a01e713d4ff8b1a5f033e22a3a39f8da4
david-sarah <david-sarah@jacaranda.org>
2011-11-19 22:46:10 -0800

    Make a start on the full signup script.
    Ignore-this: af80b1bad0a7b87561358c7add5f26c6
    
    darcs-hash:20111120064610-93fa1-27b0fa5722829d35b62c0e6b77ad72ddbde1cc65

---
+++ full_signup.py
@@ -0,0 +1,34 @@
+import sys
+from twisted.internet import defer, reactor
+from twisted.python.failure import Failure
+from txaws.service import AWSCredentials
+
+from lae_site.config import Config
+
+config = Config()
+
+
+if len(sys.argv) < 4:
+    print "Usage: python signup.py ACTIVATION_KEY PRODUCT_CODE CUSTOMER_EMAIL"
+    sys.exit(1)
+
+def signup(activation_key, product_code, customer_email):
+    ps = [p for p in config.products if p['product_code'] == product_code]
+    if len(ps) != 1:
+        raise AssertionError("Product code %r matches %d products." % (product_code, len(ps)))
+
+    product = ps[0]
+    full_name = ps['full_name']
+    product_token = ps['product_token']
+    ami_image_id = ps['ami_image_id']
+    instance_size = ps['instance_size']
+
+def cb(x):
+    print str(x)
+    if isinstance(x, Failure) and hasattr(x.value, 'response'):
+        print x.value.response
+
+d = defer.succeed(None)
+d.addBoth(cb)
+d.addBoth(lambda ign: reactor.stop())
+reactor.run()
