5fd0547896dd76343aba10a05e8b14d307f4c228
david-sarah <david-sarah@jacaranda.org>
2011-11-28 12:37:07 -0800

    Add missing lae_automation/config.py.
    Ignore-this: 4a6424e791821f1686b9d8272ebf658e
    
    darcs-hash:20111128203707-93fa1-16d53af9af10a91ee4628e6f930cd79f811ef659

---
+++ lae_automation/config.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import os, json
+
+
+class Config(object):
+    __slots__ = ['products', 'other']
+
+    DEFAULT_CONFIG_PATH = os.path.expanduser('../lae_automation_config.json')
+
+    def __init__(self, configFile = DEFAULT_CONFIG_PATH):
+        """
+        Given a path string or file-like object load a configuration.
+
+        The configuration is available as attributes.
+        """
+        config = self._load_config_json(configFile)
+
+        assert isinstance(config, dict)
+        assert "products" in config, config
+        self.products = config.pop("products")
+        assert isinstance(self.products, list)
+        for value in self.products:
+            assert isinstance(value, dict), value
+            for field in ("full_name", "product_code", "product_token", "ami_image_id", "instance_size"):
+                assert field in value, value
+                value[field] = str(value[field])
+                assert isinstance(value[field], str), value
+
+        self.other = config
+
+    @staticmethod
+    def _load_config_json(configFile):
+        if type(configFile) is str:
+            configFile = open(configFile, 'r')
+
+        try:
+            return json.load( configFile )
+        finally:
+            configFile.close()
