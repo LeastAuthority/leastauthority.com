297545dffa2fcc9843ed87cba650d7fe7f9f1912
david-sarah <david-sarah@jacaranda.org>
2011-12-13 22:33:43 -0800

    test_confirmation.py: cover the PGP case.
    Ignore-this: 525f37c9c8290aaaea7424605ef47c33
    
    darcs-hash:20111214063343-93fa1-5a8ceff59332153844b58567cfb1339bab2d48a1

--- lae_automation/test/test_confirmation.py
+++ lae_automation/test/test_confirmation.py
@@ -21,47 +21,75 @@ class TestConfirmation(unittest.TestCase):
     FROM_EMAIL = confirmation.FROM_EMAIL
     CUSTOMER_NAME = 'Fred Bloggs'
     CUSTOMER_EMAIL = 'fbloggs@example.net'
+    PGP_NOTIFICATION_EMAIL = confirmation.PGP_NOTIFICATION_EMAIL
     EXTERNAL_INTRODUCER_FURL = 'pb://foo@bar/baz'
-    CUSTOMER_KEYINFO = ''
 
     def setUp(self):
         FilePath('smtppassword').setContent(self.SMTP_PASSWORD)
 
-    def test_send_signup_confirmation_success(self):
-        the_factory = Mock()
-        def call_ESMTPSenderFactory(username, password, fromEmail, toEmail, f, d):
-            self.failUnlessEqual(username, self.SMTP_USERNAME)
-            self.failUnlessEqual(password, self.SMTP_PASSWORD)
-            self.failUnlessEqual(fromEmail, self.FROM_EMAIL)
-            self.failUnlessEqual(toEmail, self.CUSTOMER_EMAIL)
-            f.seek(0, 0)
-            # assume f can be read in one call
-            message = f.read()
-            assert f.read() == ''
+    def _call_ESMTPSenderFactory_non_PGP(self, username, password, fromEmail, toEmail, f, d):
+        self.failUnlessEqual(username, self.SMTP_USERNAME)
+        self.failUnlessEqual(password, self.SMTP_PASSWORD)
+        self.failUnlessEqual(fromEmail, self.FROM_EMAIL)
+        self.failUnlessEqual(toEmail, self.CUSTOMER_EMAIL)
+        f.seek(0, 0)
+        # assume f can be read in one call
+        message = f.read()
+        assert f.read() == ''
 
-            # although MIME specifies CRLF line endings, it is just LF at this point
-            (headers, sep, body) = message.partition('\n\n')
-            self.failUnlessEqual(sep, '\n\n')
-            self.failUnlessIn('Message-ID: ', headers)
-            self.failUnlessIn('Date: ', headers)
-            self.failUnlessIn('Subject: ', headers)
-            self.failUnlessIn('From: ', headers)
-            self.failUnlessIn('To: ', headers)
-            # FIXME: test for UTF-8
-            self.failUnlessIn('Content-Type: text/plain', headers)
-            self.failUnlessIn(self.CUSTOMER_NAME, body)
-            self.failUnlessIn('https://leastauthority.com/howtoconfigure', body)
-            self.failUnlessIn(self.EXTERNAL_INTRODUCER_FURL, body)
+        # although MIME specifies CRLF line endings, it is just LF at this point
+        (headers, sep, body) = message.partition('\n\n')
+        self.failUnlessEqual(sep, '\n\n')
+        self.failUnlessIn('Message-ID: ', headers)
+        self.failUnlessIn('Date: ', headers)
+        self.failUnlessIn('Subject: ', headers)
+        self.failUnlessIn('From: ', headers)
+        self.failUnlessIn('To: ', headers)
+        # FIXME: test for UTF-8
+        self.failUnlessIn('Content-Type: text/plain', headers)
+        self.failUnlessIn(self.CUSTOMER_NAME, body)
+        self.failUnlessIn('https://leastauthority.com/howtoconfigure', body)
+        self.failUnlessIn(self.EXTERNAL_INTRODUCER_FURL, body)
 
-            eventually(d.callback, None)
-            return the_factory
-        self.patch(confirmation, 'ESMTPSenderFactory', call_ESMTPSenderFactory)
+        eventually(d.callback, None)
+        return self.the_factory
+
+    def _call_ESMTPSenderFactory_PGP(self, username, password, fromEmail, toEmail, f, d):
+        self.failUnlessEqual(username, self.SMTP_USERNAME)
+        self.failUnlessEqual(password, self.SMTP_PASSWORD)
+        self.failUnlessEqual(fromEmail, self.FROM_EMAIL)
+        self.failUnlessEqual(toEmail, self.PGP_NOTIFICATION_EMAIL)
+        f.seek(0, 0)
+        # assume f can be read in one call
+        message = f.read()
+        assert f.read() == ''
+
+        # although MIME specifies CRLF line endings, it is just LF at this point
+        (headers, sep, body) = message.partition('\n\n')
+        self.failUnlessEqual(sep, '\n\n')
+        self.failUnlessIn('Message-ID: ', headers)
+        self.failUnlessIn('Date: ', headers)
+        self.failUnlessIn('Subject: ', headers)
+        self.failUnlessIn('From: ', headers)
+        self.failUnlessIn('To: ', headers)
+        # FIXME: test for UTF-8
+        self.failUnlessIn('Content-Type: text/plain', headers)
+        self.failUnlessIn(self.CUSTOMER_NAME, body)
+        self.failUnlessIn(self.CUSTOMER_EMAIL, body)
+        self.failIfIn(self.EXTERNAL_INTRODUCER_FURL, body)
+
+        eventually(d.callback, None)
+        return self.the_factory
+
+    def _test_send_signup_confirmation_success(self, call_factory, customer_keyinfo):
+        self.the_factory = Mock()
+        self.patch(confirmation, 'ESMTPSenderFactory', call_factory)
 
         connected = {}
         def call_connectTCP(smtphost, port, factory):
             self.failUnlessEqual(smtphost, self.SMTP_HOST)
             self.failUnlessEqual(port, self.SMTP_PORT)
-            self.failUnlessEqual(factory, the_factory)
+            self.failUnlessEqual(factory, self.the_factory)
             self.failUnlessEqual(factory.domain, self.SENDER_DOMAIN)
             connected['flag'] = True
         self.patch(confirmation, 'connectTCP', call_connectTCP)
@@ -69,15 +97,21 @@ class TestConfirmation(unittest.TestCase):
         stdout = StringIO()
         stderr = StringIO()
         d = send_signup_confirmation(self.CUSTOMER_NAME, self.CUSTOMER_EMAIL, self.EXTERNAL_INTRODUCER_FURL,
-                                     self.CUSTOMER_KEYINFO, stdout, stderr, password_path='smtppassword')
+                                     customer_keyinfo, stdout, stderr, password_path='smtppassword')
         def _check(ign):
             self.failUnless('flag' in connected)
             out = stdout.getvalue()
-            self.failUnlessIn("Sending confirmation e-mail", out)
+            self.failUnlessIn("confirmation e-mail", out)
             self.failUnlessIn(self.CUSTOMER_EMAIL, out)
-            self.failUnlessIn("Confirmation e-mail sent.", out)
+            self.failUnlessIn("sent.", out)
         d.addCallback(_check)
         return d
+
+    def test_send_signup_confirmation_success_non_PGP(self):
+        return self._test_send_signup_confirmation_success(self._call_ESMTPSenderFactory_non_PGP, '')
+
+    def test_send_signup_confirmation_success_PGP(self):
+        return self._test_send_signup_confirmation_success(self._call_ESMTPSenderFactory_PGP, '1234 ... ABCD')
 
     def test_send_signup_confirmation_factory_exception(self):
         stdout = StringIO()
@@ -88,7 +122,7 @@ class TestConfirmation(unittest.TestCase):
         self.patch(confirmation, 'ESMTPSenderFactory', call_ESMTPSenderFactory)
 
         d = send_signup_confirmation(self.CUSTOMER_NAME, self.CUSTOMER_EMAIL, self.EXTERNAL_INTRODUCER_FURL,
-                                     self.CUSTOMER_KEYINFO, stdout, stderr, password_path='smtppassword')
+                                     '', stdout, stderr, password_path='smtppassword')
         def _bad_success(ign):
             self.fail("should have got a failure")
         def _check_failure(f):
@@ -112,7 +146,7 @@ class TestConfirmation(unittest.TestCase):
         self.patch(confirmation, 'connectTCP', call_connectTCP)
 
         d = send_signup_confirmation(self.CUSTOMER_NAME, self.CUSTOMER_EMAIL, self.EXTERNAL_INTRODUCER_FURL,
-                                     self.CUSTOMER_KEYINFO, stdout, stderr, password_path='smtppassword')
+                                     '', stdout, stderr, password_path='smtppassword')
         def _bad_success(ign):
             self.fail("should have got a failure")
         def _check_failure(f):
@@ -136,7 +170,7 @@ class TestConfirmation(unittest.TestCase):
         self.patch(confirmation, 'connectTCP', call_connectTCP)
 
         d = send_signup_confirmation(self.CUSTOMER_NAME, self.CUSTOMER_EMAIL, self.EXTERNAL_INTRODUCER_FURL,
-                                     self.CUSTOMER_KEYINFO, stdout, stderr, password_path='smtppassword')
+                                     '', stdout, stderr, password_path='smtppassword')
         def _bad_success(ign):
             self.fail("should have got a failure")
         def _check_failure(f):
