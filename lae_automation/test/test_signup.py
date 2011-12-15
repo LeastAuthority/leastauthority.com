59fd8062c6246e975d0071b0697f816f3b43372b
wilcoxjg <wilcoxjg@gmail.com>
2011-12-15 00:27:09 -0800

    test_signup.py:  85% covered with tahoe-esque style
    Ignore-this: 57ba035a7c5825566ee18dc5b7b44064
    
    darcs-hash:20111215082709-8f86b-c7c07c27f6dcd5398fa8a020d7b57634ec0e7b6e

--- lae_automation/test/test_signup.py
+++ lae_automation/test/test_signup.py
@@ -6,10 +6,6 @@ from twisted.internet import defer
 import mock, sys
 from lae_automation.signup import signup
 
-
-#to be mocked out
-#from lae_automation.signup import install_server, bounce_server, send_signup_confirmation
-
 # Vector data for request responses: activate desktop-, verify-, and describeEC2- responses.
 from lae_automation.test.testinitvector import adphttprequestheader, adprequestresponse, verifyhttprequestheader, verifyrequestresponse, describeEC2instresponse
 
@@ -25,12 +21,6 @@ class TestSignupModule(TestCase):
             patcher = mock.patch(name)
             self._patchers.append(patcher)
             return patcher.__enter__()
-
-        #self.mockdescribe_instances = start_patch('lae_automation.initialize.EC2Client.describe_instances')
-        #self.mockdescribe_instances.return_value = defer.succeed( ('0.0.0.0', '0.0.0.0') )
-        self.mockinstall_server = start_patch('lae_automation.signup.install_server')
-        self.mockbounce_server = start_patch('lae_automation.signup.bounce_server')
-        self.mocksend_confirmation = start_patch('lae_automation.signup.send_signup_confirmation')
 
     def tearDown(self):
         [p.__exit__() for p in self._patchers]
@@ -59,6 +49,7 @@ class TestSignupModule(TestCase):
             self.failUnlessEqual(header_dict['Content-MD5'], '1B2M2Y8AsgTpgAmY7PhCfg==')
             return defer.succeed('Completed devpay bucket creation submission.')
         self.patch(Query, 'submit', call_query_submit)
+
         from lae_automation.initialize import EC2Client
         def call_run_instances(EC2Client,
                                ami_image_id,
@@ -77,8 +68,47 @@ class TestSignupModule(TestCase):
                     self.instance_id = 'MOCKEC2INSTANCEID'
             return defer.succeed([MOCKEC2()])
         self.patch(EC2Client, 'run_instances', call_run_instances)
+        def call_describe_instances(EC2Client, instance_id):
+            self.failUnlessEqual(instance_id, 'MOCKEC2INSTANCEID')
+            return defer.succeed( ('0.0.0.0', '0.0.0.0') )
+        self.patch(EC2Client, 'describe_instances', call_describe_instances)
 
-        
+
+        from lae_automation import signup as signup_servercontainer
+        def call_install_server(public_host, key_filename, stdout, stderr):
+            self.failUnlessEqual(public_host, '0.0.0.0')
+            self.failUnlessEqual(key_filename, 'EC2MOCKKEYFILENAME.pem')
+        self.patch(signup_servercontainer, 'install_server', call_install_server)        
+        def call_bounce_server(public_host,
+                               key_filename,
+                               private_host,
+                               creds,
+                               user_token,
+                               product_token,
+                               bucket_name,
+                               stdout,
+                               stderr,
+                               secretsfile):
+            self.failUnlessEqual(public_host, '0.0.0.0')
+            self.failUnlessEqual(key_filename, 'EC2MOCKKEYFILENAME.pem')
+            self.failUnlessEqual(private_host, '0.0.0.0')
+            self.failUnlessEqual(user_token, '{UserToken}TESTUSERTOKEN%s=='%('A'*385,))
+            self.failUnlessEqual(product_token, '{ProductToken}TESTPRODUCTTOKEN%s='%('A'*295,))
+            self.failUnlessEqual(bucket_name, 'lae-abcdefgh-MSEED')
+            self.failUnlessEqual(secretsfile, 'MSECRETSFILE')
+            self.failUnlessEqual(creds.access_key, 'TESTAAAAAAAAAAAAAAAA')
+            self.failUnlessEqual(creds.secret_key, 'TESTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+        self.patch(signup_servercontainer, 'bounce_server', call_bounce_server)
+
+        def call_send_signup_confirmation(customer_name, customer_email, furl, customer_keyinfo, stdout, stderr):
+            self.failUnlessEqual(customer_name, 'MNAME')
+            self.failUnlessEqual(customer_email, 'MEMAIL')
+            self.failUnlessEqual(furl, None)
+            self.failUnlessEqual(customer_keyinfo, 'MKEYINFO')
+            return defer.succeed("Tested send confirmation email call!")
+
+        self.patch(signup_servercontainer, 'send_signup_confirmation', call_send_signup_confirmation)
+
         # Arguments to signup
         MACTIVATIONKEY = 'MOCKACTIVATONKEY'
         MPRODUCTCODE = 'ABCDEFGH'
