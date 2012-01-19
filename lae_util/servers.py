ebe5e1e852a7b00dd95c9f637d1a94b80f1d706c
david-sarah <david-sarah@jacaranda.org>
2012-01-18 21:06:42 -0800

    Correction to a change in lae_util/servers.py.
    Ignore-this: 2ab29fc9388a7e656325251cc81718a4
    
    darcs-hash:20120119050642-93fa1-bf9e4cfa06f00e7c1ab2348e858030d7c3432014

--- lae_util/servers.py
+++ lae_util/servers.py
@@ -5,6 +5,6 @@ from lae_util.timestamp import format_iso_time
 def append_record(filename, *args):
     f = open(filename, "a")
     try:
-        f.write(",".join((format_iso_time(time.time()),) + map(str, args)) + "\n")
+        f.write(",".join([format_iso_time(time.time()),] + map(str, args)) + "\n")
     finally:
         f.close()
