6ee0052653c0f697f3600bd4464f82654254b662
david-sarah <david-sarah@jacaranda.org>
2012-02-23 23:11:41 -0800

    Revert to sending monitoring reports to <info@leastauthority.com> for now.
    Ignore-this: 6b328b33cdc86bf466b70a5f42f13826
    
    darcs-hash:20120224071141-93fa1-717e2ba2c26fdaaa48ca33d6cd56e81fdd17a874

--- lae_automation/monitor.py
+++ lae_automation/monitor.py
@@ -125,15 +125,18 @@ multiservercheck.py
 SENDER_DOMAIN = "leastauthority.com"
 FROM_EMAIL = "info@leastauthority.com"
 FROM_ADDRESS = "Monitoring <%s>" % (FROM_EMAIL,)
-TO_EMAIL = "monitoring@leastauthority.com"
+TO_EMAIL = "info@leastauthority.com"
+TO_EMAIL2 = "monitoring@leastauthority.com"
 USER_AGENT = "Least Authority Enterprises e-mail sender"
 
-SMTP_HOST = "127.0.0.1"
+SMTP_HOST = "smtp.googlemail.com"
+SMTP_HOST2 = "127.0.0.1"
 SMTP_PORT = 25
 SMTP_USERNAME = FROM_EMAIL
+SMTP_USERNAME2 = ''
 
 
-def send_monitoring_report(errors, password_path='../monitoring_smtppassword'):
+def send_monitoring_report(errors, password_path='../smtppassword'):
     password = FilePath(password_path).getContent().strip()
 
     if errors:
@@ -147,5 +150,8 @@ def send_monitoring_report(errors, password_path='../monitoring_smtppassword'):
                "Content-Type": 'text/plain; charset="utf-8"',
               }
 
-    return send_plain_email(SMTP_HOST, SMTP_USERNAME, password, FROM_EMAIL, TO_EMAIL,
-                            content, headers, SENDER_DOMAIN, SMTP_PORT)
+    d = send_plain_email(SMTP_HOST, SMTP_USERNAME, password, FROM_EMAIL, TO_EMAIL,
+                         content, headers, SENDER_DOMAIN, SMTP_PORT)
+    #d.addBoth(lambda ign: send_plain_email(SMTP_HOST2, SMTP_USERNAME2, '', FROM_EMAIL, TO_EMAIL2,
+    #                                       content, headers, SENDER_DOMAIN, SMTP_PORT))
+    return d
