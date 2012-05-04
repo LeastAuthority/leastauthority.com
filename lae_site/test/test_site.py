264dd8dab9172ef03e4cc01fc6c38e155d94f84a
david-sarah <david-sarah@jacaranda.org>
2012-05-03 20:40:00 -0700

    Add a test of make_site.
    Ignore-this: 6a6b105610034ec5316169f014fb0157
    
    darcs-hash:20120504034000-93fa1-297dc649667c0077c1ce5a08e43f2c7881acb3f7

---
+++ lae_site/test/test_site.py
@@ -0,0 +1,43 @@
+
+import string
+from cStringIO import StringIO
+
+import mock
+
+from twisted.trial.unittest import TestCase
+from twisted.web.http import OK, NOT_FOUND
+from twisted.web.server import unquote
+
+from lae_site.config import Config
+from lae_site.handlers import make_site
+
+
+SITE_CONFIG_JSON = """{ "products": [] }"""
+
+class Site(TestCase):
+    def test_site(self):
+        config = Config(StringIO(SITE_CONFIG_JSON))
+        site = make_site(config)
+
+        (req, resp) = self._mock_request(site, '/', 'GET')
+        req.setResponseCode.assert_called_with(OK)
+        self.failUnlessIn("<em>provider-independent security</em>", resp)
+
+        (req, resp) = self._mock_request(site, '/support', 'GET')
+        req.setResponseCode.assert_called_with(OK)
+        self.failUnlessIn("<h3>Product Support</h3>", resp)
+
+        (req, resp) = self._mock_request(site, '/noexist', 'GET')
+        req.setResponseCode.assert_called_with(NOT_FOUND)
+
+    def _mock_request(self, site, path, method, **args):
+        req = mock.Mock(name='HTTPRequest')
+        req.path = path
+        req.prepath = []
+        req.postpath = map(unquote, string.split(path[1:], '/'))
+        req.method = method
+        req.args = args
+
+        resource = site.getResourceFor(req)
+        resp = resource.render(req)
+        return (req, resp)
