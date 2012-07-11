7655b32ae8f7652f5b3b3773f953bbbda15eba03
david-sarah <david-sarah@jacaranda.org>
2012-07-11 15:53:54 -0700

    Clean bitrot from setup_server.py.
    Ignore-this: 1d468a7be3099c677845dbd9f92625a1
    
    darcs-hash:20120711225354-93fa1-1c49bcb352df02f254901bb9afead34c04aae3a4

--- setup_server.py
+++ setup_server.py
@@ -2,25 +2,36 @@
 
 import sys
 
+from twisted.python.filepath import FilePath
+
 from lae_automation.server import install_server, bounce_server
 
 
 if len(sys.argv) < 9:
-    print "Usage: python setup_server.py PUBLIC_IP KEY_FILE PRIVATE_IP USER_ACCESS_KEY_ID USER_SECRET_KEY USER_TOKEN LONG_PRODUCT_TOKEN BUCKET_NAME [--no-install]"
+    print "Usage: python setup_server.py PUBLIC_IP PRIVATE_IP USER_ACCESS_KEY_ID USER_SECRET_KEY USER_TOKEN LONG_PRODUCT_TOKEN BUCKET_NAME SECRETS_PATH [--no-install]"
     print "Happy set-upping!"
     sys.exit(1)
 
 publicip = sys.argv[1]
-ec2keyfilename = sys.argv[2]
-privateip = sys.argv[3]
-useraccesskeyid = sys.argv[4]
-usersecretkey = sys.argv[5]
-usertoken = sys.argv[6]
-producttoken = sys.argv[7]
-bucketname = sys.argv[8]
+privateip = sys.argv[2]
+useraccesskeyid = sys.argv[3]
+usersecretkey = sys.argv[4]
+usertoken = sys.argv[5]
+producttoken = sys.argv[6]
+bucketname = sys.argv[7]
+secretspath = sys.argv[8]
 
-if "--no-install" not in sys.argv:
-    install_server(publicip, ec2keyfilename, sys.stdout, sys.stderr)
+ec2_privkey_path = '../EC2adminkeys2.pem'
+monitor_pubkey = FilePath('../EC2monitorkeys2.pub').getContent().strip()
+monitor_privkey_path = '../EC2monitorkeys2.pem'
 
-bounce_server(publicip, ec2keyfilename, privateip, useraccesskeyid, usersecretkey, usertoken, producttoken, bucketname,
-              sys.stdout, sys.stderr, sys.stderr)
+try:
+    secretsfile = open(secretspath, 'a')
+
+    if "--no-install" not in sys.argv:
+        install_server(publicip, ec2_privkey_path, monitor_pubkey, monitor_privkey_path, sys.stdout, sys.stderr)
+
+    bounce_server(publicip, ec2_privkey_path, privateip, useraccesskeyid, usersecretkey, usertoken, producttoken, bucketname,
+                  sys.stdout, sys.stderr, secretsfile)
+finally:
+    secretsfile.close()
