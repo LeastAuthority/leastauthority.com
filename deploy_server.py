e07d72b1e1adefca20a211e50838169dcbe0e04b
david-sarah <david-sarah@jacaranda.org>
2012-07-11 14:46:33 -0700

    Add deploy_server.py.
    Ignore-this: 643d22975737686a949eddd11e52c927
    
    darcs-hash:20120711214633-93fa1-939a5e745c1b77386ba84490a790016fe4c2f8c8

---
+++ deploy_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import os, sys
+
+from twisted.internet import reactor
+from twisted.python.failure import Failure
+
+from lae_automation.config import Config
+from lae_automation.signup import deploy_server
+
+
+if len(sys.argv) < 13:
+    print "Usage: python deploy_server.py ACCESS_KEY_ID SECRET_KEY USER_TOKEN PRODUCT_TOKEN BUCKET_NAME AMI_IMAGE_ID INSTANCE_SIZE CUSTOMER_NAME CUSTOMER_EMAIL CUSTOMER_KEYINFO SECRETS_FILE CONFIG_PATH"
+    print "Happy server-deploying!"
+    sys.exit(1)
+
+useraccesskeyid = sys.argv[1]
+usersecretkey = sys.argv[2]
+usertoken = sys.argv[3]
+producttoken = sys.argv[4]
+bucketname = sys.argv[5]
+amiimageid = sys.argv[6]
+instancesize = sys.argv[7]
+customer_name = sys.argv[8]
+customer_email = sys.argv[9]
+customer_keyinfo = sys.argv[10]
+secretsfile = sys.argv[11]
+configpath = sys.argv[12]
+
+EC2_ENDPOINT = 'https://ec2.us-east-1.amazonaws.com/'
+#EC2_ENDPOINT = 'https://ec2.amazonaws.com/'
+
+def cb(x):
+    print str(x)
+    if isinstance(x, Failure) and hasattr(x.value, 'response'):
+        print x.value.response
+
+config = Config(configpath)
+d = deploy_server(useraccesskeyid, usersecretkey, usertoken, producttoken,
+                  bucketname, None, amiimageid, instancesize,
+                  customer_name, customer_email, customer_keyinfo, sys.stdout, sys.stderr,
+                  secretsfile, config)
+d.addBoth(cb)
+d.addCallbacks(lambda ign: os._exit(0), lambda ign: os._exit(1))
+reactor.run()
