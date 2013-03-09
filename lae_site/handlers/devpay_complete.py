5162a4cc46776a2c2fd527327d1968e50f993142
zancas <zancas@leastauthority.com>
2013-03-08 21:15:30 -0800

    Two additional changes to make the server launch with the new secrets path.

--- lae_site/handlers/devpay_complete.py
+++ lae_site/handlers/devpay_complete.py
@@ -338,7 +338,6 @@ successful_activationkeys = None
 def start(basefp):
     global flappcommand, all_activationkeys, successful_activationkeys
     
-
     signup_furl_fp = basefp.child('secret_config').child(SIGNUP_FURL_FILE)
     activation_requests_fp = basefp.child(ACTIVATION_REQUESTS_FILE)
     signups_fp = basefp.child(SIGNUPS_FILE)
