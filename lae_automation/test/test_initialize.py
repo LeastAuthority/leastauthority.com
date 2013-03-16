4f16c46ad6fcf3d7811e710c03a2edf465b21f22
David-Sarah Hopwood <david-sarah@jacaranda.org>
2013-03-16 02:37:42 +0000

    Improve test_initialize.py.
    Signed-off-by: David-Sarah Hopwood <david-sarah@jacaranda.org>

--- lae_automation/test/test_initialize.py
+++ lae_automation/test/test_initialize.py
@@ -5,34 +5,38 @@ from twisted.internet import defer
 
 import mock
 
+import lae_automation.initialize
 from lae_automation.initialize import activate_user_account_desktop
 
 
+ACTIVATIONKEY= 'MOCKACTIVATONKEY'
+PRODUCTTOKEN = 'TESTPRODUCTTOKEN'+'A'*295
+USERTOKEN = 'TESTUSERTOKEN'+'A'*385
+
+
 class InitializationTests (TestCase):
+    def _patch_mock(self, obj, attribute):
+        m = mock.Mock()
+        self.patch(obj, attribute, m)
+        return m
 
     def setUp(self):
-        self._patchers = []
-        def start_patch(name):
-            patcher = mock.patch(name)
-            self._patchers.append(patcher)
-            return patcher.__enter__()
+        self.mocklsc = self._patch_mock(lae_automation.initialize, 'LicenseServiceClient')
 
-        self.mocklsc = start_patch('lae_automation.initialize.LicenseServiceClient')
-
-    def tearDown(self):
-        [p.__exit__() for p in self._patchers]
 
     def test_activate_user_account_desktop(self):
-        def make_deferred_fire_factory(value):
-            return defer.succeed(value)
+        def call_adpr(activationkey, producttoken):
+            mockadpr = mock.Mock(name='ActivateDesktopProductResponse')
+            mockadpr.usertoken = USERTOKEN
+            return defer.succeed(mockadpr)
 
-        mockadpr = mock.Mock(name='ActivateDesktopProductResponse')
-        mockadpr.usertoken = "{UserToken}..."
+        adprcallmock = self.mocklsc.return_value.activate_desktop_product
+        adprcallmock.side_effect = call_adpr
 
-        self.mocklsc.return_value.activate_desktop_product.return_value = make_deferred_fire_factory(mockadpr)
-
-        return activate_user_account_desktop(
-            activationkey = mock.sentinel.activationkey,
-            producttoken = mock.sentinel.producttoken,
+        activate_returnval = activate_user_account_desktop(ACTIVATIONKEY,
+            PRODUCTTOKEN,
             stdout = StringIO(),
             stderr = StringIO())
+
+        adprcallmock.assert_called_once_with(ACTIVATIONKEY, PRODUCTTOKEN)
+        return activate_returnval
