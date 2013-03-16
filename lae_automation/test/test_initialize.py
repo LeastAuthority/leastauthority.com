4b5560cdf9802294e23f13f1020dfd0f1881a7a5
David-Sarah Hopwood <david-sarah@jacaranda.org>
2013-03-16 02:38:51 +0000

    Test to show that non-matching fingerprints cause failure.
    Author: Zancas <zancas@leastauthority.com>
    Signed-off-by: David-Sarah Hopwood <david-sarah@jacaranda.org>

--- lae_automation/test/test_initialize.py
+++ lae_automation/test/test_initialize.py
@@ -1,12 +1,16 @@
+
+import sys
 from cStringIO import StringIO
 
 from twisted.trial.unittest import TestCase
 from twisted.internet import defer
+from twisted.python.failure import Failure
 
 import mock
 
 import lae_automation.initialize
-from lae_automation.initialize import activate_user_account_desktop
+from lae_automation.initialize import activate_user_account_desktop, \
+    verify_and_store_serverssh_pubkey, PublicKeyMismatch
 
 
 ACTIVATIONKEY= 'MOCKACTIVATONKEY'
@@ -21,6 +25,9 @@ class InitializationTests (TestCase):
         return m
 
     def setUp(self):
+        self.mockwaitfor_console = self._patch_mock(lae_automation.initialize, 'wait_for_EC2_consoleoutput')
+        self.mockwaitfor_EC2prop = self._patch_mock(lae_automation.initialize, 'wait_for_EC2_properties')
+        self.mockwaitfor_keyscan = self._patch_mock(lae_automation.initialize, 'wait_for_and_store_pubkeyfp_from_keyscan')
         self.mocklsc = self._patch_mock(lae_automation.initialize, 'LicenseServiceClient')
 
 
@@ -40,3 +47,15 @@ class InitializationTests (TestCase):
 
         adprcallmock.assert_called_once_with(ACTIVATIONKEY, PRODUCTTOKEN)
         return activate_returnval
+
+    def test_verify_and_store_serverssh_pubkey(self):
+        self.mockwaitfor_console.side_effect = lambda *args: defer.succeed('NONMATCHING FINGERPRINT_FROM_CONSOLE')
+        self.mockwaitfor_EC2prop.side_effect = lambda *args: defer.succeed((('IPfromAWS', 'XXX'), 'XXX'))
+        self.mockwaitfor_keyscan.return_value = ('NONMATCHING FINGERPRINT_FROM_KEYSCAN', 'PUBKEY', 'sshpubkey_IPfromAWS', 'IPfromAWS')
+
+        mismatchfailure = Failure(PublicKeyMismatch)
+        argtuple = ('ec2accesskeyid', 'ec2secretkey', 'endpoint_uri', 'addressparser',
+                    5, 15, sys.stdout, sys.stderr, 'instance_id')
+        d = self.failUnlessFailure(verify_and_store_serverssh_pubkey(*argtuple),
+                                   mismatchfailure.value)
+        return d
