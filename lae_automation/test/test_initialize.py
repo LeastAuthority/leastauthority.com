af5145368fc816e8d576f4b7e73ea284eb2aa9a5
zancas <zancas@leastauthority.com>
2013-04-19 02:55:52 -0700

    update test_initialize to reflect new IP dependent sshfpverify
    wait_for_EC2_propertie is no longer needed in the sshfpverification
    code.

--- lae_automation/test/test_initialize.py
+++ lae_automation/test/test_initialize.py
@@ -24,9 +24,10 @@ class InitializationTests (TestCase):
         return m
 
     def setUp(self):
-        self.mockwaitfor_console = self._patch_mock(lae_automation.initialize, 'wait_for_EC2_sshfp')
-        self.mockwaitfor_EC2prop = self._patch_mock(lae_automation.initialize, 'wait_for_EC2_properties')
-        self.mockwaitfor_keyscan = self._patch_mock(lae_automation.initialize, 'wait_for_and_store_pubkeyfp_from_keyscan')
+        self.mockwaitfor_console = self._patch_mock(lae_automation.initialize,
+                                                    'wait_for_EC2_sshfp')
+        self.mockwaitfor_keyscan = self._patch_mock(lae_automation.initialize,
+                                                    'wait_for_and_store_pubkeyfp_from_keyscan')
         self.mocklsc = self._patch_mock(lae_automation.initialize, 'LicenseServiceClient')
 
 
@@ -48,13 +49,14 @@ class InitializationTests (TestCase):
         return activate_returnval
 
     def test_verify_and_store_serverssh_pubkey(self):
-        self.mockwaitfor_console.side_effect = lambda *args: defer.succeed('NONMATCHING FINGERPRINT_FROM_CONSOLE')
-        self.mockwaitfor_EC2prop.side_effect = lambda *args: defer.succeed((('IPfromAWS', 'XXX'), 'XXX'))
-        self.mockwaitfor_keyscan.return_value = ('NONMATCHING FINGERPRINT_FROM_KEYSCAN', 'HASHED_PUBKEY', 'IPfromAWS')
+        self.mockwaitfor_console.side_effect = lambda *args: defer.succeed(
+            'NONMATCHING FINGERPRINT_FROM_CONSOLE')
+        self.mockwaitfor_keyscan.side_effect = lambda *args: defer.succeed(
+            ('NONMATCHING FINGERPRINT_FROM_KEYSCAN', 'HASHED_PUBKEY') )
 
         mismatchfailure = Failure(PublicKeyMismatch)
-        argtuple = ('ec2accesskeyid', 'ec2secretkey', 'endpoint_uri', 'addressparser',
-                    5, 15, sys.stdout, sys.stderr, 'instance_id')
+        argtuple = ('ec2accesskeyid', 'ec2secretkey', 'endpoint_uri', 'pub_ipaddress', .1, 600,
+                    sys.stdout, sys.stderr, 'instance_id')
         d = self.failUnlessFailure(verify_and_store_serverssh_pubkey(*argtuple),
                                    mismatchfailure.value)
         return d
