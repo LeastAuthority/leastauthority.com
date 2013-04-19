363677024ca6d1d4b1d7618db74c23bffd701536
zancas <zancas@leastauthority.com>
2013-04-19 03:05:53 -0700

    Test fp verifying signup.
    The consoleoutputreponse is the example xml associated with the getconsoleoutput action, but the
    'output' field is a base64 encoded output from an actual EC2 I spun up during testing.  The
    fingerprint and key were also obtained from that (now terminated) instance.

--- lae_automation/test/test_signup.py
+++ lae_automation/test/test_signup.py
@@ -76,6 +76,16 @@ createtagsresponse = """<CreateTagsResponse xmlns="http://ec2.amazonaws.com/doc/
   <return>true</return>
 </CreateTagsResponse>"""
 
+# Get Console
+getconsoleoutputresponse = """<GetConsoleOutputResponse xmlns="http://ec2.amazonaws.com/doc/2013-02-01/">
+  <requestId>59dbff89-35bd-4eac-99ed-be587EXAMPLE</requestId> 
+  <instanceId>i-MOCKTEST</instanceId>
+  <timestamp>2010-10-14T01:12:41.000Z</timestamp>
+  <output>MjAxMy0wNC0xMSAyMDozMjoyMiwxMTYgLSBfX2luaXRfXy5weVtXQVJOSU5HXTogVW5oYW5kbGVk\nIG5vbi1tdWx0aXBhcnQgdXNlcmRhdGEgJycNCkdlbmVyYXRpbmcgcHVibGljL3ByaXZhdGUgcnNh\nIGtleSBwYWlyLg0KWW91ciBpZGVudGlmaWNhdGlvbiBoYXMgYmVlbiBzYXZlZCBpbiAvZXRjL3Nz\naC9zc2hfaG9zdF9yc2Ffa2V5Lg0KWW91ciBwdWJsaWMga2V5IGhhcyBiZWVuIHNhdmVkIGluIC9l\ndGMvc3NoL3NzaF9ob3N0X3JzYV9rZXkucHViLg0KVGhlIGtleSBmaW5nZXJwcmludCBpczoNCmI4\nOjgzOmNmOjFkOjk3OjRiOjQ0OjhmOmE3OjA1OjI5OjRlOmY2OjFlOmFmOmRkIHJvb3RAaXAtMTAt\nMTk0LTI5LTM5DQpUaGUga2V5J3MgcmFuZG9tYXJ0IGltYWdlIGlzOg0KKy0tWyBSU0EgMjA0OF0t\nLS0tKw0KfCAgICAgICAgICAgICAgICAgfA0KfCAgICAgICAgICAgLiAgICAgfA0KfCAgICAgICAg\nKyArICAgICAgfA0KfCAgICAgICA9ICsgKyAgICAgfA0KfCAgICAgIC4gUyA9ICsgICAgfA0KfCAg\nICAgLiAuIG8gQiAgICAgfA0KfCAgICAuIG8gLiAqIC4gICAgfA0KfCAgICAgbyBvICsgKyAuICAg\nfA0KfCAgICAgIG8gLiBvIC4gRSAgfA0KKy0tLS0tLS0tLS0tLS0tLS0tKw0KR2VuZXJhdGluZyBw\ndWJsaWMvcHJpdmF0ZSBkc2Ega2V5IHBhaXIuDQpZb3VyIGlkZW50aWZpY2F0aW9uIGhhcyBiZWVu\nIHNhdmVkIGluIC9ldGMvc3NoL3NzaF9ob3N0X2RzYV9rZXkuDQpZb3VyIHB1YmxpYyBrZXkgaGFz\nIGJlZW4gc2F2ZWQgaW4gL2V0Yy9zc2gvc3NoX2hvc3RfZHNhX2tleS5wdWIuDQpUaGUga2V5IGZp\nbmdlcnByaW50IGlzOg0KMjU6ZmQ6Nzk6MjE6MmM6NjI6ZDI6MGQ6NzI6MGE6NGM6NTg6MGI6NmE6\nNWM6MjAgcm9vdEBpcC0xMC0xOTQtMjktMzkNClRoZSBrZXkncyByYW5kb21hcnQgaW1hZ2UgaXM6\nDQorLS1bIERTQSAxMDI0XS0tLS0rDQp8RS5vKisgLiBvICAgICAgICB8DQp8by5vLi5vID0gKyAu\nICAgICB8DQp8Lm8gIC4gbyA9ID0gbyAuICB8DQp8LiAgICAgIG8gKyBvIG8gLiB8DQp8ICAgICAg\nICBTICAgbyAuICB8DQp8ICAgICAgICAgICAgIC4gICB8DQp8ICAgICAgICAgICAgICAgICB8DQp8\nICAgICAgICAgICAgICAgICB8DQp8ICAgICAgICAgICAgICAgICB8DQorLS0tLS0tLS0tLS0tLS0t\nLS0rDQpHZW5lcmF0aW5nIHB1YmxpYy9wcml2YXRlIGVjZHNhIGtleSBwYWlyLg0KWW91ciBpZGVu\ndGlmaWNhdGlvbiBoYXMgYmVlbiBzYXZlZCBpbiAvZXRjL3NzaC9zc2hfaG9zdF9lY2RzYV9rZXku\nDQpZb3VyIHB1YmxpYyBrZXkgaGFzIGJlZW4gc2F2ZWQgaW4gL2V0Yy9zc2gvc3NoX2hvc3RfZWNk\nc2Ffa2V5LnB1Yi4NClRoZSBrZXkgZmluZ2VycHJpbnQgaXM6DQo0MjpjOTowZTpiNTozMzo3NDo1\nZDowMDpkODo1ODowZTo1MDozMjpiNTpiNDoyNiByb290QGlwLTEwLTE5NC0yOS0zOQ0KVGhlIGtl\neSdzIHJhbmRvbWFydCBpbWFnZSBpczoNCistLVtFQ0RTQSAgMjU2XS0tLSsNCnwgICAgKytCPStv\nLm8uICAgIHwNCnwgICAgIEJvTy4gLiAgICAgIHwNCnwgICAgRSBAIC4gICAgICAgIHwNCnwgICAg\nICogbyAgICAgICAgIHwNCnwgICAgICBvIFMgICAgICAgIHwNCnwgICAgICAgLiAgICAgICAgIHwN\nCnwgICAgICAgICAgICAgICAgIHwNCnwgICAgICAgICAgICAgICAgIHwNCnwgICAgICAgICAgICAg\nICAgIHwNCistLS0tLS0tLS0tLS0tLS0tLSsNClNraXBwaW5nIHByb2ZpbGUgaW4gL2V0Yy9hcHBh\ncm1vci5kL2Rpc2FibGU6IHVzci5zYmluLnJzeXNsb2dkDQogKiBTdGFydGluZyBBcHBBcm1vciBw\ncm9maWxlcyAgICAgICAbWzgwRyANG1s3NEdbIE9LIF0NCmxhbmRzY2FwZS1jbGllbnQgaXMgbm90\nIGNvbmZpZ3VyZWQsIHBsZWFzZSBydW4gbGFuZHNjYXBlLWNvbmZpZy4NCkdlbmVyYXRpbmcgbG9j\nYWxlcy4uLgogIGVuX1VTLlVURi04Li4uIGRvbmUKR2VuZXJhdGlvbiBjb21wbGV0ZS4KZWMyOiAK\nZWMyOiAjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj\nIyMjIyMjIyMjCmVjMjogLS0tLS1CRUdJTiBTU0ggSE9TVCBLRVkgRklOR0VSUFJJTlRTLS0tLS0K\nZWMyOiAxMDI0IDI1OmZkOjc5OjIxOjJjOjYyOmQyOjBkOjcyOjBhOjRjOjU4OjBiOjZhOjVjOjIw\nICByb290QGlwLTEwLTE5NC0yOS0zOSAoRFNBKQplYzI6IDI1NiA0MjpjOTowZTpiNTozMzo3NDo1\nZDowMDpkODo1ODowZTo1MDozMjpiNTpiNDoyNiAgcm9vdEBpcC0xMC0xOTQtMjktMzkgKEVDRFNB\nKQplYzI6IDIwNDggYjg6ODM6Y2Y6MWQ6OTc6NGI6NDQ6OGY6YTc6MDU6Mjk6NGU6ZjY6MWU6YWY6\nZGQgIHJvb3RAaXAtMTAtMTk0LTI5LTM5IChSU0EpCmVjMjogLS0tLS1FTkQgU1NIIEhPU1QgS0VZ\nIEZJTkdFUlBSSU5UUy0tLS0tCmVjMjogIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj\nIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwotLS0tLUJFR0lOIFNTSCBIT1NUIEtFWSBLRVlT\nLS0tLS0KZWNkc2Etc2hhMi1uaXN0cDI1NiBBQUFBRTJWalpITmhMWE5vWVRJdGJtbHpkSEF5TlRZ\nQUFBQUlibWx6ZEhBeU5UWUFBQUJCQkd6d1l6WjF1eXVDSHJNR3ZHZHQyazFKakRhUTl1R2RGQXM2\nQ3k3dlkyTW95c3IxbmZaVE1KME5BV2I4MlkxZ3I0amZqbUIwY1BtQks4VGcxd2Urb1BvPSByb290\nQGlwLTEwLTE5NC0yOS0zOQpzc2gtcnNhIEFBQUFCM056YUMxeWMyRUFBQUFEQVFBQkFBQUJBUUNn\nb0M4OXZlNjFVaS9VaTVwbklTdmlkWVcvdVVyVFN6MEYwajBjenV6RHlTUGwvUnF4S3VadllRQWp1\nZzZPRW4wT2htbmg0Mmo2RGs0THIwOUg5R0xKVHVEdjJrVE5oUUY5ODBSWFVQcTNOVHlvengxc3A4\nMjRYM3pHK2VQaWlZejJPUUkvQ2YzcjJjQVVxZ2dKakE4d1BhV1NYeXV5cC9MczI4NjFYdnNuUzVp\nSVNueWxwRXVOL09YTkZSbXVKUnpzK2hjazV1ck1Weno1QWRibjl6U0svTkpSUFlTM3FTZnZDTjZw\nWEJ5TGNFSlVFTkNLSUE4MFZSSEtudi9pc0tzY0Fadm1ESkNvclJXWFJ2eEI2eExCMlZuVGJxQlVU\nUWErMHpyemZRU0pEOEMyMlliUGxnd3NGdG4wNzl3T2trYVF0ZC9BaGhQWGZUbEFVbGpBZkMvQiBy\nb290QGlwLTEwLTE5NC0yOS0zOQotLS0tLUVORCBTU0ggSE9TVCBLRVkgS0VZUy0tLS0tCmNsb3Vk\nLWluaXQgYm9vdCBmaW5pc2hlZCBhdCBUaHUsIDExIEFwciAyMDEzIDIwOjMyOjMyICswMDAwLiBV\ncCAyOC4yNCBzZWNvbmRzCg==\n</output>
+</GetConsoleOutputResponse>"""  
+
+MOCKSERVERSSHFP = 'b8:83:cf:1d:97:4b:44:8f:a7:05:29:4e:f6:1e:af:dd'
+MOCKHASHEDPUBKEY = """|1|lrzohCU8y8Obch3wa7+gnvEJuI0=|I1GQU+vw3MgMnyvY+SxnhCyArHg= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgoC89ve61Ui/Ui5pnISvidYW/uUrTSz0F0j0czuzDySPl/RqxKuZvYQAjug6OEn0Ohmnh42j6Dk4Lr09H9GLJTuDv2kTNhQF980RXUPq3NTyozx1sp824X3zG+ePiiYz2OQI/Cf3r2cAUqggJjA8wPaWSXyuyp/Ls2861XvsnS5iISnylpEuN/OXNFRmuJRzs+hck5urMVzz5Adbn9zSK/NJRPYS3qSfvCN6pXByLcEJUENCKIA80VRHKnv/isKscAZvmDJCorRWXRvxB6xLB2VnTbqBUTQa+0zrzfQSJD8C22YbPlgwsFtn079wOkkaQtd/AhhPXfTlAUljAfC/B"""
 # Vector data for the config file data:
 CONFIGFILEJSON = """{
   "products": [
@@ -111,7 +121,7 @@ MONITORPUBKEY = 'MONITOR PUBLIC KEY'
 class TestSignupModule(TestCase):
     def setUp(self):
         self.fakeURLs = [adphttprequestheader, verifyhttprequestheader]
-        self.mhr_return_values = [adprequestresponse, verifyrequestresponse, describeEC2instresponse]
+        self.mhr_return_values = [adprequestresponse, verifyrequestresponse]
         self.CONFIGFILEPATH = 'init_test_config.json'
         self.SERVERINFOPATH = 'mock_serverinfo.csv'
         self.EC2SECRETPATH = 'mock_ec2secret'
@@ -139,6 +149,13 @@ class TestSignupModule(TestCase):
             self.failUnlessEqual(request_url, self.fakeURLs.pop(0))
             return defer.succeed(self.mhr_return_values.pop(0))
         self.patch(queryapi, 'make_http_request', call_make_http_request)
+
+        from lae_automation.aws.queryapi import EC2ConsoleClient
+        def call_EC2ConsoleClient_describe_console_output(EC2Instance, instance_id):
+            return defer.succeed( 
+                EC2Instance.parser.describe_console_output(getconsoleoutputresponse) )
+        self.patch(EC2ConsoleClient, 'describe_console_output', 
+                   call_EC2ConsoleClient_describe_console_output)
 
         # Because the S3 Client call to S3 is made through txaws, it circumvents make_http_request,
         # and necessitates a separate patch to isolate the system from remote components.
@@ -177,6 +194,11 @@ class TestSignupModule(TestCase):
             self.failUnlessEqual(instance_ids, ('i-MOCKEC2INSTANCEID',))
             return defer.succeed( [('0.0.0.0', '0.0.0.1')] )
         self.patch(EC2Client, 'describe_instances', call_describe_instances)
+
+        def call_get_and_store_pubkeyfp_from_keyscan(targetIP, stdout):
+            return (MOCKSERVERSSHFP, MOCKHASHEDPUBKEY)
+        self.patch(initialize, 'get_and_store_pubkeyfp_from_keyscan', 
+                   call_get_and_store_pubkeyfp_from_keyscan)
 
         from lae_automation.server import NotListeningError
         self.first = True
@@ -244,6 +266,7 @@ class TestSignupModule(TestCase):
         MSEED = 'MSEED'
         MSECRETSFILE = 'MSECRETSFILE'
         MLOGFILENAME = '2012-01-01T000000Z-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        self.patch(signup, 'VASTOTALWAIT', 0)
 
         d = signup.signup(MACTIVATIONKEY, MPRODUCTCODE, MNAME, MEMAIL, MKEYINFO, stdout, stderr,
                           MSEED, MSECRETSFILE, MLOGFILENAME, self.CONFIGFILEPATH,
@@ -264,8 +287,8 @@ class TestSignupModule(TestCase):
         FilePath(self.CONFIGFILEPATH).setContent(ZEROPRODUCT)
 
         self.failUnlessRaises(AssertionError, signup.signup,
-                              MACTIVATIONKEY, MPRODUCTCODE, MNAME, MEMAIL, MKEYINFO, stdout, 
-                              stderr, MSEED, MSECRETSFILE, MLOGFILENAME, self.CONFIGFILEPATH,
+                              MACTIVATIONKEY, MPRODUCTCODE, MNAME, MEMAIL, MKEYINFO, stdout, stderr,
+                              MSEED, MSECRETSFILE, MLOGFILENAME, self.CONFIGFILEPATH,
                               self.SERVERINFOPATH, self.EC2SECRETPATH)
 
     def test_timeout_verify(self):
@@ -338,11 +361,12 @@ class TestSignupModule(TestCase):
         MSEED = 'MSEED'
         MSECRETSFILE = 'MSECRETSFILE'
         MLOGFILENAME = '2012-01-01T000000Z-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        self.patch(signup, 'VASTOTALWAIT', 0)
 
-        from lae_automation.server import NotListeningError
-        def call_install_server(publichost, admin_privkey_path, monitor_pubkey, monitor_privkey_path, stdout, stderr):
-            raise NotListeningError()
-        self.patch(signup, 'install_server', call_install_server)
+        from lae_automation.aws import queryapi
+        def call_get_EC2_consoleoutput(ec2accesskeyid, ec2secretkey, endpoint_uri, instance_id):
+            return defer.succeed(None)
+        self.patch(queryapi, 'get_EC2_consoleoutput', call_get_EC2_consoleoutput)
 
         d = signup.signup(MACTIVATIONKEY, MPRODUCTCODE, MNAME, MEMAIL, MKEYINFO, stdout, stderr,
                           MSEED, MSECRETSFILE, MLOGFILENAME, self.CONFIGFILEPATH,
