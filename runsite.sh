b410905560bf87671dff09e0be3420f84a89b30d
Leif Ryge <leif@synthesize.us>
2013-07-12 03:23:45 +0000

    make runsite.sh kill existing process
    I plan to do some proper process supervision eventually, but OTAT.

--- runsite.sh
+++ runsite.sh
@@ -2,4 +2,13 @@
 cd /home/website/leastauthority.com
 flappserver restart `pwd`/flapp
 sleep 2
-PYTHONPATH=. authbind --deep sh -c 'python -u `pwd`/lae_site/main.py "$@" >>../site.out 2>&1'
+cmd="python -u `pwd`/lae_site/main.py"
+pids=$(pgrep -fl "$cmd" |cut -f1 -d' ')
+if [ "$pids" != "" ]; then
+       echo killing $pids
+       kill $pids
+fi
+sleep .1
+PYTHONPATH=. authbind --deep sh -c "$cmd"' "$@" >>../site.out 2>&1' &
+sleep .1
+pgrep -fl "$cmd"
