10deb42b7d9df4df1dc32c13d1e26282863142ec
Leif Ryge <leif@synthesize.us>
2013-07-12 02:19:19 +0000

    make runsite.sh exit 0

--- runsite.sh
+++ runsite.sh
@@ -2,17 +2,5 @@
 cd /home/website/leastauthority.com
 flappserver restart `pwd`/flapp
 sleep 2
-cmd="python -u `pwd`/lae_site/main.py"
-pids=$(pgrep -fl "$cmd" |cut -f1 -d' ')
-if [ "$pids" != "" ]; then
-       echo killing $pids
-       kill $pids
-fi
-sleep .1
-if [ "$1" = "--dev" ]; then
-    PYTHONPATH=. $cmd --dev
-else
-    PYTHONPATH=. authbind --deep sh -c "$cmd"' "$@" >>../site.out 2>&1' &
-fi
-sleep .1
-pgrep -fl "$cmd"
+PYTHONPATH=. authbind --deep sh -c 'python -u `pwd`/lae_site/main.py "$@" >>../site.out 2>&1'
+exit 0
