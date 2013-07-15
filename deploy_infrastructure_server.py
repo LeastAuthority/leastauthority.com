4409843903f0ad83b106cf106e6c8398fdc2e707
zancas <zancas@leastauthority.com>
2013-07-15 11:37:11 -0600

    apt-get install foolscap on infrastructure server, and change cli

--- deploy_infrastructure_server.py
+++ deploy_infrastructure_server.py
@@ -19,15 +19,13 @@ from twisted.internet import defer
 
 parser = argparse.ArgumentParser(description="Deploy a new infrastructure server. You must specify each necessary repository-and-reference (e.g. leastauthority.com-and-SHA1) as an ordered pair of path_to_repository, and reference to the specific commit you want deployed.")
 
-parser.add_argument("instance_name", type=str, help="How this instance is referred to e.g. via AWS console.")
-
 parser.add_argument("leastauthority_com_version_ID", help="This ordered parameter-pair consists of two parts, which are sufficient to specify a commit. First: the absolute path to the git repository which contains the leastauthority.com code to deploy. Second: the reference to the specific commit, within that repository, which will be deployed.", nargs=2)
 
 parser.add_argument("secrets_version_ID", help="This ordered parameter-pair consists of two parts, which are sufficient to specify a commit. First: the absolute path to the git repository which contains the secret_config code to deploy. Second: the reference to the specific commit, within that repository, which will be deployed.", nargs=2)
 
 exc_group = parser.add_mutually_exclusive_group()
 exc_group.add_argument('--existing_host', type=str)
-exc_group.add_argument('--new_host', action='store_true', default=False)
+exc_group.add_argument('--new_host', type=str, help="How this instance is referred to e.g. via AWS console.")
 
 args = parser.parse_args()
 print "args: %s" % args
@@ -39,7 +37,6 @@ secret_conf_repo_path = args.secrets_version_ID[0]
 secrets_commit_ref = args.secrets_version_ID[1]
 existing_host = args.existing_host
 new_host = args.new_host
-instance_name = args.instance_name
 
 # XXX configpath is a function of the secret_config repo specified in the arguments!
 
@@ -88,7 +85,7 @@ if existing_host:
 elif new_host:
     d = deploy_infrastructure_EC2(ec2accesskeyid, ec2secretkey, endpoint_uri,
                                   ami_image_id, instance_size, bucket_name,
-                                  keypair_name, instance_name,
+                                  keypair_name, new_host,
                                   admin_privkey_path, website_pubkey,
                                   leastauthority_repo_path,
                                   leastauth_commit_ref,
