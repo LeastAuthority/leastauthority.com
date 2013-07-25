edea036ad7087195e4302fc8f960257767a82a03
zancas <zancas@leastauthority.com>
2013-07-25 07:34:38 -0600

    Make private key file permissions ssh-correct.

--- lae_automation/server.py
+++ lae_automation/server.py
@@ -317,6 +317,9 @@ postfix	postfix/main_mailer_type select	No configuration"""
     setup_git_deploy(publichost, '/home/website/leastauthority.com', leastauth_repo, la_commit_hash)
     setup_git_deploy(publichost, '/home/website/secret_config', secretconf_repo, sc_commit_hash)
 
+    with cd('/home/website/secret_config'):
+        run('chmod 400 *pem')
+
     with cd('/home/website/leastauthority.com'):
         #FIXME: make idempotent
         if not files.exists('/home/website/leastauthority.com/flapp'):
