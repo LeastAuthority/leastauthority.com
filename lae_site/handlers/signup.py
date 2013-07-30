aa387b3f743715eb9f445f03553f2b639b5d1014
Daira Hopwood <daira@leastauthority.com>
2013-07-30 22:31:33 +0100

    Another attempt to make /signup not return an error.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_site/handlers/signup.py
+++ lae_site/handlers/signup.py
@@ -2,8 +2,6 @@
 from twisted.web.util import Redirect
 from twisted.web.resource import Resource
 
-#from lae_site.handlers.web import env
-
 
 class SignupHandler(Resource):
     isLeaf = 0
@@ -11,17 +9,6 @@ class SignupHandler(Resource):
     def __init__(self, products):
         self.products = products
         self.children = {}
-        self.putChild('', self)
+        self.putChild('', Redirect('/products'))
         for product in self.products:
             self.putChild(str(product['short_name']), Redirect(str(product['signup_url'])))
-
-    def render_GET(self, request):
-        return Redirect("/products")
-
-        # FIXME: escape the short and full names
-        #productlist = ''.join(['<li><a href="/signup/%s">%s</a>' % (p['short_name'], p['full_name'])
-        #                       for p in self.products if p['listed']])
-
-        #tmpl = env.get_template('signup.html')
-        #request.setResponseCode(200)
-        #return tmpl.render(productlist=productlist).encode('utf-8')
