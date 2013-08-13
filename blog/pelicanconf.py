2016ba24a37656b92ee4e261d595c1c4d3c2726c
zancas <zancas@leastauthority.com>
2013-08-13 22:22:45 +0000

    Started pelican blog

---
+++ blog/pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u"Zooko Wilcox-O'Hearn"
+SITENAME = u'Least Authority Blog'
+SITEURL = ''
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = "themes/simple"
+DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_PATH = '../lae_site/templates/blog/'
