7c91c7fbb9c02c9f940e8636cc1f08a11df29bc4
Daira Hopwood <daira@leastauthority.com>
2013-08-15 01:56:59 +0100

    Change HTML doctype used by devpay_complete handlers to HTML5.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_site/handlers/devpay_complete.py
+++ lae_site/handlers/devpay_complete.py
@@ -26,7 +26,7 @@ ACTIVE_PRODUCTS = set(['s4'])
 SIGNUP_BASE_URL = "https://leastauthority.com/signup/"
 
 def html(title, body):
-    return """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    return """<!DOCTYPE HTML>
 <html>
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
