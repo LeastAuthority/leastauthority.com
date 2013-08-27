4a45d680a5670913fa6c24ca332e9577ed3b8ecb
zancas <zancas@leastauthority.com>
2013-08-27 16:23:11 -0600

    Rename stripe subscription form and print out POST response.

--- lae_site/handlers/subscribe.py
+++ lae_site/handlers/subscribe.py
@@ -1,14 +1,15 @@
 
 #XXXfrom twisted.web.util import Redirect
+import stripe
 from twisted.web.resource import Resource
 
 from lae_site.handlers.web import env
-
 
 class SubscriptionHandler(Resource):
     #XXXisLeaf = 0
 
     def __init__(self, basefp, products):
+        pass
         """self.products = products
         self.children = {}
         self.putChild('', Redirect('/products'))
@@ -16,6 +17,8 @@ class SubscriptionHandler(Resource):
             self.putChild(str(product['short_name']), Redirect(str(product['signup_url'])))"""
 
     def render_GET(self, request):
-        tmpl = env.get_template('payment.html')
-        request.setResponseCode(200)
+        tmpl = env.get_template('subscription_signup.html')
         return tmpl.render().encode('utf-8', 'replace')
+
+    def render_POST(self, request):
+        return '<html><body>%s</body></html>' % (request.args,)
