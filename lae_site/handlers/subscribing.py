e5e16c4a5c8b8613fd8bab9b1f8cf426a93f6957
zancas <zancas@leastauthority.com>
2013-08-28 18:53:53 -0600

    render_POST is now handled by the subscription-complete resource.

--- lae_site/handlers/subscribing.py
+++ lae_site/handlers/subscribing.py
@@ -1,5 +1,4 @@
 
-#XXXfrom twisted.web.util import Redirect
 from twisted.web.resource import Resource
 
 from lae_site.handlers.web import env
@@ -13,13 +12,3 @@ class SubscriptionSubmitHandler(Resource):
     def render_GET(self, request):
         tmpl = env.get_template('subscription_signup.html')
         return tmpl.render().encode('utf-8', 'replace')
-
-    def render_POST(self, request):
-        import stripe, cgi
-        stripe.api_key = "sk_test_mkGsLqEW6SLnZa487HYfJVLf"#'pk_test_czwzkTp2tactuLOEOqbMTRzG'
-        token = request.args['stripeToken'][0]
-        card = token
-        plan = 's4'
-        email = request.args['Email'][0]
-        customer = stripe.Customer.create(card=token, plan='s4', email=request.args['Email'][0])
-        return '<html><body>%s</body></html>' % (cgi.escape(customer['id']),)
