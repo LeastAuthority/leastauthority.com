9ecaf074ae6aa994314d5e545fb99a2ba5ec37d5
Leif Ryge <leif@synthesize.us>
2013-09-11 12:27:19 +0000

    add --dev option to site.py and runsite.sh
     - runs without SSL, on port 8000, doesn't need authbind

--- runsite.sh
+++ runsite.sh
@@ -10,6 +10,10 @@ if [ "$pids" != "" ]; then
        kill $pids
 fi
 sleep .1
-PYTHONPATH=. authbind --deep sh -c "$cmd"' "$@" >>../site.out 2>&1' &
+if [ "$1" = "--dev" ]; then
+    PYTHONPATH=. $cmd --dev
+else
+    PYTHONPATH=. authbind --deep sh -c "$cmd"' "$@" >>../site.out 2>&1' &
+fi
 sleep .1
 pgrep -fl "$cmd"
