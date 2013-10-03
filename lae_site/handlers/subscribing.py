4acc9df9da35302ba816a8aa94d6ef1566e90a63
Emile Snyder <emile.snyder@gmail.com>
2013-10-03 08:31:03 -0700

    Make the collect-email result page not reference amazon devpay, link to stripe payment handler, auto-fill email field.

--- lae_site/handlers/subscribing.py
+++ lae_site/handlers/subscribing.py
@@ -1,3 +1,4 @@
+import sys
 
 from lae_site.handlers.web import env
 from lae_site.handlers.devpay_complete import HandlerBase
@@ -10,4 +11,5 @@ class SubscriptionSubmitHandler(HandlerBase):
 
     def render_GET(self, request):
         tmpl = env.get_template('subscription_signup.html')
-        return tmpl.render().encode('utf-8', 'replace')
+        email = self.get_arg(request, 'email')
+        return tmpl.render(email=email).encode('utf-8', 'replace')
