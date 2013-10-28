c9a5ae9caeaab6dcac672dd35c91748496b0743a
zancas <zancas@leastauthority.com>
2013-10-28 11:53:42 -0600

    Identify anonymous js functs so we can unit test.
    This code was heavily influenced/designed by nejucomo.

--- content/static/js/subscription_signup.js
+++ content/static/js/subscription_signup.js
@@ -1,34 +1,40 @@
 "use strict";
 
-// This identifies your website in the createToken call below
-Stripe.setPublishableKey('pk_test_czwzkTp2tactuLOEOqbMTRzG')
-jQuery(function($) {
-    $('#payment-form').submit(function(event) {
-        var $form = $(this);
+window.credit_card_verifier = (function () {
+      return {
+	  stripeResponseHandler: function(status, response) {
+	      var $form = $('#payment-form');
 
-        // Disable the submit button to prevent repeated clicks
-        $form.find('button').prop('disabled', true);
+	      if (response.error) {
+		  // Show the errors on the form
+		  $form.find('.payment-errors').text(response.error.message);
+		  $form.find('button').prop('disabled', false);
+	      } else {
+		  // token contains id, last4, and card type
+		  var token = response.id;
+		  // Insert the token into the form so it gets submitted to the server
+		  $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+		  // and submit
+		  $form.get(0).submit();
+	      }
+	  },
+	  form_submission_handler: function (event) {
+	      var $form = $(this);
+	      // Disable the submit button to prevent repeated clicks
+              $form.find('button').prop('disabled', true);
+	      
 
-        Stripe.createToken($form, stripeResponseHandler);
-	
-        // Prevent the form from submitting with the default action
-        return false;
-    });
-});
-
-var stripeResponseHandler = function(status, response) {
-    var $form = $('#payment-form');
-
-    if (response.error) {
-	// Show the errors on the form
-	$form.find('.payment-errors').text(response.error.message);
-	$form.find('button').prop('disabled', false);
-    } else {
-	// token contains id, last4, and card type
-	var token = response.id;
-	// Insert the token into the form so it gets submitted to the server
-	$form.append($('<input type="hidden" name="stripeToken" />').val(token));
-	// and submit
-	$form.get(0).submit();
-    }
-};
+              Stripe.createToken($form, credit_card_verifier.stripeResponseHandler);
+	      
+	      return false;
+	  },
+	  register_callbacks: function () {
+	      // This identifies your website in the createToken call below
+	      Stripe.setPublishableKey('pk_test_czwzkTp2tactuLOEOqbMTRzG');
+	      jQuery(function ($) {
+		  $('#payment-form').submit( credit_card_verifier.form_submission_handler );
+	      });
+	      
+	  },
+      }
+}());
