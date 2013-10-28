4aa2f86acddd0fe7d5f65c53edc599fb0ab759aa
zancas <zancas@leastauthority.com>
2013-10-28 16:24:27 -0600

    First unittest with interesting assertions.
    I'd like to replace the invocation of jasmine.any(Object)
    with something more specific.

--- content/static/js/test/unit/unittests.js
+++ content/static/js/test/unit/unittests.js
@@ -1,10 +1,28 @@
 "use strict";
 
-describe('subcription to stripe', function () {
-  it('should be defined', function () {
-    expect(creditcardVerifier).toBeDefined();
-    expect(creditcardVerifier.stripeResponseHandler).toBeDefined();
-    expect(creditcardVerifier.formSubmissionHandler).toBeDefined();
-    expect(creditcardVerifier.register_callbacks).toBeDefined();
-  });
+describe('callback registration', function () {
+    it('should be defined', function () {
+	expect(creditcardVerifier).toBeDefined();
+	expect(creditcardVerifier.stripeResponseHandler).toBeDefined();
+	expect(creditcardVerifier.formSubmissionHandler).toBeDefined();
+	expect(creditcardVerifier.register_callbacks).toBeDefined();
+    });
+});
+
+describe('The submission handler', function () { 
+
+    beforeEach(function() {
+	window.Stripe = {
+	    createToken: function($form, responsehandler){}
+	};
+	spyOn(Stripe, 'createToken');
+	window.mockform = jasmine.createSpy("this_form");
+	creditcardVerifier.formSubmissionHandler.call(window.mockform, jasmine.createSpy("event"));
+    });
+    
+    it('registers the response handler', function () {
+	expect(Stripe.createToken).toHaveBeenCalledWith(jasmine.any(Object), creditcardVerifier.stripeResponseHandler);
+    });
+
+
 });
