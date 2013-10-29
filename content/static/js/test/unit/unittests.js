558769b4fdb164b323fe547dd436fa427df8b41d
zancas <zancas@leastauthority.com>
2013-10-29 15:29:41 -0600

    Ensure that Stripe is mocked for tests.

--- content/static/js/test/unit/unittests.js
+++ content/static/js/test/unit/unittests.js
@@ -10,42 +10,20 @@ describe('callback registration', function () {
 });
 
 describe('The submission handler', function () { 
+    var aliasStripe;
     beforeEach(function () {
+	aliasStripe = window.Stripe;
 	window.Stripe = { createToken : function($form, responsehandler){} };
 	spyOn(window.Stripe, 'createToken');
 	creditcardVerifier.formSubmissionHandler.call( jasmine.createSpy("this_form"), jasmine.createSpy("event") );
     });
 
+    afterEach( function () {
+	window.Stripe = aliasStripe;
+	delete window.aliasStripe;
+    });
+
     it('registers the response handler', function () {
 	expect(Stripe.createToken).toHaveBeenCalledWith(jasmine.any(Object), creditcardVerifier.stripeResponseHandler);
-    });
-    
+    }); 
 });
-
-/*
-describe('The submission handler2', function () { 
-    beforeEach(function() {
-	spyOn(Stripe, 'createToken');
-	window.mockform = jasmine.createSpy("this_form");
-	creditcardVerifier.formSubmissionHandler.call(window.mockform, jasmine.createSpy("event"));
-    });
-    
-    it('registers the response handler', function () {
-	expect(Stripe.createToken).toHaveBeenCalledWith(jasmine.any(Object), creditcardVerifier.stripeResponseHandler);
-    });
-});
-
-
-describe('The submission handler3', function () { 
-    window.Stripe = {};
-    var Stripe;
-    beforeEach(function() {
-	spyOn(Stripe, 'createToken');
-	mockform = jasmine.createSpy("this_form");
-	creditcardVerifier.formSubmissionHandler.call(mockform, jasmine.createSpy("event"));
-    });
-    
-    it('registers the response handler', function () {
-	expect(Stripe.createToken).toHaveBeenCalledWith(jasmine.any(Object), creditcardVerifier.stripeResponseHandler);
-    });
-});*/
