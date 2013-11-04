220277649c7e569b32dcfbaff2a38d66c9a3a5ed
zancas <zancas@leastauthority.com>
2013-11-04 14:46:46 -0700

    Simplify suite to reuse code.

--- content/static/js/test/unit/unittests.js
+++ content/static/js/test/unit/unittests.js
@@ -3,63 +3,44 @@
 describe('callback registration', function () {
     it('should be defined', function () {
 	expect(creditcardVerifier).toBeDefined();
-	expect(creditcardVerifier.stripeResponseHandler).toBeDefined();
 	expect(creditcardVerifier.formSubmissionHandler).toBeDefined();
+	expect(creditcardVerifier.stripeResponseHandler).toBeDefined();
 	expect(creditcardVerifier.register_callbacks).toBeDefined();
     });
 });
 
 describe('The form submission handler', function () { 
-    var asideStripe, aside$;
+    var asideStripe, aside$, jqueryform, HTMLformSpy, button;
     beforeEach(function () {
 	aside$ = window.$;
 	window.$ = {};
 	asideStripe = window.Stripe;
 	window.Stripe = {};
+	window.Stripe = jasmine.createSpyObj('Stripe', ['createToken']);
+	jqueryform = jasmine.createSpyObj('form', ['find']);
+	window.$ = jasmine.createSpy("$").andReturn(jqueryform);
+	HTMLformSpy = jasmine.createSpy("HTMLform");
+	button = jasmine.createSpyObj('button', ['prop'])
+	jqueryform.find.andReturn(button)
+
+	creditcardVerifier.formSubmissionHandler.call( HTMLformSpy, jasmine.createSpy("click") );
     });
 
     afterEach( function () {
 	window.Stripe = asideStripe;
-	asideStripe = {};
 	window.$ = aside$;
-	aside$ = {};
+	asideStripe, aside$, jqueryform, HTMLformSpy, button = {};
     });
 
     it('finds the form submission button', function () {
-	window.Stripe = jasmine.createSpyObj('Stripe', ['createToken']);
-	var jqueryform = jasmine.createSpyObj('form', ['find']);
-	window.$ = jasmine.createSpy("$").andReturn(jqueryform);
-	var HTMLformSpy = jasmine.createSpy("HTMLform");
-	var button = jasmine.createSpyObj('button', ['prop'])
-	jqueryform.find.andReturn(button)
-
-	creditcardVerifier.formSubmissionHandler.call( HTMLformSpy, jasmine.createSpy("click") );
 	expect(jqueryform.find).toHaveBeenCalledWith('button');
     }); 
 
     it('disables the form submission button', function () {
-	window.Stripe = jasmine.createSpyObj('Stripe', ['createToken']);
-	var jqueryform = jasmine.createSpyObj('form', ['find']);
-	window.$ = jasmine.createSpy("$").andReturn(jqueryform);
-	var HTMLformSpy = jasmine.createSpy("HTMLform");
-	var button = jasmine.createSpyObj('button', ['prop'])
-	jqueryform.find.andReturn(button)
-
-	creditcardVerifier.formSubmissionHandler.call( HTMLformSpy, jasmine.createSpy("click") );
 	expect(button.prop).toHaveBeenCalledWith('disabled', true);
     }); 
 
     it('registers the response handler inside Stripe.createToken', function () {
-	window.Stripe = jasmine.createSpyObj('Stripe', ['createToken']);
-	var jqueryform = jasmine.createSpyObj('form', ['find']);
-	window.$ = jasmine.createSpy("$").andReturn(jqueryform);
-	var HTMLformSpy = jasmine.createSpy("HTMLform");
-	var button = jasmine.createSpyObj('button', ['prop'])
-	jqueryform.find.andReturn(button)
-
-	creditcardVerifier.formSubmissionHandler.call( HTMLformSpy, jasmine.createSpy("click") );
 	expect(Stripe.createToken).toHaveBeenCalledWith( jqueryform, creditcardVerifier.stripeResponseHandler );
     }); 
-
-
 });
