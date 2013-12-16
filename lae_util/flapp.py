5fe1376f2fb2a53f02efa43e555b46bf4aab8af2
Daira Hopwood <daira@leastauthority.com>
2013-12-16 22:57:13 +0000

    lae_util/flapp.py: refactor FlappCommand to return a Deferred rather than taking when_done and when_failed continuations.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_util/flapp.py
+++ lae_util/flapp.py
@@ -4,6 +4,12 @@ from foolscap.api import Tub
 from foolscap.appserver.client import RunCommand, ClientOptions
 
 
+class CommandFailed(Exception):
+    def __init__(self, rc):
+        Exception.__init__(self, "Command failed with exit code %d" % (rc,))
+        self.rc = rc
+
+
 class FlappCommand(object):
     def __init__(self, furlfile):
         self.flappclient_args = ["-f", furlfile, "run-command"]
@@ -22,47 +28,33 @@ class FlappCommand(object):
         def _got_rref(rref):
             self.rref = rref
             done.callback(None)
-        self.d.addCallbacks(_got_rref, done.errback)
+        def _failed(f):
+            done.errback(f)
+            return f
+        self.d.addCallbacks(_got_rref, _failed)
         return done
 
-    def run(self, content, stdout, stderr, when_done, when_failed):
+    def run(self, content):
         assert isinstance(content, bytes), (`content`, type(content))
         assert self.rref is not None
         options = ClientOptions()
-        options.stdout = stdout
-        options.stderr = stderr
-        print >>stderr, 'self.flappclient_args: %s' % self.flappclient_args
         options.parseOptions(self.flappclient_args)
 
         def stdio(proto):
-            print >>stderr, '\n***\ncontent: %s' % content
-            #This value is being sent to the stdin of the flapp, and
-            #must be coerced to a bytestring.
-            proto.dataReceived(content.encode('utf-8'))
+            # This value is being sent to the stdin of the flapp.
+            proto.dataReceived(content)
             proto.connectionLost("EOF")
 
         options.subOptions.stdio = stdio
-        options.subOptions.stdout = stdout
-        options.subOptions.stderr = stderr
 
+        done = defer.Deferred()
         def _go(ign):
-            print >>stdout, "Starting..."
             return RunCommand().run(self.rref, options.subOptions)
-        def _done(rc):
-            if rc == 0:
-                when_done()
-            else:
-                print >>stdout, "Command failed with exit code %r." % (rc,)
-                when_failed()
-        def _error(f):
-            print >>stdout, str(f)
-            when_failed()
-        def _recover(f):
-            try:
-                print >>stderr, str(f)
-            except Exception:
-                print >>stderr, "something weird"
+        def _check_return_code(rc):
+            if rc != 0:
+                raise CommandFailed(rc)
 
         self.d.addCallback(_go)
-        self.d.addCallbacks(_done, _error)
-        self.d.addErrback(_recover)
+        self.d.addCallback(_check_return_code)
+        self.d.addBoth(done.callback)
+        return done
