3f2ca713880d7997ed89caf862df1ec1cfe69f65
Daira Hopwood <daira@leastauthority.com>
2014-01-29 01:40:31 +0000

    subscription_signup.js: consistent 4-space indentation.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- content/static/js/subscription_signup.js
+++ content/static/js/subscription_signup.js
@@ -1,50 +1,49 @@
 "use strict";
 
 window.creditcardVerifier = (function () {
-      return {
-	  register_callbacks: function () {
-	      var $form = $('#payment-form');
-	      // This identifies your website in the createToken call below
-	      Stripe.setPublishableKey('pk_test_IBiTH5UtEo2kB10eb1OSsv0w');
-	      $form.submit( creditcardVerifier.formSubmissionHandler );
-          $form.show();
-	  },
-	  formSubmissionHandler: function (event) {
-	      var $form = $('#payment-form');
-	      // Disable the submit button to prevent repeated clicks
-              $form.find('button').prop('disabled', true);      
+    return {
+        register_callbacks: function () {
+            var $form = $('#payment-form');
+            // This identifies your website in the createToken call below
+            Stripe.setPublishableKey('pk_test_IBiTH5UtEo2kB10eb1OSsv0w');
+            $form.submit( creditcardVerifier.formSubmissionHandler );
+            $form.show();
+        },
+        formSubmissionHandler: function (event) {
+            var $form = $('#payment-form');
+            // Disable the submit button to prevent repeated clicks
+            $form.find('button').prop('disabled', true);
 
-              Stripe.createToken($form, creditcardVerifier.stripeResponseHandler);
-	      
-	      return false;
-	  },
-      use_pgp: function (checked) {
-          var $pgp = $('#pgp');
-          if (checked) {
-              $pgp.show();
-          } else {
-              $pgp.hide();
-              $('#pgp_pubkey').val('');
-          }
-          return true;
-      },
-	  stripeResponseHandler: function(status, response) {
-	      var $form = $('#payment-form');
+            Stripe.createToken($form, creditcardVerifier.stripeResponseHandler);
+            return false;
+        },
+        use_pgp: function (checked) {
+            var $pgp = $('#pgp');
+            if (checked) {
+                $pgp.show();
+            } else {
+                $pgp.hide();
+                $('#pgp_pubkey').val('');
+            }
+            return true;
+        },
+        stripeResponseHandler: function(status, response) {
+            var $form = $('#payment-form');
 
-	      if (response.error) {
-		  // Show the errors on the form
-		  $form.find('.payment-errors').text(response.error.message);
-		  $form.find('button').prop('disabled', false);
-	      } else {
-		  // token contains id, last4, and card type
-		  var token = response.id;
-		  // Insert the token into the form so it gets submitted to the server
-		  $form.append($('<input type="hidden" name="stripeToken">').val(token));
-		  // and submit
-		  $form.get(0).submit();
-	      }
-	  },
-      }
+            if (response.error) {
+                // Show the errors on the form
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('button').prop('disabled', false);
+            } else {
+                // token contains id, last4, and card type
+                var token = response.id;
+                // Insert the token into the form so it gets submitted to the server
+                $form.append($('<input type="hidden" name="stripeToken">').val(token));
+                // and submit
+                $form.get(0).submit();
+            }
+        },
+    }
 }());
 
 $(document).ready(creditcardVerifier.register_callbacks);
