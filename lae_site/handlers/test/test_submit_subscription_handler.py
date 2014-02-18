bdb3bd9b97b539572244c3fd816cd6d8e179f141
zancas <zancas@leastauthority.com>
2014-02-17 23:19:31 -0700

    Complete basic unittest frame

--- lae_site/handlers/test/test_submit_subscription_handler.py
+++ lae_site/handlers/test/test_submit_subscription_handler.py
@@ -1,4 +1,5 @@
-import os, StringIO
+import os
+from StringIO import StringIO
 
 from twisted.internet import defer
 from twisted.trial.unittest import TestCase
@@ -6,8 +7,12 @@ from twisted.python.filepath import FilePath
 
 from lae_util.fileutil import make_dirs
 from lae_site.handlers import submit_subscription
+from lae_util import send_email
 
-MOCKAPIKEY = "sk_live_"+"A"*24 #<-- actual test key
+MOCKAPIKEY = "sk_live_"+"A"*24
+MOCKSMTPPASSWORD = "beef"*4 
+REQUESTARGS = {'stripeToken':"STUB", 'email':"STUB2", 'pgp_pubkey':"STUB3"}
+MOCKFURLFP = "FAKEPATH"
 
 class MockRequest(object):
     def __init__(self, method, d, args):
@@ -32,45 +37,77 @@ class MockFlappCommand(object):
         print >>stdout, "Starting..."
         return defer.succeed(None)
 
+class MockPlan(object):
+    def __init__(self):
+        self.id = "MOCKS4"
+        
+
 class MockSubscription(object):
     def __init__(self):
         self.id = "sub_"+"A"*14
+        self.plan = MockPlan()
 
 class MockCustomer(object):
     def __init__(self):
         self.subscription = MockSubscription()
+        self.email = 'EMAILSTUB'
+        self.id = 'IDSTUB'
 
     @classmethod
-    def create(cls, args):
+    def create(cls, api_key, card, plan, email):
         return MockCustomer()
 
 class MockCardError(Exception):
     def __init__(self, value):
         self.value = value
 
+class MockAPIError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+class MockInvalidRequestError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+class MockTemplate(object):
+    def __init__(self, args):
+        self.args = args
+    def render(self, characters):
+        return "STUB"
+
 class MockEnv(object):
     def __init__(self):
         pass
+    def get_template(self, htmltemplate):
+        return MockTemplate(repr(htmltemplate))
     
 class TestSubmitSubscriptionHandler(TestCase):
     def setUp(self):
         #Patch out environment
-        self.patch(submit_subscription, 'FlappCommand', MockFlappCommand)
+        self.patch(submit_subscription, 'flappcommand', MockFlappCommand(MOCKFURLFP))
         self.patch(submit_subscription.stripe, 'Customer', MockCustomer)
         self.patch(submit_subscription.stripe, 'CardError', MockCardError)
-        self.patch(submit_subscription, 'env', MockEnv)
+        self.patch(submit_subscription.stripe, 'APIError', MockAPIError)
+        self.patch(submit_subscription.stripe, 'InvalidRequestError', MockInvalidRequestError)
+        self.patch(submit_subscription, 'env', MockEnv())
 
         #Create directory for file IO
         temp = self.mktemp()
-        self.basedir = temp.rsplit('/',2)[0]
+        self.basedirfp = FilePath(temp.rsplit('/',2)[0])
         os.rmdir(temp.rsplit('/',1)[0])
 
         #Create mock api key
-        make_dirs(FilePath(self.basedir).child('secret_config').path)
-        FilePath(self.basedir).child('secret_config').child('stripeapikey').setContent(MOCKAPIKEY)
+        make_dirs(self.basedirfp.child('secret_config').path)
+        self.basedirfp.child('secret_config').child('stripeapikey').setContent(MOCKAPIKEY)
+        self.basedirfp.child('secret_config').child('smtppassword').setContent(MOCKSMTPPASSWORD)
+        self.mocksmtppswdpath = self.basedirfp.child('secret_config').child('smtppassword').path
+        self.patch(send_email, 'SMTP_PASSWORD_PATH', self.mocksmtppswdpath)
         
-    def tearDown(self):
-        self.basedir = ''
 
-    def test_foo(self):
-        submit_subscription.stripe.Customer.create('fo')
+    def tearDown(self):
+        self.basedirfp = ''
+
+    def test_render(self):
+        ssubhand_obj = submit_subscription.SubmitSubscriptionHandler(self.basedirfp)
+        mockrequest = MockRequest('POST', defer, REQUESTARGS)
+        ssubhand_obj.render(mockrequest)
