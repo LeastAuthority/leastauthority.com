1c468b4ff4e56fb883b290998e3167784c270fb2
zancas <zancas@leastauthority.com>
2014-02-17 18:51:21 -0700

    Define Appropriate Mocks for render unittest

--- lae_site/handlers/test/test_submit_subscription_handler.py
+++ lae_site/handlers/test/test_submit_subscription_handler.py
@@ -3,11 +3,30 @@ from twisted.trial.unittest import TestCase
 
 from lae_site.handlers import submit_subscription
 
+
+class MockFlappCommand(object):
+    def __init__(self, furlfile):
+        self.should_succeed = True
+    def start(self):
+        return defer.succeed(None)
+    def run(self, content, stdout):
+        print >>stdout, "Starting..."
+        return defer.succeed(None)
+
+class MockStripe(object):
+    def __init__(self):
+        pass
+
+class MockEnv(object):
+    def __init__(self):
+        pass
+
 class TestSubmitSubscriptionHandler(TestCase):
     def setUp(self):
-        fc, stripe, ws_env = submit_subscription.flappcommand, submit_subscription.stripe, submit_subscription.env
-
-        submit_subscription.flappcommand = "MOCKFLAPPCOMMAND"
-        submit_subscription.stripe = "MOCKSTRIPE"
-        submit_subscription.env = "MOCKENVIRONMENT"
-    
+        stripe, ws_env = submit_subscription.stripe, submit_subscription.env
+        self.patch(submit_subscription, 'FlappCommand', MockFlappCommand)
+        self.patch(submit_subscription, 'stripe', MockStripe)
+        self.patch(submit_subscription, 'env', MockEnv)
+        
+    def tearDown(self):
+        pass
