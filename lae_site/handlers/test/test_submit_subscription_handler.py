c5d6d9cf3cd920177f2c4c9098efbf00ddd7eee8
zancas <zancas@leastauthority.com>
2014-02-17 22:16:55 -0700

    Add exception handling to SubmitSubscriptionHandler

--- lae_site/handlers/test/test_submit_subscription_handler.py
+++ lae_site/handlers/test/test_submit_subscription_handler.py
@@ -1,9 +1,13 @@
+import os, StringIO
 
+from twisted.internet import defer
 from twisted.trial.unittest import TestCase
+from twisted.python.filepath import FilePath
 
+from lae_util.fileutil import make_dirs
 from lae_site.handlers import submit_subscription
 
-
+MOCKAPIKEY = "sk_live_"+"A"*24 #<-- actual test key
 
 class MockRequest(object):
     def __init__(self, method, d, args):
@@ -28,20 +32,45 @@ class MockFlappCommand(object):
         print >>stdout, "Starting..."
         return defer.succeed(None)
 
-class MockStripe(object):
+class MockSubscription(object):
     def __init__(self):
-        pass
+        self.id = "sub_"+"A"*14
+
+class MockCustomer(object):
+    def __init__(self):
+        self.subscription = MockSubscription()
+
+    @classmethod
+    def create(cls, args):
+        return MockCustomer()
+
+class MockCardError(Exception):
+    def __init__(self, value):
+        self.value = value
 
 class MockEnv(object):
     def __init__(self):
         pass
-
+    
 class TestSubmitSubscriptionHandler(TestCase):
     def setUp(self):
-        stripe, ws_env = submit_subscription.stripe, submit_subscription.env
+        #Patch out environment
         self.patch(submit_subscription, 'FlappCommand', MockFlappCommand)
-        self.patch(submit_subscription, 'stripe', MockStripe)
+        self.patch(submit_subscription.stripe, 'Customer', MockCustomer)
+        self.patch(submit_subscription.stripe, 'CardError', MockCardError)
         self.patch(submit_subscription, 'env', MockEnv)
+
+        #Create directory for file IO
+        temp = self.mktemp()
+        self.basedir = temp.rsplit('/',2)[0]
+        os.rmdir(temp.rsplit('/',1)[0])
+
+        #Create mock api key
+        make_dirs(FilePath(self.basedir).child('secret_config').path)
+        FilePath(self.basedir).child('secret_config').child('stripeapikey').setContent(MOCKAPIKEY)
         
     def tearDown(self):
-        pass
+        self.basedir = ''
+
+    def test_foo(self):
+        submit_subscription.stripe.Customer.create('fo')
