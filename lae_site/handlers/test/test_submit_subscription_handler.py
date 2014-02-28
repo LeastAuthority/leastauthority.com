35f13193495d1b0eb4c9c42b79064bd7b58b956e
zancas <zancas@leastauthority.com>
2014-02-27 18:15:15 -0700

    Pass first unittest

--- lae_site/handlers/test/test_submit_subscription_handler.py
+++ lae_site/handlers/test/test_submit_subscription_handler.py
@@ -9,15 +9,15 @@ from lae_site.handlers import submit_subscription
 from lae_util import send_email
 from lae_site.handlers.submit_subscription import stripe
 
-
 MOCKAPIKEY = "sk_live_"+"A"*24
-MOCKSMTPPASSWORD = "beef"*4 
-REQUESTARGS = {'stripeToken':"STUB", 'email':"STUB2", 'pgp_pubkey':"STUB3"}
+MOCKSMTPPASSWORD = "beef"*4
+REQUESTARGS = {'stripeToken':["tokenstub"], 'email':["test@test"], 'pgp_pubkey':["testpgppubkey"]}
 MOCKFURLFP = "FAKEPATH"
 
 class MockRequest(object):
     def __init__(self, argdict):
         self.args = argdict
+
 
 class MockFlappCommand(object):
     def __init__(self, furlfile):
@@ -27,40 +27,43 @@ class MockFlappCommand(object):
     def run(self, content, stdout):
         return defer.succeed(None)
 
+
 class MockPlan(object):
     def __init__(self):
         self.id = "MOCKS4"
-        
+
 
 class MockSubscription(object):
     def __init__(self):
         self.id = "sub_"+"A"*14
         self.plan = MockPlan()
 
+
 class MockCustomer(object):
-    def __init__(self, api_key, card, plan, email):
+    def create(self, api_key, card, plan, email):
         self.subscription = MockSubscription()
         self.email = email
         self.id = 'IDSTUB'
         self.init_key = api_key
         self.init_email = email
         self.init_plan = plan
+        return self
 
-    @classmethod
-    def create(cls, api_key, card, plan, email):
-        return MockCustomer(api_key, card, plan, email)
 
 class MockCardError(Exception):
     def __init__(self, value):
         self.value = value
 
+
 class MockAPIError(Exception):
     def __init__(self, value):
         self.value = value
 
+
 class MockInvalidRequestError(Exception):
     def __init__(self, value):
         self.value = value
+
 
 class MockTemplate(object):
     def __init__(self, args):
@@ -68,17 +71,19 @@ class MockTemplate(object):
     def render(self, characters):
         return "STUB"
 
+
 class MockEnv(object):
     def __init__(self):
         pass
     def get_template(self, htmltemplate):
         return MockTemplate(repr(htmltemplate))
-    
+
+
 class TestSubscribedCustomerCreation(TestCase):
     def setUp(self):
         #Patch out environment
         self.patch(submit_subscription, 'flappcommand', MockFlappCommand(MOCKFURLFP))
-        #self.patch(submit_subscription.stripe, 'Customer', MockCustomer)
+        self.patch(submit_subscription.stripe, 'Customer', MockCustomer())
         self.patch(submit_subscription, 'env', MockEnv())
 
         #Create directory for file IO
@@ -92,7 +97,7 @@ class TestSubscribedCustomerCreation(TestCase):
         self.basedirfp.child('secret_config').child('smtppassword').setContent(MOCKSMTPPASSWORD)
         self.mocksmtppswdpath = self.basedirfp.child('secret_config').child('smtppassword').path
         self.patch(send_email, 'SMTP_PASSWORD_PATH', self.mocksmtppswdpath)
-        
+
         self.mc = 'SettingUp'
         self.ssubhand_obj = submit_subscription.SubmitSubscriptionHandler(self.basedirfp)
 
@@ -103,13 +108,16 @@ class TestSubscribedCustomerCreation(TestCase):
 
     def test_successful_customer_creation(self):
         mockrequest = MockRequest(REQUESTARGS)
-        def call_stripe_Customer_create(api_key, stripe_auth_token, plan, user_email):
-            print "api_key: %s" % api_key
-            self.mc = MockCustomer.create(api_key, stripe_auth_token, plan, user_email) 
+        def call_stripe_Customer_create(api_key, card, plan, email):
+            self.mc = MockCustomer()
+            MockCustomer.create(self.mc, api_key, card, plan, email)
             return self.mc
-        
-        self.patch(stripe, 'Customer', MockCustomer)
+
         self.patch(stripe.Customer, 'create', call_stripe_Customer_create)
         self.ssubhand_obj.render(mockrequest)
 
-        #self.failUnlessEqual(self.mc, 'fruitcake')
+        self.failUnless(isinstance(self.mc, MockCustomer))
+        self.failUnlessEqual(self.mc.init_key, 'sk_live_AAAAAAAAAAAAAAAAAAAAAAAA')
+        self.failUnlessEqual(self.mc.init_email, 'test@test')
+        self.failUnlessEqual(self.mc.email, 'test@test')
+        self.failUnlessEqual(self.mc.init_plan, 'S4')
