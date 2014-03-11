d5c63901fcbb90d1ea40d8e9784dcafb8e4c7e52
zancas <zancas@leastauthority.com>
2014-03-11 15:28:00 -0600

    Update all jasmine tests, and js Code Under Test
    This commit is regrettably huge.
    After its application, the JS and associated
    unittests are in the 'current' 2014-03-11
    state.
    
    While the commit itself is like too large to facilitate
    easy review, the associated unittests are quite verbose.
    
    To run the unitests, load:
    
    content/static/js/test/unit/runner.html

--- content/static/js/subscription_signup.js
+++ content/static/js/subscription_signup.js
@@ -1,51 +1,62 @@
 "use strict";
 
 window.creditcardVerifier = (function () {
-      return {
-	  register_callbacks: function () {
-	      var $form = $('#payment-form');
-	      // The form is only visible if js is enabled.
-              $form.show();
-	      // This identifies your website in the createToken call below
-	      Stripe.setPublishableKey('pk_test_IBiTH5UtEo2kB10eb1OSsv0w');
-	      $form.submit( creditcardVerifier.formSubmissionHandler );
-	  },
-	  formSubmissionHandler: function (event) {
-	      var $form = $('#payment-form');
-	      // Disable the submit button to prevent repeated clicks
-              $form.find('button').prop('disabled', true);      
+    return {
+        initialize_page: function () {
+            var $form = $('#payment-form');
+            creditcardVerifier.register_callbacks($form);
+            $('#loading').hide();
+            creditcardVerifier.use_pgp();
+            $form.show();
+        },
+        register_callbacks: function ($form) {
+            // This identifies your website in the createToken call below
+            Stripe.setPublishableKey('pk_test_IBiTH5UtEo2kB10eb1OSsv0w');
+            $form.submit(creditcardVerifier.formSubmissionHandler);
+            $('#use_pgp').click(creditcardVerifier.use_pgp);
+        },
+        formSubmissionHandler: function (event) {
+            var $form = $('#payment-form');
+            // Disable the submit button to prevent repeated clicks
+            $form.find('button').prop('disabled', true);
 
-              Stripe.createToken($form, creditcardVerifier.stripeResponseHandler);
-	      
-	      return false;
-	  },
-	  use_pgp: function (checked) {
-              var $pgp = $('#pgp');
-              if (checked) {
-		  $pgp.show();
-              } else {
-		  $pgp.hide();
-		  $('#pgp_pubkey').val('');
-              }
-              return true;
-	  },
-	  stripeResponseHandler: function(status, response) {
-	      var $form = $('#payment-form');
+            Stripe.createToken($form, creditcardVerifier.stripeResponseHandler);
+            return false;
+        },
+        use_pgp: function () {
+            if ($('#use_pgp').prop('checked')) {
+                $('#pgp').show();
+            } else {
+                $('#pgp').hide();
+            }
+            return true;
+        },
+        stripeResponseHandler: function(status, response) {
+            var $form = $('#payment-form');
 
-	      if (response.error) {
-		  // Show the errors on the form
-		  $form.find('.payment-errors').text(response.error.message);
-		  $form.find('button').prop('disabled', false);
-	      } else {
-		  // token contains id, last4, and card type
-		  var token = response.id;
-		  // Insert the token into the form so it gets submitted to the server
-		  $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-		  // and submit
-		  $form.get(0).submit();
-	      }
-	  },
-      }
+            if (response.error) {
+                // Show the errors on the form
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('button').prop('disabled', false);
+            } else {
+                // Copy from pgp_pubkey_textarea to a hidden pgp_pubkey input field.
+                // We do this because input fields can't be multiline, but textareas can.
+                var pgp_pubkey = '';
+                if ($('#use_pgp').prop('checked')) {
+                    pgp_pubkey = $('#pgp_pubkey_textarea').val();  // see http://api.jquery.com/val/
+                }
+                $form.append($('<input type="hidden" name="pgp_pubkey">').val(pgp_pubkey));
+                $('#use_pgp').prop('disabled', true);
+
+                // token contains id, last4, and card type
+                var token = response.id;
+                // Insert the token into the form so it gets submitted to the server
+                $form.append($('<input type="hidden" name="stripeToken">').val(token));
+                // and submit
+                $form.get(0).submit();
+            }
+        }
+    }
 }());
 
-$(document).ready(creditcardVerifier.register_callbacks);
+$(document).ready(creditcardVerifier.initialize_page);
