7a2fa631014feeda0975a11d537a8198cf42fb34
zancas <zancas@leastauthority.com>
2014-03-11 21:01:12 -0600

    Remove command line secret leakage.

--- create_stripe_bucket.py
+++ create_stripe_bucket.py
@@ -4,24 +4,20 @@ import os, sys
 
 from twisted.internet import reactor
 from twisted.python.failure import Failure
+from twisted.python.filepath import FilePath
 
+from lae_automation.config import Config
 from lae_automation.initialize import create_stripe_user_bucket
 
-
-if len(sys.argv) < 4:
-    print "Usage: python create_stripe_user_bucket.py USER_ACCESS_KEY_ID USER_SECRET_KEY BUCKET_NAME [LOCATION]"
+if len(sys.argv) != 2:
+    print "Usage: python create_stripe_user_bucket.py BUCKETNAME"
     print "Happy bucket-creating!"
     sys.exit(1)
 
-useraccesskeyid = sys.argv[1]
-usersecretkey = sys.argv[2]
-bucketname = sys.argv[3]
-
-
-if len(sys.argv) > 4:
-    location = sys.argv[4]
-else:
-    location = None  # default
+conf = Config()
+useraccesskeyid = conf.other['ec2_access_key_id']
+usersecretkey = FilePath(conf.other['ec2_secret_path']).getContent().rstrip()
+bucketname = sys.argv[1]
 
 def cb(x):
     print str(x)
@@ -29,7 +25,7 @@ def cb(x):
         print x.value.response
 
 d = create_stripe_user_bucket(useraccesskeyid, usersecretkey, bucketname, sys.stdout, sys.stderr,
-                              location=location)
+                              location='http://s3.amazonaws.com')
 d.addBoth(cb)
 d.addCallbacks(lambda ign: os._exit(0), lambda ign: os._exit(1))
 reactor.run()
