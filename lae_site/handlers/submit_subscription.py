33432eacb4a68ac0205a31de3f668c5a20e38165
zancas <zancas@leastauthority.com>
2014-03-11 18:25:21 -0600

    Add functionality for handling stripe messages
    NOTE: submission of data from the User's
    browser to stripe is handled by the previously
    committed "subscription_signup.js".  Unittests
    for that code are also in an ancestral commit.
    
    Note: non-solvent payment credentials, cause stripe servers
    to send a message to the browser.
    Note: invalid submission data, e.g. wrong type of CC, is
    handled in browser by our html/and-or stripe.js.
    
    This handler is invoked when:
    (1) The User's payment (cc) information has been
    processed by stripe.
    (2) That information is valid
        i.  valid-and-solvent number, CVC, exp date
    (3) Stripe has responded to the requested processing.
    
    Handler functionality:
    1. The handler loads our secret stripe api key, and
    2. attempts to charge the User's cc (referenced by a unique stripe token), for
    a subscription, to our "S4" plan at stripe.
    
    3a. handles several possible exceptions generated by stripe.
    3b. In the case of a confirmation from stripe that subscription succeeded,
    i.e. 'the card was charged', the handler:
       I.  logs a successful subscription on the webserver fs
       II. invokes the full_signup service provide by the flappserver service
    
    4. In all cases renders an appropriate webpage and returns it to the
    twisted web-server for subsequent transmission to the User's browser.
    
    Also included are incomplete unittests for the above functionality.

--- lae_site/handlers/submit_subscription.py
+++ lae_site/handlers/submit_subscription.py
@@ -4,6 +4,7 @@ import stripe, traceback, simplejson, sys
 from twisted.python.filepath import FilePath
 
 from lae_util.servers import append_record
+from lae_util.send_email import send_plain_email, FROM_ADDRESS
 
 from lae_site.handlers.web import env
 from lae_util.flapp import FlappCommand
@@ -23,47 +24,68 @@ def start(basefp):
     flappcommand = FlappCommand(signup_furl_fp.path)
     return flappcommand.start()
 
-class SubmitSubscriptionHandler(HandlerBase):
 
+class SubmitSubscriptionHandler(HandlerBase):
     def __init__(self, basefp):
         HandlerBase.__init__(self, out=None)
         self._logger_helper(__name__)
         self.basefp = basefp
 
-    def render(self, request):
-        """
-        The expected HTTP method is a POST from the <form> in templates/subscription_signup.html. 
-        render_POST is handled by the HandlerBase parent which calls this this method after logging 
-        the request.
-
-        The foolscap service registered to run when flappcommand.run is called expects a bytestream
-        of US-ascii valid bytes, because it is reading from its stdin (--accept-stdin flag set upon 
-        addition).  Therefore the content passed to the command must conform to US-ascii.
-        """
-        #Parse request, info from stripe and subscriber
-        stripe_authorization_token = self.get_arg(request, 'stripeToken')
-        email_from_form = self.get_arg(request, 'email')
-
-        #Load apikey
+    #The following helper methods are all called directly-or-indirectly by render.
+    def get_stripe_api_key(self):
         stripefp = FilePath(self.basefp.path).child('secret_config').child('stripeapikey')
-        assert (('leastauthority.com' not in stripefp.path) or ('_trial_temp' in stripefp.path)), "secrets must not be in production code repo"
-        stripe_api_key = stripefp.getContent().strip()
+        if ('leastauthority.com' in stripefp.path) and ('_trial_temp' not in stripefp.path):
+            raise AssertionError("Secrets are not allowed in the production code repo: %r" % (stripefp.path,))
+        return stripefp.getContent().strip()
 
-        #invoke cc-charge by requesting subscription to recurring-payment plan
+    def get_creation_parameters(self, request):
+        # Load apikey.
+        stripe_api_key = self.get_stripe_api_key()
+        # Parse request, info from stripe and subscriber.
+        stripe_authorization_token = self.get_arg(request, 'stripeToken')
+        user_email = self.get_arg(request, 'email')
+        return stripe_api_key, stripe_authorization_token, user_email
+
+    def create_cust_errhandler(self, trace_back, error, details, email_subject):
+        error.details = details
+        print >>self.out, "Got %s from the stripe.Customer.create call:" % (error.__class__.__name__,)
+        print >>self.out, trace_back
+        headers = {
+            "From": FROM_ADDRESS,
+            "Subject": email_subject,
+        }
+        send_plain_email('info@leastauthority.com', 'support@leastauthority.com', trace_back, headers)
+        raise error
+        
+    def create_customer(self, stripe_api_key, stripe_authorization_token, user_email):
         try:
-            customer = stripe.Customer.create(api_key=stripe_api_key, card=stripe_authorization_token, plan='S4', email=email_from_form)
-        except stripe.CardError, e:
-            print >>self.out, "Got an exception from the stripe.Customer.create call:"
-            print >>self.out, dir(e)
-            print >>self.out, repr(e)
-            tmpl = env.get_template('subscription_signup.html')
-            return tmpl.render({"errorblock": e.message}).encode('utf-8', 'replace')
+            return stripe.Customer.create(api_key=stripe_api_key, card=stripe_authorization_token, plan='S4', 
+                                          email=user_email)
+        except stripe.CardError as e:
+            # Errors we expect: https://stripe.com/docs/api#errors
+            self.create_cust_errhandler(traceback.format_exc(100), e,
+                                        details=e.message,
+                                        email_subject="Stripe Card error")
+        except stripe.APIError as e:
+            self.create_cust_errhandler(traceback.format_exc(100), e,
+                                        details="Our payment processor is temporarily unavailable,"+\
+                                            " please try again in\ a few moments.",
+                                        email_subject="Stripe API error")
+        except stripe.InvalidRequestError as e:
+            self.create_cust_errhandler(traceback.format_exc(100), e,
+                                        details="Due to technical difficulties unrelated to your card"+\
+                                            " details, we were unable to charge your account. Our"+\
+                                            " engineers have been notified and will contact you with"+\
+                                            " an update shortly.",
+                                        email_subject="Stripe Invalid Request error")
+        except Exception as e:
+            self.create_cust_errhandler(traceback.format_exc(100), e,
+                                        details="Something went wrong. Please try again, or contact"+\
+                                            " <support@leastauthority.com>.",
+                                        email_subject="Stripe unexpected error")
 
-        #log that a new subscription has been created (at stripe)
-        subscriptions_fp = self.basefp.child(SUBSCRIPTIONS_FILE)
-        append_record(subscriptions_fp, customer.subscription.id)       
-
-        def when_done(ignored_None):
+    def run_full_signup(self, customer, request):
+        def when_done(ign):
             service_confirmed_fp = self.basefp.child(SERVICE_CONFIRMED_FILE)
             try:
                 append_record(service_confirmed_fp, customer.subscription.id)
@@ -71,13 +93,21 @@ class SubmitSubscriptionHandler(HandlerBase):
                 # The request really did succeed, we just failed to record that it did. Log the error locally.
                 traceback.print_exc(100, sys.stderr)
 
-        def when_failed(ignored_None):
+        def when_failed(ign):
             try:
-                pass  #XXX  Inform operations that a subscribed customer has a broken service!!
+                headers = {
+                    "From": FROM_ADDRESS,
+                    "Subject": "Sign-up error",
+                }
+                send_plain_email('info@leastauthority.com', 'support@leastauthority.com',
+                                 "A sign-up failed for <%s>." % (customer.email,), headers)
             except Exception:
                 traceback.print_exc(100, sys.stderr)
 
         customer_pgpinfo = self.get_arg(request, 'pgp_pubkey')
+        # The foolscap service registered to run when flappcommand.run is called expects a bytestream
+        # of US-ASCII bytes, because it is reading from its stdin (--accept-stdin flag set).
+        # Therefore the content passed to the command must conform to US-ASCII.
         stdin = simplejson.dumps((customer.email,
                                   customer_pgpinfo,
                                   customer.id,
@@ -90,5 +120,25 @@ class SubmitSubscriptionHandler(HandlerBase):
         d.addCallback(when_done)
         d.addErrback(when_failed)
 
+    def render(self, request):
+        # The expected HTTP method is a POST from the <form> in templates/subscription_signup.html.
+        # render_POST is handled by the HandlerBase parent which calls this this method after logging
+        # the request.
+
+        # Get information needed to create the new stripe subscription to the S4 plan
+        stripe_api_key, stripe_authorization_token, user_email = self.get_creation_parameters(request)
+        try:
+            # Invoke card charge by requesting subscription to recurring-payment plan.
+            customer = self.create_customer(stripe_api_key, stripe_authorization_token, user_email)
+        except Exception as e:
+            tmpl = env.get_template('s4-subscription-form.html')
+            return tmpl.render({"errorblock": e.details}).encode('utf-8', 'replace')
+
+        # Log that a new subscription has been created (at stripe).
+        subscriptions_fp = self.basefp.child(SUBSCRIPTIONS_FILE)
+        append_record(subscriptions_fp, customer.subscription.id)
+        # Initiate the provisioning service
+        self.run_full_signup(customer, request)
+        # Return the a page notifying the user that they've been charged and their service is being set up.
         tmpl = env.get_template('payment_verified.html')
-        return tmpl.render({"productfullname":"Simple Secure Storage Service", "productname":"S4"}).encode('utf-8')
+        return tmpl.render({"productfullname": "Simple Secure Storage Service", "productname":"S4"}).encode('utf-8')
