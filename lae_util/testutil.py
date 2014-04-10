603b1c0629d84433876db95fa34342f1a7297fb3
Daira Hopwood <daira@leastauthority.com>
2014-04-10 16:14:46 +0100

    Simplify create_workdir.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_util/testutil.py
+++ lae_util/testutil.py
@@ -6,9 +6,9 @@ from twisted.python.filepath import FilePath
 
 class TestUtilitiesMixin:
     def create_workdir(self):
-        tempdir = os.path.join(self.__class__.__module__, self.__class__.__name__,
-                               self.shortDescription())
-        filepath_of_tempdir = FilePath(tempdir)
-        filepath_of_tempdir.makedirs()
-        return filepath_of_tempdir
+        tempdirfp = (FilePath(self.__class__.__module__)
+                       .child(self.__class__.__name__)
+                       .child(self.shortDescription()))
+        tempdirfp.makedirs()
+        return tempdirfp
 
