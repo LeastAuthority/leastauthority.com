f7bf9370943501eedf7c9b498d40e3afcdc59898
Daira Hopwood <daira@leastauthority.com>
2014-04-18 16:18:02 +0100

    Strip DevPay remnants from emailcollector.py.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_site/handlers/emailcollector.py
+++ lae_site/handlers/emailcollector.py
@@ -10,234 +10,13 @@ from lae_util.send_email import send_plain_email, FROM_EMAIL, FROM_ADDRESS
 from lae_site.handlers.web import env
 from lae_site.handlers.main import HandlerBase
 
-EMAILS_FILE              = 'emails.csv'
-DEVPAY_COMPLETIONS_FILE  = 'devpay_completions.csv'
-ACTIVATION_REQUESTS_FILE = 'activation_requests.csv'
-SIGNUPS_FILE             = 'signups.csv'
-SIGNUP_FURL_FILE         = 'signup.furl'
-
+EMAILS_FILE = 'emails.csv'
 VALID_EMAIL_RE = re.compile("[^@]+@[^@]+")
-
 ACTIVE_PRODUCTS = set(['s4'])
-SIGNUP_BASE_URL = "https://leastauthority.com/signup/"
-
-def html(title, body):
-    return """<!DOCTYPE HTML>
-<html>
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <link href="/static/img/icon.png" rel="shortcut icon">
-  <title>%s</title>
-</head>
-<body style="background: #FFFFFF">%s""" % (title, body)
-    return ""
-
-
-ACTIVATION_FORM_HTML = """<p>
-The activation of your account is not quite finished; to complete it,
-please tell us the name and email address you want us to use to communicate
-with you. We will not resell or pass on this email address, and will only use it
-in connection with your account, and to send you occasional announcements of new
-products, features, offers and price changes.
-</p>
-<p>
-If you use PGP or GPG, you could also send us your public key, so that we
-can encrypt our emails to you and verify signed emails from you.
-(Your fingerprint is enough if your key is available from the usual keyservers.)
-This helps to protect you from phishing attempts -- although Tahoe-LAFS'
-provider-independent security means that you don't need to tell us any secrets anyway!
-</p>
-<div style="width:45em">
-<form action="/activation-request" method="post" enctype="multipart/form-data">
-<table>
-  <tr>
-    <td style="width:25em"><label for="Name">Name you would like us to call you by: </label></td>
-    <td><input type="text" id="Name" name="Name" style="width:20em"></td>
-  </tr>
-  <tr>
-    <td style="width:25em"><label for="Email">Email address: </label></td>
-    <td><input type="text" id="Email" name="Email" style="width:20em"><td>
-  </tr>
-  <tr>
-    <td style="width:25em"><label for="ActivationKey">Activation Key: </label></td>
-    <td><input type="text" id="ActivationKey" name="ActivationKey" value="%(activationkey)s" style="width:20em"></td>
-  </tr>
-  <tr>
-    <td style="width:25em"><label for="ProductName">Product: </label></td>
-    <td><input type="text" id="ProductName" name="ProductName" value="%(productfullname)s" disabled style="width:20em">
-        <input type="hidden" name="ProductCode" value="%(productcode)s"></td>
-  </tr>
-  <tr>
-    <td style="width:25em"><label for="PublicKey">Your OpenPGP public key or fingerprint, optional: </label></td>
-    <td><input type="text" id="PublicKey" name="PublicKey" style="width:20em; height:12ex"></td>
-  </tr>
-  <tr>
-    <td><input type="submit" value="Request activation"></td>
-  </tr>
-</table>
-</form>
-</div>
-<p>
-If this form does not work for any reason, you can also send the same information
-by email to <a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-<p>
-Happy Simple Secure Storing!
-</p>
-<p>
-The Least Authority Enterprises team (Zooko, Daira, Za, Leif, and Nathan)
-</p>
-<hr>
-</body>
-</html>
-"""
-
-# TODO: allow customizing the text per-product.
-DEVPAY_RESPONSE_HAVE_CODES_HTML = html("Activation for Simple Secure Storage Service", """
-<p>
-Thank you for starting to activate your Simple Secure Storage Service!
-</p>
-""" + ACTIVATION_FORM_HTML)
-
-# Amazon sometimes doesn't tell us the product code and/or activation key.
-# In that case, the easiest way to get those codes is for the user to click on
-# the 'Go to Application' link, so tell them to do that, rather than requiring
-# them to paste in the code(s).
-DEVPAY_RESPONSE_MISSING_CODE_HTML = html("Activation for Simple Secure Storage Service", """
-<p>
-Thank you for starting to activate your Simple Secure Storage Service!
-</p>
-<p>
-Please follow the link above that says "click here to get activation
-keys". It will take you to the next page. On that page you do not need to do
-the <b>Generate Key</b> step &mdash; just go ahead and click the <b>Go to
-Application</b> link for this subscription.
-</p>
-<p>
-If you have any problem completing the sign-up, please contact
-<a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-<p>
-Happy Simple Secure Storing!
-</p>
-<p>
-The Least Authority Enterprises team (Zooko, Daira, Za, Leif, and Nathan)
-</p>
-<hr>
-</body>
-</html>
-""")
-
-ACTIVATIONREQ_RESPONSE_NORMAL_HTML = html("Activation requested", """
-<p>
-Your activation request has been received.
-</p>
-<p>
-When your account is activated, we will send you a confirmation email with
-instructions on how to start using Simple Secure Storage Service, or alternatively
-an email requesting any information that was missing. If you don't receive this
-within an hour (also check your spam folder in case it is there), please contact
-<a href="mailto:support@LeastAuthority.com">&lt;support@LeastAuthority.com&gt</a>.
-</p>
-<p>
-Thanks again for signing up.
-</p>
-<p>
-The Least Authority Enterprises team (Zooko, Daira, Za, Leif, and Nathan)
-</p>
-<hr>
-<p>
-The progress of your server setup is shown below. You do not need to read and
-understand it -- it is shown solely to let you know that progress is being
-made. Once the server setup is complete, an email will be sent to you with
-all the information you need to know.
-</p>
-<p>
-Please don't reload this page, since the activation key is
-only valid once.
-</p>
-<pre>
-""")
-
-ACTIVATIONREQ_RESPONSE_MISSING_KEY_HTML = html("Missing activation key or product code", """
-<p>
-The activation key or product code was missing from the request. This can happen in some cases
-if you reload the page after an activation request. If you are having any difficulty signing up,
-please contact <a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-</p>
-<pre>
-""")
-
-ACTIVATIONREQ_RESPONSE_MISSING_NAME_HTML = html("Missing name", """
-<p style="color: red;">
-The 'Name' field was not filled in. All fields except for the PGP key information
-are required. If you are having any difficulty signing up, please contact
-<a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-</p>
-<hr>
-""" + ACTIVATION_FORM_HTML)
-
-ACTIVATIONREQ_RESPONSE_MISSING_OR_INVALID_EMAIL_HTML = html("Missing or invalid email address", """
-<p style="color: red;">
-The 'Email address' field was not filled in with a valid-looking address. All fields except for
-the PGP key information are required. If you are having any difficulty signing up, please contact
-<a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-</p>
-<hr>
-""" + ACTIVATION_FORM_HTML)
-
-ACTIVATIONREQ_RESPONSE_ALREADY_SUCCEEDED_HTML = html("Activation already succeeded", """
-<p>
-This activation key has already been used in a successful sign-up. If you have not received
-the confirmation e-mail, please contact
-<a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-</p>
-<hr>
-</body>
-</html>
-""")
-
-ACTIVATIONREQ_RESPONSE_ALREADY_USED_HTML = html("Activation key already used", """
-<p>
-This activation key has already been used. If you have not yet received any e-mail
-about your sign-up, please contact
-<a href="mailto:support@leastauthority.com">&lt;support@leastauthority.com&gt</a>.
-</p>
-<hr>
-</body>
-</html>
-""")
-
-SUCCEEDED_HTML = """</pre>
-<p>
-Your Simple Secure Storage Service has been activated!
-</p>
-<p>
-Please go to
-<a href="https://leastauthority.com/howtoconfigure" target="_blank">https://leastauthority.com/howtoconfigure</a>
-for instructions on how to set up and configure your Tahoe-LAFS gateway
-(using the <tt>introducer.furl</tt> printed above) and start using the service.
-</p>
-<p>
-You will also shortly get a confirmation email at the address you provided.
-</p>
-<hr>
-</body>
-</html>
-"""
-
-FAILED_HTML = """</pre>
-<p>
-We weren't able to complete your account activation automatically, but don't worry,
-we'll finish it manually if possible, and email you when that is done or if we need
-more information.
-</p>
-<hr>
-</body>
-</html>
-"""
 
 AUTOREPLY_EMAIL_SUBJECT = "Thank you for your interest in %(full_product_name)s"
 
-AUTOREPLY_NOT_READY_EMAIL_BODY = """Hello,
+AUTOREPLY_EMAIL_BODY = """Hello,
 
 We've received your request to sign up for %(full_product_name)s.
 When we're ready to sign you up, we'll send another email.
@@ -251,54 +30,34 @@ Thanks for your patience.
 The Least Authority Enterprises team
 """
 
-AUTOREPLY_READY_EMAIL_BODY = """Hello,
-
-We've received your request to sign up for %(full_product_name)s.
-
-If you haven't already done so, please go to %(signup_url)s
-to confirm your sign up and payment details with Amazon Payments, and start
-the activation process.
-
-If you have any questions, please email them to <support@LeastAuthority.com>.
-
-Thanks!
-
---\x20
-The Least Authority Enterprises team
-"""
-
 
 class CollectEmailHandler(HandlerBase):
-    def __init__(self, basefp, products, out=None):
+    def __init__(self, basefp, out=None):
         HandlerBase.__init__(self, out=out)
         self._logger_helper(__name__)
         self.basefp = basefp
-        self.products = products
 
     def render(self, request):
         print >>self.out, "Yay, another potential customer:", request.args
-        email = self.get_arg(request, 'Email')
         productname = self.get_arg(request, 'ProductName')
-        productfullname = self.get_arg(request, 'ProductFullName')
 
         if productname in ACTIVE_PRODUCTS:
-            signup_url = SIGNUP_BASE_URL + productname
-            valid_email_template = 'valid_email_activeproduct.html'
-        else:
-            signup_url = None
-            valid_email_template = 'valid_email_inactiveproduct.html'
+            raise AssertionError("we shouldn't get here for an active product: %r", (productname,))
+
+        email = self.get_arg(request, 'Email')
+        productfullname = self.get_arg(request, 'ProductFullName')
 
         append_record(self.basefp.child(EMAILS_FILE), email, productname)
 
         request.setResponseCode(200)
 
         if email and VALID_EMAIL_RE.match(email):
-            tmpl = env.get_template(valid_email_template)
+            tmpl = env.get_template('valid_email.html')
             (start, _, rest) = tmpl.render(productname=productname, productfullname=productfullname, email=email).encode('utf-8').partition("MAGIC")
             request.write(start)
 
             d = defer.succeed(None)
-            d.addCallback(lambda ign: send_autoreply(email, productfullname, signup_url))
+            d.addCallback(lambda ign: send_autoreply(email, productfullname))
             def _sent(ign):
                 request.write("We've just sent you an email to check that you can receive email from us.")
                 request.write(rest)
@@ -319,19 +78,10 @@ class CollectEmailHandler(HandlerBase):
             return tmpl.render(productname=productname, productfullname=productfullname).encode('utf-8')
 
 
-def send_autoreply(customer_email, full_product_name, signup_url):
-    # TODO: the name is URL-escaped UTF-8. It should be OK to unescape it since the email is plain text,
-    # but I'm being cautious for now since I haven't reviewed email.mime.text.MIMEText to make sure that's safe.
-    if signup_url is None:
-        content = AUTOREPLY_NOT_READY_EMAIL_BODY % {
-                   "full_product_name": full_product_name,
-                  }
-    else:
-        content = AUTOREPLY_READY_EMAIL_BODY % {
-                   "full_product_name": full_product_name,
-                   "signup_url": signup_url,
-                  }
-
+def send_autoreply(customer_email, full_product_name):
+    content = AUTOREPLY_EMAIL_BODY % {
+                "full_product_name": full_product_name,
+              }
     headers = {
                "From": FROM_ADDRESS,
                "Subject": AUTOREPLY_EMAIL_SUBJECT % {"full_product_name": full_product_name},
