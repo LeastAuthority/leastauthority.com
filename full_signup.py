7a8287536a376003b8a4855a9b0d0e4f204e6ed7
zancas <zancas@leastauthority.com>
2014-04-25 15:34:25 -0600

    whitespace cleanup

--- full_signup.py
+++ full_signup.py
@@ -18,10 +18,11 @@ def main(stdin, flapp_stdout, flapp_stderr):
      plan_id,
      subscription_id) = simplejson.loads(stdin.read())
 
-    abslogdir_fp, stripesecrets_log_fp, SSEC2secrets_log_fp, signup_log_fp = \
-        create_log_filepaths(plan_id,
-                             customer_id, 
-                             subscription_id)
+    (abslogdir_fp,
+    stripesecrets_log_fp,
+    SSEC2secrets_log_fp,
+    signup_log_fp) = create_log_filepaths(plan_id, customer_id, subscription_id)
+
     append_record(flapp_stdout, "Writing logs to %r." % (abslogdir_fp.path,))
 
     stripesecrets_log_fp.setContent(simplejson.dumps({
@@ -44,10 +45,10 @@ def main(stdin, flapp_stdout, flapp_stderr):
         return err
 
     d = defer.succeed(None)
-    d.addCallback(lambda ign: activate_subscribed_service(customer_email, customer_pgpinfo, 
-                                                          customer_id, subscription_id, 
-                                                          plan_id,  
-                                                          signup_stdout, signup_stderr, 
+    d.addCallback(lambda ign: activate_subscribed_service(customer_email, customer_pgpinfo,
+                                                          customer_id, subscription_id,
+                                                          plan_id,
+                                                          signup_stdout, signup_stderr,
                                                           SSEC2_secretsfile, signup_log_fp.path)
                   )
     d.addErrback(errhandler)
@@ -62,7 +63,7 @@ if __name__ == '__main__':
         make_dirs(logDir.path)
     flapp_stdout = logDir.child('stdout')
     flapp_stderr = logDir.child('stderr')
-    
+
     d = defer.succeed(None)
     d.addCallback(lambda ign: main(stdin, flapp_stdout, flapp_stderr))
     def _print_except(f):
