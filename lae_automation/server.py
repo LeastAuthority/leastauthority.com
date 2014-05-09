f458c8bc8cd65b723ea58c86ac762e4b5a499539
Daira Hopwood <daira@leastauthority.com>
2014-05-09 19:25:16 +0100

    Logging in as monitor with the admin key doesn't work.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_automation/server.py
+++ lae_automation/server.py
@@ -372,7 +372,7 @@ def update_tahoe(publichost, admin_privkey_path, stdout, stderr, do_update_txaws
 
 
 def delete_statmover_emissions(publichost, admin_privkey_path, stdout, stderr):
-    set_host_and_key(publichost, admin_privkey_path, username="monitor")
+    set_host_and_key(publichost, admin_privkey_path)
     sudo('rm -rf /home/monitor/statmover/emissions')
 
     set_host_and_key(publichost, admin_privkey_path, username="customer")
