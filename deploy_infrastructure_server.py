87848b33e2e492b0ba9b0dd4e1f711294748e262
Daira Hopwood <daira@leastauthority.com>
2014-05-19 15:10:45 +0100

    Remove lae_infrastructure_config.json. Rationale: the paths it specifies are likely to differ between systems.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- deploy_infrastructure_server.py
+++ deploy_infrastructure_server.py
@@ -9,18 +9,40 @@
 (e.g. instance name) are parsed from the lae_automation_config.json configuration file.
 """
 
-import os, sys, argparse, simplejson
+import os, sys, argparse
 from twisted.python.filepath import FilePath
 from lae_automation.initialize import deploy_infrastructure_EC2
 from lae_automation.signup import EC2_ENDPOINT
 from lae_automation.server import install_infrastructure_server
 from twisted.internet import defer
 
+
+DEFAULT_AMI_IMAGE_ID = 'ami-7fe7fe16'
+DEFAULT_INSTANCE_SIZE = 'm1.small'
+
 parser = argparse.ArgumentParser(description=
-                                 "Deploy a new infrastructure server. Each repository-and-reference "
-                                 "(e.g. leastauthority.com-and-SHA1) is specified as a path to the "
-                                 "repository followed by a reference to the specific commit to be "
-                                 "deployed.")
+                    "Deploy a new infrastructure server. Each repository-and-reference "
+                    "(e.g. leastauthority.com-and-SHA1) is specified as a path to the "
+                    "repository followed by a reference to the specific commit to be "
+                    "deployed.")
+
+parser.add_argument("ec2_secret_path", help=
+                    "The absolute path to the EC2 provisioning secret that authorizes deployment "
+                    "of the infrastructure server.")
+parser.add_argument("ec2_accesskeyid_path", help=
+                    "The absolute path to the EC2 access key id file.")
+
+parser.add_argument("--ami_image_id", default=DEFAULT_AMI_IMAGE_ID, help=
+                    "The AMI image ID to use for the new server (default: %r)." % (DEFAULT_AMI_IMAGE_ID,))
+parser.add_argument("--instance_size", default=DEFAULT_INSTANCE_SIZE, help=
+                    "The instance size to use for a new server (default: %r)." % (DEFAULT_INSTANCE_SIZE,))
+
+parser.add_argument("admin_keypair_name", help=
+                    "The name of the keypair (as configured in the AWS account) for the new server. "
+                    "Unused if --existing_host is specified.")
+parser.add_argument("admin_privkey_path", help=
+                    "The path to the private key (.pem) file of the keypair for the new server. "
+                    "Unused if --existing_host is specified.")
 
 parser.add_argument("leastauthority_repo_path", help=
                     "The absolute path to the git repository containing the leastauthority.com "
@@ -38,11 +60,18 @@ parser.add_argument("secret_config_commit_ref", help=
 
 exc_group = parser.add_mutually_exclusive_group()
 exc_group.add_argument('--existing_host', type=str)
-exc_group.add_argument('--new_host', type=str, help="How this instance is referred to e.g. via AWS console.")
+exc_group.add_argument('--new_host', type=str, help=
+                       "How this instance is referred to e.g. via AWS console.")
 
 args = parser.parse_args()
 print "args: %r" % (args,)
 
+ec2_secret_path = args.ec2_secret_path
+ec2_accesskeyid_path = args.ec2_accesskeyid_path
+ami_image_id = args.ami_image_id
+instance_size = args.instance_size
+admin_keypair_name = args.admin_keypair_name
+admin_privkey_path = args.admin_privkey_path
 leastauthority_repo_path = args.leastauthority_repo_path
 leastauthority_commit_ref = args.leastauthority_commit_ref
 secret_config_repo_path = args.secret_config_repo_path
@@ -50,30 +79,14 @@ secret_config_commit_ref = args.secret_config_commit_ref
 existing_host = args.existing_host
 new_host = args.new_host
 
-# XXX configpath is a function of the secret_config repo specified in the arguments!
-
-configpath='../secret_config/lae_infrastructure_config.json'
-
-config = simplejson.loads(FilePath(configpath).getContent())
-
-#Configuration copied from most recent product
-#https://en.wikipedia.org/wiki/Amazon_Machine_Image
-ami_image_id = config['ami_image_id'] # https://cloud-images.ubuntu.com/locator/ec2/ COMMENT TO JSON
-instance_size = config['instance_size']
-ec2accesskeyid = config['ec2_access_key_id']
-keypair_name = config['admin_keypair_name']
-admin_privkey_path = config['admin_privkey_path']
-ec2secretpath = config['ec2_secret_path']
 endpoint_uri = EC2_ENDPOINT
 bucket_name = 'dummy'
 website_pubkey = None
 stdout = sys.stdout
 stderr = sys.stderr
 
-#Configuration which is specific to the test account
-
-
-ec2secretkey = FilePath(ec2secretpath).getContent().strip()
+ec2_accesskeyid = FilePath(ec2_accesskeyid_path).getContent().strip()
+ec2_secret      = FilePath(ec2_secret_path).getContent().strip()
 
 def printer(x):
     """This handy function let's us see what's going on between calls in the callback chain."""
@@ -87,15 +100,14 @@ def eb(x):
 
 if existing_host:
     d = defer.succeed(install_infrastructure_server(
-                                  existing_host,
-                                  admin_privkey_path, website_pubkey,
+                                  existing_host, admin_privkey_path, website_pubkey,
                                   leastauthority_repo_path, leastauthority_commit_ref,
                                   secret_config_repo_path, secret_config_commit_ref,
                                   stdout, stderr))
 elif new_host:
-    d = deploy_infrastructure_EC2(ec2accesskeyid, ec2secretkey, endpoint_uri,
+    d = deploy_infrastructure_EC2(ec2_accesskeyid, ec2_secret, endpoint_uri,
                                   ami_image_id, instance_size, bucket_name,
-                                  keypair_name, new_host,
+                                  admin_keypair_name, new_host,
                                   admin_privkey_path, website_pubkey,
                                   leastauthority_repo_path, leastauthority_commit_ref,
                                   secret_config_repo_path, secret_config_commit_ref,
