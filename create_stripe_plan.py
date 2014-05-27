52f9648f18df720192146dd40bed4e1543876a3f
Zooko Wilcox-O'Hearn <zooko@zooko.com>
2014-05-27 19:41:05 +0000

    change id, add trial period 30 days
    also make all strings of text unicodes, capitalize ISO currency code, and add a
    statement_description, and change the name.

--- create_stripe_plan.py
+++ create_stripe_plan.py
@@ -5,7 +5,10 @@ from twisted.python.filepath import FilePath
 stripe.api_key = FilePath.getContent('../secret_config/stripeapikey').strip()
 
 stripe.Plan.create(amount=2500,
-                   interval='month',
-                   name='S4',
-                   currency='usd',
-                   id='S4_01')
+                   interval=u'month',
+                   name=u'LeastAuthority Simple Secure Storage Service (S4)',
+                   currency=u'USD',
+                   id=u'S4_consumer_iteration_2_beta1_2014-05-27',
+                   trial_period_days=30,
+                   statement_description=u'S4',
+                   )
