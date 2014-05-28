20ecc107d74e050e0f9aa112d1a8049b612edc07
zancas <zancas@leastauthority.com>
2014-05-28 14:49:04 -0600

    update plan parameters, per Zooko's suggestions

--- create_stripe_plan.py
+++ create_stripe_plan.py
@@ -7,16 +7,18 @@ from lae_automation.config import Config
 config = Config()
 
 stripe.api_key = FilePath.getContent('../secret_config/stripeapikey').strip()
-amount = config.products[0]["amount"]
+amount = int(config.products[0]["amount"])
 interval = config.products[0]["interval"]
 currency = config.products[0]["currency"]
 name = config.products[0]["plan_name"]
 plan_id = config.products[0]["plan_ID"]
 trial_period_days = int(config.products[0]["plan_trial_period_days"])
+statement_description = config.products[0]["statement_description"]
 
 stripe.Plan.create(amount=amount,
                    interval=interval,
                    name=name,
                    currency=currency,
                    id=plan_id,
-                   trial_period_days=trial_period_days)
+                   trial_period_days=trial_period_days,
+                   statement_description=statement_description)
