15b4d9f9590e7a1ce95cbb74639360a72eea1691
zancas <zancas@leastauthority.com>
2014-06-10 16:57:35 -0600

    first draft update testing blog

--- update_testing_blog.py
+++ update_testing_blog.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 """This script:
-(1) updates a testing infrastructure-server.
+(1) updates a testing blog instance.
 """
 
 import os, sys, subprocess
 
-from twisted.internet import defer
-
 from lae_automation.signup import EC2_ENDPOINT
-from lae_automation.server import update_leastauthority_repo
+from lae_automation.server import update_blog
 
 branch_check_command = ['/usr/bin/git', '--git-dir', '../secret_config/.git', 'branch', '--list', 'testing']
 current_branch = subprocess.check_output(branch_check_command).strip()
@@ -16,35 +14,14 @@ if current_branch != "* testing":
     print "The testing branch of the secret_config repo must be checked out to run this script."
     sys.exit(1)
 
-leastauthority_repo_path = "./.git"
-least_repo_HEAD_command = ['/usr/bin/git', '--git-dir', leastauthority_repo_path, 'rev-parse', 'HEAD']
-leastauthority_commit_ref = subprocess.check_output(least_repo_HEAD_command).strip()
-print leastauthority_commit_ref
-
 admin_keypair_name = "ec2sshadmin"
 admin_privkey_path = "../secret_config/ec2sshadmin.pem"
 
-endpoint_uri = EC2_ENDPOINT
-bucket_name = 'dummy'
-website_pubkey = None
-stdout = sys.stdout
-stderr = sys.stderr
+blog_repo_path = "../blog_source/.git"
+blog_repo_HEAD_command = ['/usr/bin/git', '--git-dir', blog_repo_path, 'rev-parse', 'HEAD']
+blog_commit_ref = subprocess.check_output(blog_repo_HEAD_command).strip()
+print blog_commit_ref
 
-def printer(x):
-    print "callBack return value 'x' is %s" % (x,)
-    return x
+update_blog("107.21.225.70", blog_repo_path, blog_commit_ref, admin_privkey_path))
 
-def eb(x):
-    print >> sys.stderr, "Error returned ?"
-    print >> sys.stderr, x
-
-
-d = defer.succeed(update_leastauthority_repo("107.21.225.70", leastauthority_repo_path,
-                                             leastauthority_commit_ref, admin_privkey_path))
-
-d.addCallbacks(printer, eb)
-d.addCallbacks(lambda ign: os._exit(0), lambda ign: os._exit(1))
-from twisted.internet import reactor
-
-reactor.run()
 
