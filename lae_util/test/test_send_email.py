4ccbe2f95914d3c00a8ea80abcae8203917b72d3
zancas <zancas@leastauthority.com>
2014-06-12 17:43:17 -0600

    rm unnecessarily complicated mockery

--- lae_util/test/test_send_email.py
+++ lae_util/test/test_send_email.py
@@ -1,31 +1,15 @@
 
-from mock import MagicMock, patch
 from twisted.trial.unittest import TestCase
 
 from lae_automation.confirmation import CONFIRMATION_EMAIL_SUBJECT
-from lae_util.send_email import compose_plain_email, MIMEText, FROM_EMAIL
+from lae_util.send_email import compose_plain_email, FROM_EMAIL, FROM_ADDRESS
 
 
-class EmailSendingCommonFixture(TestCase):
-    def setUp(self):
-        self.msg_MOCK_patcher = patch('lae_util.send_email.MIMEText', spec=True)
-        self.msg_MOCK_class = self.msg_MOCK_patcher.start()
-        self.msg_MOCK_class.as_string.return_value = 'SOMESTRING'
-
-    def tearDown(self):
-        self.msg_MOCK = self.msg_MOCK_patcher.stop()
-
-
-class PGPLessConfirmation(EmailSendingCommonFixture):
-    def setUp(self):
-        EmailSendingCommonFixture.setUp(self)
-        self.headers_MOCK = { "From": FROM_EMAIL, "Subject": CONFIRMATION_EMAIL_SUBJECT }
-
-    def tearDown(self):
-        super(EmailSendingCommonFixture, self).tearDown()
-
-    def test_compose_email(self):
-        compose_plain_email( MagicMock(name='fromEmail'),
-                             MagicMock(name='toEmail'),
-                             MagicMock(name='content'),
-                             self.headers_MOCK )
+class TestEmailComposition(TestCase):
+    def test_compose_pgpless_email(self):
+        TEST_HEADERS = {"From": FROM_ADDRESS, "Subject": CONFIRMATION_EMAIL_SUBJECT}
+        result = compose_plain_email( FROM_EMAIL,
+                                      "test@test",
+                                      'content',
+                                      TEST_HEADERS )
+        self.failUnlessEqual(result.count("Content-Type"), 1)
