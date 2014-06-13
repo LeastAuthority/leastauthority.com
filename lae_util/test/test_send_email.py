e5e9c865097246c7a15c6fa3fe59efc838b238dc
Daira Hopwood <daira@leastauthority.com>
2014-06-13 17:41:04 +0100

    Add a test for email header composition. refs #263
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

---
+++ lae_util/test/test_send_email.py
@@ -0,0 +1,20 @@
+
+from itertools import takewhile
+
+from twisted.trial.unittest import TestCase
+
+from lae_automation.confirmation import CONFIRMATION_EMAIL_SUBJECT
+from lae_util.send_email import compose_plain_email, FROM_EMAIL, FROM_ADDRESS
+
+
+class TestEmailComposition(TestCase):
+    def test_compose_pgpless_email(self):
+        TEST_HEADERS = {"From": FROM_ADDRESS, "Subject": CONFIRMATION_EMAIL_SUBJECT}
+        result = compose_plain_email(FROM_EMAIL,
+                                     'test@test',
+                                     'content',
+                                     TEST_HEADERS)
+
+        headers = takewhile(bool, result.splitlines())
+        num_of_content_type_headers = sum([int(line.startswith('Content-Type:')) for line in headers])
+        self.failUnlessEqual(num_of_content_type_headers, 1)
