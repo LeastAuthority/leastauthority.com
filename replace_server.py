738fef3f715aec6c5a91bb2223edeee17d7f33f6
zancas <zancas@leastauthority.com>
2014-06-26 18:04:59 -0600

    passes smoke tests

--- replace_server.py
+++ replace_server.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
-import os, sys, simplejson, time, base64
+import os, sys, simplejson
 
 from twisted.internet import defer, reactor
 from twisted.python.filepath import FilePath
 
 from lae_automation.signup import create_log_filepaths, replace_server
-from lae_util.timestamp import format_iso_time
 from lae_util.streams import LoggingStream
 
 
@@ -30,12 +29,14 @@ if __name__ == '__main__':
     signup_log_fp) = create_log_filepaths(stripe_plan_id, stripe_customer_id, stripe_subscription_id)
 
     # The stripe secrets are unchanged.
-    stripesecrets_log_fp.setContent(stripe_secrets)
+    stripesecrets_log_fp.setContent(simplejson.dumps(stripe_secrets))
 
     SSEC2_secretsfile = SSEC2secrets_log_fp.open('a+')
     signup_logfile = signup_log_fp.open('a+')
     signup_stdout = LoggingStream(signup_logfile, '>')
     signup_stderr = LoggingStream(signup_logfile, '')
+    # This is to work around the fact that fabric echoes all commands and output to sys.stdout.
+    # It does have a way to disable that, but not (easily) to redirect it.
     sys.stdout = signup_stderr
 
     # Secrets necessary to provision SSEC2
@@ -47,26 +48,24 @@ if __name__ == '__main__':
     instancesize = sys.argv[4]
     customer_email = sys.argv[5]
 
-    # This is to work around the fact that fabric echoes all commands and output to sys.stdout.
-    # It does have a way to disable that, but not (easily) to redirect it.
-    sys.stdout = stderr
 
     def _close(res):
-        stdout.flush()
-        stderr.flush()
-        secretsfile.close()
-        logfile.close()
+        sys.stdout.flush()
+        sys.stderr.flush()
+        signup_log_fp.open().close()
+        stripesecrets_log_fp.open().close()
+        SSEC2secrets_log_fp.open().close()
         return res
     def _err(f):
-        print >>stderr, str(f)
+        print >>sys.stderr, str(f)
         if hasattr(f.value, 'response'):
-            print >>stderr, f.value.response
-        print >>stdout, "%s: %s" % (f.value.__class__.__name__, f.value)
+            print >>sys.stderr, f.value.response
+        print >>sys.stdout, "%s: %s" % (f.value.__class__.__name__, f.value)
         return f
 
     d = defer.succeed(None)
-    d.addCallback(lambda ign: replace_server(old_ssec2_secrets, amiimageid, instancesize, customer_email, stdout, stderr,
-                                             ssec2_secrets_file, logfilename))
+    d.addCallback(lambda ign: replace_server(old_ssec2_secrets, amiimageid, instancesize, customer_email, signup_stdout, signup_stderr,
+                                             SSEC2_secretsfile, signup_logfile))
     d.addErrback(_err)
     d.addBoth(_close)
     d.addCallbacks(lambda ign: os._exit(0), lambda ign: os._exit(1))
