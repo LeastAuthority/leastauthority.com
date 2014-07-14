f99f4145f6ecc4c1fcff1a2ec5cd79533ee8d86a
zancas <zancas@leastauthority.com>
2014-07-14 13:35:03 -0600

    make arg to tar correct

--- manualPGPsignup_packer.sh
+++ manualPGPsignup_packer.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 targets=`grep -l -r -i $1 ../secrets/S4_consumer_iteration_2_beta1_2014-05-27/`
-
-tar -cjvf ../secrets/manualPGPtarballs/$1_signup_PGP_data.tar.bz2 $targets
+tar -cjvf "../secrets/manualPGPtarballs/$1_signup_PGP_data.tar.bz2" $targets
