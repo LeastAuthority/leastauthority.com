e24bd58904f930b030d9073168fa8fa79c0c4864
zancas <zancas@leastauthority.com>
2014-07-14 13:49:03 -0600

    make string before passing as arg

--- manualPGPsignup_scper.sh
+++ manualPGPsignup_scper.sh
@@ -2,4 +2,4 @@
 
 localdir=~/mf/confirmations/$1
 mkdir $localdir
-scp website@leastauthority.com:/home/website/secrets/manualPGPtarballs/$1_signup_PGP_data.tar.bz2 $localdir/
+scp website@leastauthority.com:"/home/website/secrets/manualPGPtarballs/$1_signup_PGP_data.tar.bz2" "$localdir"/
