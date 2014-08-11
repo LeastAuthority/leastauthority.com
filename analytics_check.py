80434985e6314b3285d24d04d637020290158fd6
Daira Hopwood <daira@leastauthority.com>
2014-08-11 23:49:14 +0100

    Split infrastructurecheck.py into website_check.py and analytics_check.py.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

---
+++ analytics_check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys, os
+
+from twisted.internet import reactor, defer
+
+from lae_automation.monitor import check_infrastructure, monitoring_check
+
+
+lasterrorspath = '../lasterrors_analytics.txt'
+URLS_TO_CHECK = ["https://analytics.leastauthority.com/"]
+
+def checker(stdout, stderr):
+    d = defer.succeed(None)
+    for url in URLS_TO_CHECK:
+        d.addCallback(lambda ign, url=url: check_infrastructure(url, stdout, stderr))
+    return d
+
+d = monitoring_check(checker, lasterrorspath, sys.stdout, sys.stderr)
+d.addCallbacks(lambda ign: os._exit(0), lambda ign: os._exit(1))
+reactor.run()
--- website_check.py
+++ website_check.py
@@ -2,16 +2,19 @@
 
 import sys, os
 
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 
 from lae_automation.monitor import check_infrastructure, monitoring_check
 
 
 lasterrorspath = '../lasterrors_website.txt'
-WEBSITE_URL = "https://leastauthority.com/"
+URLS_TO_CHECK = ["https://leastauthority.com/"]
 
 def checker(stdout, stderr):
-    return check_infrastructure(WEBSITE_URL, stdout, stderr)
+    d = defer.succeed(None)
+    for url in URLS_TO_CHECK:
+        d.addCallback(lambda ign, url=url: check_infrastructure(url, stdout, stderr))
+    return d
 
 d = monitoring_check(checker, lasterrorspath, sys.stdout, sys.stderr)
 d.addCallbacks(lambda ign: os._exit(0), lambda ign: os._exit(1))
