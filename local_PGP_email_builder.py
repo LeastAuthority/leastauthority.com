e69a790eddef77ad4ad3bfeee379ff8595a07b64
zancas <zancas@leastauthority.com>
2014-09-01 13:53:24 -0600

    Remove "split" derived subprocess commands.
    Make LA engineer to email a parameter.  <-- Removes some (but not all hard-coding).

--- local_PGP_email_builder.py
+++ local_PGP_email_builder.py
@@ -8,13 +8,11 @@ from twisted.python.filepath import FilePath
 from lae_automation.confirmation import CONFIRMATION_EMAIL_BODY
 
 def extract_logs_from_tarball(archive_name):
-    extraction_arg_string = 'tar -xjvf %s' % archive_name
-    extraction_arg_list = extraction_arg_string.split()
+    extraction_arg_list = ['tar', '-xjvf', archive_name]
     subprocess.call(extraction_arg_list)
     path_pattern = 'home/website/secrets/S4_consumer_iteration_2_beta1_2014-05-27/*/*'
     for path in glob.glob(path_pattern):
-        cp_arg_string = 'cp %s ./' % path
-        cp_arg_list = cp_arg_string.split()
+        cp_arg_list = ['cp', str(path), './']
         cp_sp = subprocess.call(cp_arg_list)
     return True
 
@@ -26,8 +24,7 @@ def extract_PGP_key(work_dir_path):
     return True
 
 def import_PGP_key(PGP_file_path):
-    gpg_import_call_string = 'gpg --import PGP_pubkey.asc'
-    gpg_import_call_list = gpg_import_call_string.split()
+    gpg_import_call_list = ['gpg', '--import', 'PGP_pubkey.asc']
     import_errput = open('gpg_import_err.txt', 'w')
     sp = subprocess.Popen(gpg_import_call_list, stderr = import_errput)
     sp.wait()
@@ -45,14 +42,18 @@ def create_confirmation_email(introducer_furl):
     email_content = CONFIRMATION_EMAIL_BODY % {'external_introducer_furl':introducer_furl}
     FilePath('confirmation.txt').setContent(email_content)
 
-def encrypt_sign_confirmation(ID):
-    enc_string = 'gpg -seaR %s -R zancas@leastauthority.com -o msg.asc confirmation.txt' % ID
-    enc_list = enc_string.split()
+def encrypt_sign_confirmation(ID, PGP_contact_email):
+    print "Attempting to generate ascii armored cyphertext in 'msg.asc'."
+    enc_list = ['gpg', '-seaR', str(ID), '-R', PGP_contact_email, '-o', 'msg.asc', 'confirmation.txt']
     sp = subprocess.Popen(enc_list)
     sp.wait()
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print "Usage: python $(leastauthority.com_DIR)/local_PGP_email_builder.py email_of_subscriber email_of_LA_PGP_handler"
+        sys.exit(-10)
     signup_email = sys.argv[1]
+    encryption_confirmation_handler_email = sys.argv[2]
     work_dir_path = '/home/arc/mf/confirmations/%s/' % signup_email
     os.chdir(work_dir_path)
     archive_name = '%s_signup_PGP_data.tar.bz2' % signup_email
@@ -61,7 +62,7 @@ if __name__ == '__main__':
             ID = import_PGP_key('PGP_pubkey.asc')
             intro_furl = extract_furl()
             create_confirmation_email(intro_furl)
-            encrypt_sign_confirmation(ID)
+            encrypt_sign_confirmation(ID, encryption_confirmation_handler_email)
     else:
         print "Failed to extract logs"
         sys.exit(-1)
