9e704cf96dcd5e1fd389ba74993d9c32f0269075
zancas <zancas@leastauthority.com>
2014-09-08 17:11:23 -0600

    checkpoint

--- test/test_local_PGP_email_builder.py
+++ test/test_local_PGP_email_builder.py
@@ -1,5 +1,21 @@
+import mock, subprocess, glob
+
 from twisted.trial.unittest import TestCase
 
+import local_PGP_email_builder
+
+
 class CommonTestFixture(TestCase):
-    def test_runstest(self):
-        pass
+    def setUp(self):
+        self.MOCK_SP_CALL = mock.Mock()
+        def call_subprocess_call(call_list):
+            self.MOCK_SP_CALL(call_list)
+        self.patch(local_PGP_email_builder.subprocess, 'call', call_subprocess_call)
+
+        self.GLOB_GLOB_CALL = mock.Mock()
+        def call_glob_glob(call_list):
+            self.MOCK_SP_CALL(call_list)
+        self.patch(local_PGP_email_builder.subprocess, 'call', call_subprocess_call)
+
+    def test_extract_logs_from_tarball(self):
+        local_PGP_email_builder.extract_logs_from_tarball("FOO@SPAM.tar.bz2", "FAKESERVICEID")
