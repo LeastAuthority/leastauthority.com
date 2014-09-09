f1954557de9b77bc72df9aff3d142f64400b488d
zancas <zancas@leastauthority.com>
2014-09-09 13:49:01 -0600

    checkpoint, untested

--- lae_automation/pgp.py
+++ lae_automation/pgp.py
@@ -48,31 +48,3 @@ def encrypt_sign_confirmation(ID, PGP_contact_email):
     enc_list = ['gpg', '-seaR', str(ID), '-R', PGP_contact_email, '-o', 'msg.asc', 'confirmation.txt']
     sp = subprocess.Popen(enc_list)
     sp.wait()
-
-def main():
-    if len(sys.argv) != 4:
-        print "Usage: python $(leastauthority.com_DIR)/local_PGP_email_builder.py service_id email_of_subscriber email_of_LA_PGP_handler"
-        sys.exit(-10)
-    service_id = sys.argv[1]
-    signup_email = sys.argv[2]
-    encryption_confirmation_handler_email = sys.argv[3]
-    work_dir_path = os.getcwd()
-    if (os.path.basename(work_dir_path) != signup_email) or not (os.path.dirname(os.getcwd()).endswith('confirmations')):
-        print "This script must be run in the PGP-signup confirmation-email directory for the relevant service, and signup: "
-        print "e.g. S4_EXAMPLE_SERVICE/confirmations/foo@spam.net/"
-        print "instead it was run in: ' "+os.getcwd()+" '"
-        sys.exit(-9)
-    os.chdir(work_dir_path)
-    archive_name = '%s_signup_PGP_data.tar.bz2' % signup_email
-    if extract_logs_from_tarball(archive_name, service_id):
-        if extract_PGP_key(work_dir_path):
-            ID = import_PGP_key('PGP_pubkey.asc')
-            intro_furl = extract_furl()
-            create_confirmation_email(intro_furl)
-            encrypt_sign_confirmation(ID, encryption_confirmation_handler_email)
-    else:
-        print "Failed to extract logs"
-        sys.exit(-1)
-
-if __name__ == '__main__':
-    main()
