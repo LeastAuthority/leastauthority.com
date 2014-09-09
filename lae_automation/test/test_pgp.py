4b7ff83973331badd69d5520981c44a8609c95fc
zancas <zancas@leastauthority.com>
2014-09-09 16:26:26 -0600

    add test stubs reorder functions

--- lae_automation/test/test_pgp.py
+++ lae_automation/test/test_pgp.py
@@ -1,1 +1,20 @@
 import mock
+
+from twisted.trial.unittest import TestCase
+
+class CommonTestFixture(TestCase):
+
+    def setUp(self):
+        self.STUB = mock.Mock()
+
+class TestExtractors(CommonTestFixture):
+
+    def test_extract_logs_from_tarball(self):
+        pass
+
+    def test_extract_PGP_key(self):
+        pass
+
+    def test_extract_furl(self):
+        pass
+
