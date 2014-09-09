27e020dd9c2dcec19fec5f687555283304ae9af0
zancas <zancas@leastauthority.com>
2014-09-09 16:41:04 -0600

    Add a few more test stubs

--- lae_automation/test/test_pgp.py
+++ lae_automation/test/test_pgp.py
@@ -18,3 +18,15 @@ class TestExtractors(CommonTestFixture):
     def test_extract_furl(self):
         pass
 
+class TestPGPCall(CommonTestFixture):
+
+    def test_import_PGP_key(self):
+        pass
+
+    def test_encrypt_sign_confirmation(self):
+        pass
+
+class TestConfirmationCreator(CommonTestFixture):
+
+    def test_create_confirmation_email(self):
+        pass
