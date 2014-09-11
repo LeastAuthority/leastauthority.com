8531a676dcee40e2ba713b8b61cfde6c02ebed02
zancas <zancas@leastauthority.com>
2014-09-11 16:32:44 -0600

    quote variable input to script

--- manualPGPsignup_packer.sh
+++ manualPGPsignup_packer.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-targets=`grep -l -r -i $1 /home/website/secrets/S4_consumer_iteration_2_beta1_2014-05-27/`
-tar -cjvf "/home/website/secrets/manualPGPtarballs/$1_signup_PGP_data.tar.bz2" `basename $targets`
+targets=`grep -l -r -i "$1" /home/website/secrets/S4_consumer_iteration_2_beta1_2014-05-27/`
+tar -cjvf "/home/website/secrets/manualPGPtarballs/$1_signup_PGP_data.tar.bz2" `basename "$targets"`
