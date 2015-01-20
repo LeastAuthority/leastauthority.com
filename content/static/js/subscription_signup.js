666f0f65c07a787c360e4034a823210f9515ac16
Daira Hopwood <daira@leastauthority.com>
2015-01-20 00:11:05 +0000

    Disable PGP signup support. refs #272
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- content/static/js/subscription_signup.js
+++ content/static/js/subscription_signup.js
@@ -13,7 +13,6 @@ window.creditcardVerifier = (function () {
             // This identifies your website in the createToken call below
             Stripe.setPublishableKey('pk_live_iJU9nYuC9rMkMedhZFdIPATZ');
             $form.submit(creditcardVerifier.formSubmissionHandler);
-            $('#use_pgp').click(creditcardVerifier.use_pgp);
         },
         formSubmissionHandler: function (event) {
             var $form = $('#payment-form');
@@ -23,14 +22,6 @@ window.creditcardVerifier = (function () {
             Stripe.createToken($form, creditcardVerifier.stripeResponseHandler);
             return false;
         },
-        use_pgp: function () {
-            if ($('#use_pgp').prop('checked')) {
-                $('#pgp').show();
-            } else {
-                $('#pgp').hide();
-            }
-            return true;
-        },
         stripeResponseHandler: function(status, response) {
             var $form = $('#payment-form');
 
@@ -39,15 +30,6 @@ window.creditcardVerifier = (function () {
                 $form.find('.payment-errors').text(response.error.message);
                 $form.find('button').prop('disabled', false);
             } else {
-                // Copy from pgp_pubkey_textarea to a hidden pgp_pubkey input field.
-                // We do this because input fields can't be multiline, but textareas can.
-                var pgp_pubkey = '';
-                if ($('#use_pgp').prop('checked')) {
-                    pgp_pubkey = $('#pgp_pubkey_textarea').val();  // see http://api.jquery.com/val/
-                }
-                $form.append($('<input type="hidden" name="pgp_pubkey">').val(pgp_pubkey));
-                $('#use_pgp').prop('disabled', true);
-
                 // token contains id, last4, and card type
                 var token = response.id;
                 // Insert the token into the form so it gets submitted to the server
