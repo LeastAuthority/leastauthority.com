d6c24c6f3fd3203c2b988433490507c3db31f063
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2016-07-26 12:58:55 +0530

    terminate.py: add a function to delete an instance
    Uses txaws APIs to delete an instance, given the instance ID.

--- lae_automation/terminate.py
+++ lae_automation/terminate.py
@@ -1,5 +1,7 @@
-
-#import txaws
+from txaws.ec2.client import EC2Client
+from txaws.credentials import AWSCredentials
+from txaws.service import AWSServiceEndpoint
+from signup import EC2_ENDPOINT
 import simplejson
 
 DEBUG = True
@@ -36,6 +38,15 @@ def find_instance_id_for_signup(signupfp):
 
     raise Exception("instanceid not found")
 
+def delete_ec2_instance(ec2_accesskeyid, ec2_secretkey, instanceid):
+    endpoint_uri = EC2_ENDPOINT
+    ec2creds = AWSCredentials(ec2_accesskeyid, ec2_secretkey)
+    endpoint = AWSServiceEndpoint(uri=endpoint_uri)
+    client = EC2Client(creds=ec2creds, endpoint=endpoint)
+    try:
+        client.terminate_instances(instanceid)
+    except Exception, e:
+        if DEBUG: print str(e)
 
 def terminate_customer_server(email, customerid, secretsdirfp):
     signupfp = find_customer_signup(email, customerid, secretsdirfp)
