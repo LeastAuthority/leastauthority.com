1be63d9b595220c26333c9ce8b5dd46b8da481df
Daira Hopwood <daira@leastauthority.com>
2016-08-02 14:29:49 +0100

    Add WIP for test_delete_ec2_instance.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_automation/test/test_terminate.py
+++ lae_automation/test/test_terminate.py
@@ -3,8 +3,10 @@
 
 from twisted.trial.unittest import TestCase
 from twisted.python.filepath import FilePath
+from twisted.internet import defer
 
 from lae_util.fileutil import make_dirs
+from lae_util.namespace import Namespace
 from lae_automation import terminate
 
 
@@ -53,7 +55,18 @@ class TestTerminate(TestCase):
                              'i-instance')
 
     def test_delete_ec2_instance(self):
-        pass
+        ns = Namespace()
+        ns.called = False
+
+        from lae_automation.initialize import EC2Client
+        def call_terminate_instances(EC2ClientObject, *instanceids):
+            self.failUnlessEqual(instanceids, ['i-instance'])
+            ns.called = True
+            return defer.succeed(None)
+
+        self.patch(EC2Client, 'terminate_instances', call_terminate_instances)
+        #...
+        #self.failUnless(ns.called)
 
     def test_load_ec2_credentials(self):
         pass
