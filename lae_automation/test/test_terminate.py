b8c7d76b9638a3c6079860cb6d32c8c61f141135
Daira Hopwood <daira@leastauthority.com>
2016-08-11 14:59:22 +0100

    Test for delete_ec2_instance.
    Signed-off-by: Daira Hopwood <daira@leastauthority.com>

--- lae_automation/test/test_terminate.py
+++ lae_automation/test/test_terminate.py
@@ -5,9 +5,13 @@ from twisted.trial.unittest import TestCase
 from twisted.python.filepath import FilePath
 from twisted.internet import defer
 
+from txaws.credentials import AWSCredentials
+from txaws.service import AWSServiceEndpoint
+
 from lae_util.fileutil import make_dirs
 from lae_util.namespace import Namespace
 from lae_automation import terminate
+from lae_automation.config import Config
 
 
 class TestTerminate(TestCase):
@@ -57,16 +61,26 @@ class TestTerminate(TestCase):
     def test_delete_ec2_instance(self):
         ns = Namespace()
         ns.called = False
+        expected_instanceid = 'i-instance'
 
-        from lae_automation.initialize import EC2Client
-        def call_terminate_instances(EC2ClientObject, *instanceids):
-            self.failUnlessEqual(instanceids, ['i-instance'])
-            ns.called = True
-            return defer.succeed(None)
+        class FakeEC2Client(object):
+            def __init__(EC2ClientObject, creds, endpoint):
+                self.failUnlessIsInstance(creds, AWSCredentials)
+                self.failUnlessIsInstance(endpoint, AWSServiceEndpoint)
+            def terminate_instances(EC2ClientObject, *instanceids):
+                self.failUnlessEqual(instanceids, (expected_instanceid,))
+                ns.called = True
+                return defer.succeed(None)
 
-        self.patch(EC2Client, 'terminate_instances', call_terminate_instances)
-        #...
-        #self.failUnless(ns.called)
+        self.patch(terminate, 'EC2Client', FakeEC2Client)
+
+        FilePath('secret').setContent("supersecret")
+        FilePath('test_config.json').setContent("""
+{ "products": [], "ssec2_access_key_id": "accesskeyid", "ssec2_secret_path": "secret" }
+""")
+        config = Config('test_config.json')
+        terminate.delete_ec2_instance(config, expected_instanceid)
+        self.failUnless(ns.called)
 
     def test_load_ec2_credentials(self):
         pass
