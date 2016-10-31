a293ea7188cc97b9a40dc9e263ba30f95a9fb239
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-10-31 09:42:18 -0400

    Rename "flappserver" image to just "flapp" for symmetry with web.

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,5 @@
 LEASTAUTHORITY=$(dirname $(dirname $0))
 
 docker build -t leastauthority.com/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
-docker build -t leastauthority.com/flappserver -f "${LEASTAUTHORITY}"/docker/Dockerfile.flappserver "${LEASTAUTHORITY}"
+docker build -t leastauthority.com/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
 docker build -t leastauthority.com/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
