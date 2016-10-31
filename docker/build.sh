2aac9c805c18497e40cf05dd9abac51f847c7256
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-10-31 09:25:19 -0400

    Give the correct path to the Dockerfiles.

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,6 @@
 
 LEASTAUTHORITY=$(dirname $(dirname $0))
 
-docker build -t leastauthority.com/infrastructure -f leastauthority.com/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
-docker build -t leastauthority.com/flappserver -f leastauthority.com/docker/Dockerfile.flappserver "${LEASTAUTHORITY}"
-docker build -t leastauthority.com/web -f leastauthority.com/docker/Dockerfile.web "${LEASTAUTHORITY}"
+docker build -t leastauthority.com/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
+docker build -t leastauthority.com/flappserver -f "${LEASTAUTHORITY}"/docker/Dockerfile.flappserver "${LEASTAUTHORITY}"
+docker build -t leastauthority.com/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
