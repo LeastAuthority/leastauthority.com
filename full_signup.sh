0544f4f224bdd73639b9486d92a0d0ea709f1552
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-11-02 11:43:22 -0400

    Specify good paths for a couple other files.

--- full_signup.sh
+++ full_signup.sh
@@ -5,4 +5,6 @@ export PYTHONPATH=.
 timeout --kill-after=121m 2h \
     ./full_signup.py \
     --log-directory /app/data/secrets/flappserver_logs/ \
-    --secrets-directory /app/data/secrets/
+    --secrets-directory /app/data/secrets/ \
+    --automation-config-path /app/secret_config/lae_automation_config.json \
+    --server-info-path /app/data/serverinfo.csv
