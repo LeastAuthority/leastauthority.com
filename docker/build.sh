ad70e91d3d094064cd73cec00e66e89abca1d4e5
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-11-10 12:31:47 -0500

    Break up the infrastructure base image into a couple smaller pieces; reuse some of it for some tahoe images.
    A first stab, anyway.

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,13 @@
 
 LEASTAUTHORITY=$(dirname $(dirname $0))
 
+docker build -t leastauthority.com/base -f "${LEASTAUTHORITY}"/docker/Dockerfile.base "${LEASTAUTHORITY}"
+
 docker build -t leastauthority.com/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
 docker build -t leastauthority.com/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
 docker build -t leastauthority.com/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
+
+EMPTY="$(mktemp -d)"
+docker build -t leastauthority.com/tahoe-base -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-base "${EMPTY}"
+docker build -t leastauthority.com/tahoe-introducer -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-introducer "${EMPTY}"
+docker build -t leastauthority.com/tahoe-storage -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-storage "${EMPTY}"
