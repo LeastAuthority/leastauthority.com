11b2a87866bffd11cb8e13c64b3cd28d61d3bd7c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-11-15 14:25:12 -0500

    Some versions of Docker don't like to have a '.' there.

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,6 @@
 
 LEASTAUTHORITY=$(dirname $(dirname $0))
 
-docker build -t leastauthority.com/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
-docker build -t leastauthority.com/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
-docker build -t leastauthority.com/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
+docker build -t leastauthority/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
+docker build -t leastauthority/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
+docker build -t leastauthority/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
