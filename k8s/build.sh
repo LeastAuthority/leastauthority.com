df701ac3fd452582438029a73c29235e433a9b47
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-11-16 10:51:37 -0500

    Random fixes.  The script actually works now.

--- k8s/build.sh
+++ k8s/build.sh
@@ -1,43 +1,48 @@
+#!/bin/bash -ex
+
 # K8S_POD tells us which k8s pod in which to build.
 # GIT_BRANCH tells us which leastauthority.com branch to build.
-# DOCKER_TAG tells us what tag to apply to the build images.
 
-[ -v K8S_POD && -v GIT_BRANCH && -v DOCKER_TAG ] || {
-    echo "Set K8S_POD, GIT_BRANCH, and DOCKER_TAG variables."
+[[ -v K8S_POD && -v GIT_BRANCH ]] || {
+    echo "Set K8S_POD and GIT_BRANCH variables."
     exit 1
 }
 
 # A helper to run commands inside a container in the pod.
 EXEC="kubectl exec -i ${K8S_POD} --"
 
+${EXEC} bash -e -x -c '
+    # Dependencies for the build.
+    DEPS="git docker.io python-twisted"
+
+    # If any of them are not installed, install them.
+    # This lets us skip the slightly slow "apt-get update" on subsequent runs.
+    dpkg --status ${DEPS} >/dev/null 2>&1 || apt-get update && apt-get install -y ${DEPS};
+'
+
 # First synchronize the secrets to the container.
-rsync \
-    --delete \
-    --exclude-from .gitignore \
-    --exclude-from .dockerignore \
-    -avzP \
-    --blocking-io \
-    --rsh="${EXEC} -- rsync " \
-    ./secret_config/ \
-    :/root/secret_config/
+TEMP=$(tar c ./secret_config/ | ${EXEC} bash -c 'cd $(mktemp -d); tar x; pwd')
 
 ${EXEC} bash -e -x -c '
-    # Install build dependencies.
-    apt-get update && apt-get install -y git docker.io python-twisted;
+    # Clone the git branch variable to the remote environment.
+    GIT_BRANCH="'"${GIT_BRANCH}"'"
+    WORKDIR="'"${TEMP}"'"
 
-    # Do the build in /root, why not?
-    cd /root;
-
-    # Clean up any old source checkout
-    rm -rf leastauthority.com
+    # Get back to our temporary working directory.
+    pushd "${WORKDIR}"
 
     # Get a new source checkout.
     # Get a shallow one to save time/bandwidth.
     # Get the branch the user requested.
     git clone --depth 1 --branch "${GIT_BRANCH}" http://github.com/leastauthority/leastauthority.com;
 
+    # Find the git revision hash that was just checked out.
+    # Use it as the Docker image tag so that the image can be
+    # unambiguously associated with a revision of the software.
+    DOCKER_TAG=$(git --git-dir leastauthority.com/.git rev-parse HEAD)
+
     # Put the secrets in the place expected by the build.
-    cp -a /root/secret_config leastauthority.com;
+    cp -a secret_config leastauthority.com;
 
     # Build the images.
     ./leastauthority.com/docker/build.sh;
@@ -47,10 +52,20 @@ ${EXEC} bash -e -x -c '
     docker tag leastauthority/web 127.0.0.1:30000/leastauthority/web:"${DOCKER_TAG}"
     docker tag leastauthority/flapp 127.0.0.1:30000/leastauthority/flapp:"${DOCKER_TAG}"
 
+    # Clean up the last portforwarder, if necessary.
+    [ -e /tmp/portforward.pid ] && kill $(cat /tmp/portforward.pid) || /bin/true
+
     # Forward a local port to the private registry so we can push the new images.
-    twistd portforward --port 30000 --host private-registry.leastauthority-tweaks --dest_port 30000
+    twistd \
+        --pidfile /tmp/portforward.pid \
+        portforward \
+            --port 30000 \
+            --host private-registry.leastauthority-tweaks \
+            --dest_port 30000
 
     # And push them.
     docker push 127.0.0.1:30000/leastauthority/web:"${DOCKER_TAG}"
     docker push 127.0.0.1:30000/leastauthority/flapp:"${DOCKER_TAG}"
+
+    echo "Tagged images with ${DOCKER_TAG}."
 '
