e9b33b30f4ac5330e74c699da9c1953b99c30d9c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-11-22 12:28:14 -0500

    Also build the magic wormhole relay image.

--- docker/build.sh
+++ docker/build.sh
@@ -5,3 +5,5 @@ LEASTAUTHORITY=$(dirname $(dirname $0))
 docker build -t leastauthority/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
 docker build -t leastauthority/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
 docker build -t leastauthority/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
+
+docker build -t leastauthority/magicwormhole -f "${LEASTAUTHORITY}"/docker/Dockerfile.magicwormhole-relay "${LEASTAUTHORITY}"/docker
