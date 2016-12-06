f68baa8b558cfccf0b1ac04c83ee57122e7ceae4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-12-06 11:18:58 -0500

    Try configuring the containers with configure-tahoe.

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,7 @@ docker build -t leastauthority/infrastructure -f "${LEASTAUTHORITY}"/docker/Dock
 docker build -t leastauthority/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
 docker build -t leastauthority/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
 
-docker build -t leastauthority/tahoe-base -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-base "${LEASTAUTHORITY}"/docker
+docker build -t leastauthority/tahoe-base -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-base "${LEASTAUTHORITY}"
 docker build -t leastauthority/tahoe-introducer -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-introducer "${LEASTAUTHORITY}"/docker
 docker build -t leastauthority/tahoe-storage -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-storage "${LEASTAUTHORITY}"/docker
 
