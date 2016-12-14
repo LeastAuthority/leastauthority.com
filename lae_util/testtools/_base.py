797909e12781172de2bd1fb92dd0dd8755d57a27
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-12-14 10:52:59 -0500

    Drag make_file in from common so we don't need that whole package as well.

--- lae_util/testtools/_base.py
+++ lae_util/testtools/_base.py
@@ -38,8 +38,27 @@ from twisted.python import log
 from twisted.python.filepath import FilePath
 from twisted.trial import unittest
 
-from ..common import make_file
 from ._flaky import retry_flaky
+
+
+def make_file(path, content='', permissions=None):
+    """
+    Create a file with given content and permissions.
+
+    Don't use this for sensitive content, as the permissions are applied
+    *after* the data is written to the filesystem.
+
+    :param FilePath path: Path to create the file.
+    :param str content: Content to write to the file. If not specified,
+    :param int permissions: Unix file permissions to be passed to ``chmod``.
+
+    :return: ``path``, unmodified.
+    :rtype: :py:class:`twisted.python.filepath.FilePath`
+    """
+    path.setContent(content)
+    if permissions is not None:
+        path.chmod(permissions)
+    return path
 
 
 class _MktempMixin(object):
