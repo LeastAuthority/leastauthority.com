e23bc17296155b3319d490c4efa7cf483a5b66a7
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-12-15 12:17:33 -0500

    Make hypothesis testing with temp directories a little less painful!
    This puts all temporary files created by one test run inside a directory inside the temp directory.
    This avoids creation of zillions of files in /tmp by each test run.

---
+++ lae_automation/test/__init__.py
@@ -0,0 +1,8 @@
+
+# Try to avoid creating a zillion zillion garbage directories right in
+# /tmp.  Instead, make one per run and get the rest of the test suite
+# to use it.
+
+import tempfile
+tempfile.tempdir = tempfile.mkdtemp()
+del tempfile
