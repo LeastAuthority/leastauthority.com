73c2c0d1b905c3f289ca535e572653f2bd7d88dd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-12-15 09:21:37 -0500

    docstring for the new api

--- lae_automation/initialize.py
+++ lae_automation/initialize.py
@@ -187,6 +187,20 @@ def verify_and_store_serverssh_pubkey(ec2accesskeyid, ec2secretkey, endpoint_uri
 
 
 def create_user_bucket(reactor, client, bucketname):
+    """
+    Create an S3 bucket for a user's grid.
+
+    If S3 errors are encountered, retries will be attempted.  If too
+    many errors are encountered, this will give up and return a
+    failure.
+
+    @param reactor: An IReactorTime which can be used to schedule retries.
+    @param client: A txaws.s3.client.S3Client which can be used to create the bucket.
+    @param bucketname: The name of the S3 bucket to create.
+
+    @return: A Deferred that fires when the bucket has been created or
+        fails when too many errors are encountered.
+    """
     action = startAction(
         action_type=u"initialize:create_user_bucket",
         name=bucketname,
