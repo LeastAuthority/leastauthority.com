d30c68ad4d58f0f729a30f142a35b0970edd16ca
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2016-12-25 18:03:41 -0500

    match updated api, but delete this soon

--- lae_automation/test/manual_route53.py
+++ lae_automation/test/manual_route53.py
@@ -1,11 +1,15 @@
 
+from twisted.web.client import Agent
+
 from lae_automation.route53 import get_route53_client
+
 from txaws.service import AWSCredentials, AWSServiceRegion
 
 def main(reactor):
     aws = AWSServiceRegion(AWSCredentials())
-    route53 = get_route53_client(reactor, aws)
-    return route53.list_resource_record_sets(zone_id="Z2T2TSJ409GHZ9", identifier="", maxitems="", name="", type="A")
+    agent = Agent(reactor)
+    route53 = get_route53_client(agent, aws)
+    return route53.list_resource_record_sets(zone_id="Z2T2TSJ409GHZ9")
 
 
 from twisted.internet.task import react
