795b1c155afe3594330963504f91ae7e4b759961
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-02-07 19:23:38 -0500

    update to latest txkube

--- lae_automation/kubeclient.py
+++ lae_automation/kubeclient.py
@@ -1,19 +1,47 @@
 
-from txkube import (
-    ConfigMap,
-
-    network_kubernetes,
-)
+from json import loads, dumps
+from twisted.python.url import URL
+from twisted.web.http import OK
+from twisted.web.client import readBody
 
 import attr
 
+from eliot import start_action
+from eliot.twisted import DeferredContext
 
-def selectors_match(obj, selectors):
-    missing = object()
-    return {
-        key: obj["metadata"]["labels"].get(key, missing)
-        for key in selectors
-    } == selectors
+from txkube import (
+    v1, v1beta1, network_kubernetes, authenticate_with_serviceaccount,
+    iobject_to_raw, iobject_from_raw,
+)
+from txkube._network import (
+    _BytesProducer, check_status, object_location, log_response_object,
+)
+
+
+@attr.s(frozen=True)
+class LabelSelector(object):
+    labels = attr.ib()
+
+    def match(self, obj):
+        missing = object()
+        return {
+            key: obj["metadata"]["labels"].get(key, missing)
+            for key in self.labels
+        } == self.labels
+
+
+
+class NullSelector(object):
+    def match(self, obj):
+        return True
+
+
+
+def select(collection, selector):
+    for obj in collection.items:
+        if selector.match(obj):
+            yield obj
+
 
 
 @attr.s(frozen=True)
@@ -23,41 +51,30 @@ class KubeClient(object):
     @classmethod
     def from_service_account(cls):
         from twisted.internet import reactor
-        kubernetes = network_client(
+        kubernetes = network_kubernetes(
             base_url=URL.fromText(u"https://kubernetes/"),
-            agent=authenticate_with_service_account(reactor),
+            agent=authenticate_with_serviceaccount(reactor),
         )
         client = kubernetes.client()
         return cls(k8s=client)
 
-    def _match(self, obj, selectors):
-        if selectors is None:
-            return True
-        return selectors_match(obj, selectors)
+    def select(self, kind, selector):
+        return self.k8s.list(kind).addCallback(select, selector)
 
-    def _get(self, kind, selectors):
-        d = self.k8s.list(kind)
-        def got_objs(collection):
-            for obj in collection.items:
-                if self._match(obj, selectors):
-                    yield obj
-        d.addCallback(got_objs)
-        return d
+    def get_configmaps(self, selector=NullSelector()):
+        return self.select(v1.ConfigMap, selector)
 
-    def get_configmaps(self, selectors):
-        return self._get(ConfigMap, selectors)
+    def get_services(self, selector=NullSelector()):
+        return self.select(v1.Service, selector)
 
-    def get_services(self, selectors):
-        return self._get(Service, selectors)
+    def get_deployments(self, selector=NullSelector()):
+        return self.select(v1beta1.Deployment, selector)
 
-    def get_deployments(self, selectors):
-        return self._get(Deployment, selectors)
-
-    def destroy(self, obj):
-        return self.k8s.destroy(obj)
+    def delete(self, obj):
+        return self.k8s.delete(obj)
 
     def create(self, obj):
         return self.k8s.create(obj)
 
-    def apply(self, definition):
-        pass
+    def replace(self, obj):
+        return self.k8s.replace(obj)
