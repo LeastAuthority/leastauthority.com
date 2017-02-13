92374664b3406730fb1545b216f1fc4ee66d5504
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-02-13 14:36:51 -0500

    Plugin and Dockerfile for containerizing converger

--- twisted/plugins/lae_dropin.py
+++ twisted/plugins/lae_dropin.py
@@ -7,3 +7,10 @@ s4_subscription_manager = ServiceMaker(
     "A stateful service for recording and exposing S4 subscriptions.",
     "s4-subscription-manager",
 )
+
+s4_subscription_converger = ServiceMaker(
+    "S4 Subscription Converger",
+    "lae_automation.subscription_converger",
+    "A stateless convergence loop for synchronizing Kubernetes configuration with the S4 subscription database.",
+    "s4-subscription-converger",
+)
