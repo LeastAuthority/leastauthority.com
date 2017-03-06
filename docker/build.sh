4669ac1919c8f17a044c36ec4ba2147d7cdc9abd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-03-06 10:56:34 -0500

    An attempt to switch one container over to nix

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,31 @@
 #!/bin/sh -ex
 
-LEASTAUTHORITY=$(dirname $(dirname $0))
+NIX_BUILDER="nix"
+
+SELF=$(realpath $0)
+LEASTAUTHORITY=$(dirname $(dirname ${SELF}))
+PARENT=$(dirname ${LEASTAUTHORITY})
+
+# This will outlast the script.  That's okay.  We're using it as a cache,
+# effectively.  Pretty terrible way to set things up.  Maybe I'll improve it
+# later.
+docker run \
+       --detach \
+       --name "${NIX_BUILDER}" \
+       --restart always \
+       --volume ${PARENT}:/parent \
+       numtide/nix-builder \
+	   sh -c 'ln -s /parent/leastauthority.com /leastauthority.com && sleep 100000000' \
+|| /bin/true
+
+docker exec "${NIX_BUILDER}" nix-build /leastauthority.com/docker/web.nix
+web=$(docker exec "${NIX_BUILDER}" nix-build /leastauthority.com/docker/web.nix)
+docker exec "${NIX_BUILDER}" cat "${web}" | docker load
 
 docker build -t leastauthority/base -f "${LEASTAUTHORITY}"/docker/Dockerfile.base "${LEASTAUTHORITY}"
 
 docker build -t leastauthority/infrastructure -f "${LEASTAUTHORITY}"/docker/Dockerfile.infrastructure "${LEASTAUTHORITY}"
 docker build -t leastauthority/flapp -f "${LEASTAUTHORITY}"/docker/Dockerfile.flapp "${LEASTAUTHORITY}"
-docker build -t leastauthority/web -f "${LEASTAUTHORITY}"/docker/Dockerfile.web "${LEASTAUTHORITY}"
 
 docker build -t leastauthority/tahoe-base -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-base "${LEASTAUTHORITY}"
 docker build -t leastauthority/tahoe-introducer -f "${LEASTAUTHORITY}"/docker/Dockerfile.tahoe-introducer "${LEASTAUTHORITY}"/docker
