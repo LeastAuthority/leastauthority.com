bfc25b8d1dce1e6ca1af11aba950b11dda030c4f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-03-06 13:23:43 -0500

    nope how about this

--- docker/web.nix
+++ docker/web.nix
@@ -30,18 +30,17 @@ let
             key = "privateKey=/app/k8s_secrets/website-key.pem";
             chain = "extraCertChain=/app/k8s_secrets/website-chain.pem";
           in
-            [
-	      "/bin/dash" "-c"
-	      "/bin/python -u /${python.sitePackages}/lae_site/main.py " +
-              "--secure-port=ssl:${port}:${cert}:${key}:${chain} " +
-              "--insecure-port=tcp:8080 " +
-              "--redirect-to-port=\${S4_SERVICE_PORT_HTTPS_SERVER} " +
-              "--signup-furl-path=/app/flapp-data/signup.furl " +
-              "--stripe-secret-api-key-path=/app/k8s_secrets/stripe-private.key " +
-              "--stripe-publishable-api-key-path=/app/k8s_secrets/stripe-publishable.key " +
-              "--site-logs-path=/app/data/logs/sitelogs " +
-              "--interest-path=/app/data/emails.csv " +
-              "--subscriptions-path=/app/data/subscriptions.csv " +
+            concatStringsSep " " [
+	      "/bin/python" "-u" "/${python.sitePackages}/lae_site/main.py"
+              "--secure-port=ssl:${port}:${cert}:${key}:${chain}"
+              "--insecure-port=tcp:8080"
+              "--redirect-to-port=\${S4_SERVICE_PORT_HTTPS_SERVER}"
+              "--signup-furl-path=/app/flapp-data/signup.furl"
+              "--stripe-secret-api-key-path=/app/k8s_secrets/stripe-private.key"
+              "--stripe-publishable-api-key-path=/app/k8s_secrets/stripe-publishable.key"
+              "--site-logs-path=/app/data/logs/sitelogs"
+              "--interest-path=/app/data/emails.csv"
+              "--subscriptions-path=/app/data/subscriptions.csv"
               "--service-confirmed-path=/app/data/service_confirmed.csv"
             ];
         ExposedPorts = {
