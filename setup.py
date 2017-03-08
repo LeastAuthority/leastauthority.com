5dcf4bc0c8047f7a618159c159d5b0d4a01ef0a9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-03-08 10:20:10 -0500

    okay how about this

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     include_package_data=True,
     dependency_links=[
         "https://tahoe-lafs.org/deps/",
-        "https://github.com/LeastAuthority/txkube.git@master#egg=txkube-17.1.0",
+        "git+https://github.com/LeastAuthority/txkube.git@master#egg=txkube-17.1.0",
     ],
     install_requires=[
         "python-dateutil",
