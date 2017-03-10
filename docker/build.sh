52ed962f2ff5593363bf0b2f382f20b50e9bdad3
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-03-10 08:21:40 -0500

    give me a grid build too

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,21 @@
 #!/bin/sh -ex
 
+nixbuild() {
+    nix=$1
+
+    # Build it.
+    docker exec "${NIX_BUILDER}" nix-build /leastauthority.com/docker/"${nix}"
+
+    # Get the name of the built artifact.
+    artifact=$(docker exec "${NIX_BUILDER}" nix-build /leastauthority.com/docker/"${nix}")
+
+    # Get the built artifact out of the container and load it in to Docker.
+    docker exec "${NIX_BUILDER}" cat "${artifact}" | docker load
+};
+
 NIX_BUILDER="nix"
 
-LEASTAUTHORITY=$(dirname $(dirname $0))
+LEASTAUTHORITY=${PWD}/$(dirname $(dirname $0))
 PARENT=${LEASTAUTHORITY}/..
 
 CACHE_PATH=/nix-cache
@@ -11,41 +24,27 @@ CACHE_VOLUME=nix-cache
 # A cache for build artifacts.  If we created it on a previous run, great.
 docker volume create --name "${CACHE_VOLUME}" || /bin/true
 
-# Get a Nix toolchain environment we can use for the next few steps.  Allow
-# this to fail and suppose that the reason for failure is that it is already
-# running.
-docker run \
+# Get a Nix toolchain environment we can use for the next few steps.
+if docker run \
        --detach \
        --name "${NIX_BUILDER}" \
        --volume "${CACHE_VOLUME}":"${CACHE_PATH}" \
+       --volume "${LEASTAUTHORITY}":/leastauthority.com \
        numtide/nix-builder \
-	   sleep 100000000 \
-|| /bin/true
+	   sleep 100000000; then
 
-# To facilitate leaving the nix builder running across multiple builds, copy
-# the source into the container like this instead of doing something with a
-# Docker volume.
-docker exec "${NIX_BUILDER}" rm -rf /leastauthority.com
-tar cf - --directory "${PARENT}" "./leastauthority.com" | \
-    docker exec -i "${NIX_BUILDER}" tar xvf -
+    # As a horrible hack, copy everything from the cache volume into the Nix
+    # store.  The cache volume is separate from the Nix store because the Nix
+    # store has some initial state from the container that it would be hard to
+    # initialize a volume with.
+    docker exec "${NIX_BUILDER}" sh -c "cp -r ${CACHE_PATH}/* /nix/ || /bin/true"
+fi
 
-# As a horrible hack, copy everything from the cache volume into the Nix
-# store.  The cache volume is separate from the Nix store because the Nix
-# store has some initial state from the container that it would be hard to
-# initialize a volume with.
-docker exec "${NIX_BUILDER}" sh -c "cp -r ${CACHE_PATH}/* /nix/ || /bin/true"
-
-# Build it.
-docker exec "${NIX_BUILDER}" nix-build /leastauthority.com/docker/web.nix
-
-# Get the name of the built artifact.
-web=$(docker exec "${NIX_BUILDER}" nix-build /leastauthority.com/docker/web.nix)
+nixbuild "web.nix"
+nixbuild "grid-router.nix"
 
 # Copy everything back to the cache volume so it's available for next time.
 docker exec "${NIX_BUILDER}" sh -c "cp -r /nix/* ${CACHE_PATH}/"
-
-# Get the built artifact out of the container and load it in to Docker.
-docker exec "${NIX_BUILDER}" cat "${web}" | docker load
 
 # Clean up the Nix build container.
 docker rm -f "${NIX_BUILDER}"
---
+++ docker/grid-router.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  leastauthority = pkgs.callPackage ./lae.nix {
+    pkgs = pkgs;
+    pythonPackages = pkgs.python27Packages;
+  };
+
+  grid-router = { lae pkgs }:
+    pkgs.dockerTools.buildImage {
+      name = "leastauthority/grid-router";
+      runAsRoot = ''
+        #!${pkgs.stdenv.shell}
+        ${pkgs.dockerTools.shadowSetup}
+        groupadd --system router
+        useradd --system --gid router --home-dir /app/data router
+      '';
+      contents = python.buildEnv.override {
+        extraLibs = [ pkgs.dash lae ];
+	ignoreCollisions = true;
+	postBuild = "\${out}/bin/twistd --help > /dev/null";
+      };
+      config = {
+        Cmd = [
+	  "/bin/dash"
+	  "-c"
+	  (pkgs.lib.strings.concatStringSep " " [
+	    "/bin/twist" "s4-grid-router" "--kubernetes-namespace"
+	    "$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
+	  ])
+	];
+      };
+    };
+in
+  grid-router {
+    lae = leastauthority.lae;
+    pkgs = pkgs;
+  }
