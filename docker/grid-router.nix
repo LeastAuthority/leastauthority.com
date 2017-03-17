67db4a285ab1e11ec091058162d37573a0fc0f48
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-03-17 08:53:06 -0400

    Refactor nix packages for less image rebuilding
    Now the image layers are shared between web and grid-router.
    Only the container configuration is distinct.

--- docker/grid-router.nix
+++ docker/grid-router.nix
@@ -1,39 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, lae ? import ./lae.nix {} }:
 
-let
-  leastauthority = pkgs.callPackage ./lae.nix {
-    pkgs = pkgs;
-    pythonPackages = pkgs.python27Packages;
+pkgs.dockerTools.buildImage {
+  name = "leastauthority/grid-router";
+  fromImage = lae.s4-common-image;
+  config = {
+    Cmd = [
+      "/bin/dash"
+      "-c"
+      (pkgs.lib.strings.concatStringsSep " " [
+        "/bin/twist" "s4-grid-router" "--kubernetes-namespace"
+        "$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
+      ])
+    ];
   };
-
-  grid-router = { python, lae, pkgs }:
-    pkgs.dockerTools.buildImage {
-      name = "leastauthority/grid-router";
-      runAsRoot = ''
-        #!${pkgs.stdenv.shell}
-        ${pkgs.dockerTools.shadowSetup}
-        groupadd --system router
-        useradd --system --gid router --home-dir /app/data router
-      '';
-      contents = python.buildEnv.override {
-        extraLibs = [ pkgs.dash pkgs.coreutils lae ];
-	ignoreCollisions = true;
-	postBuild = "\${out}/bin/twistd --help > /dev/null";
-      };
-      config = {
-        Cmd = [
-	  "/bin/dash"
-	  "-c"
-	  (pkgs.lib.strings.concatStringsSep " " [
-	    "/bin/twist" "s4-grid-router" "--kubernetes-namespace"
-	    "$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)"
-	  ])
-	];
-      };
-    };
-in
-  grid-router {
-    python = pkgs.python27;
-    lae = leastauthority.lae;
-    pkgs = pkgs;
-  }
+}
