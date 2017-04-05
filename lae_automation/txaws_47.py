cc575b838bbac57edb539e4fca1aec002f9e6f1a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-04-05 08:49:29 -0400

    Get a fix for the txAWS certificate problem.
    txAWS code can't find certs in the nix-based environment.
    Convince it to use Twisted's better code instead.

---
+++ lae_automation/txaws_47.py
@@ -0,0 +1,18 @@
+"""
+Hotfix for https://github.com/twisted/txaws/issues/47
+"""
+
+from twisted.web.client import BrowserLikePolicyForHTTPS
+
+from txaws.client import base
+
+def patch():
+    try:
+        base.WebVerifyingContextFactory
+    except AttributeError:
+        # Don't do anything if that thing has gone away.
+        pass
+    else:
+        # Replace that thing with another thing that makes a better thing!
+        # See the ticket for details.
+        base.WebVerifyingContextFactory = lambda host: BrowserLikePolicyForHTTPS()
