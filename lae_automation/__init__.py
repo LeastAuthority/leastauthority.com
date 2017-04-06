094a9dd939194ce13556ee96311ad058d51e0140
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-04-06 08:42:19 -0400

    That doesn't help and maybe there's a simpler way

--- lae_automation/__init__.py
+++ lae_automation/__init__.py
@@ -4,9 +4,3 @@
 """
 Automation (isn't all software?) relating to the S4 service.
 """
-
-# Do some hot-fixing here so that nothing in lae_automation has a chance to
-# run without the fixes.
-
-from .txaws_47 import patch
-patch()
