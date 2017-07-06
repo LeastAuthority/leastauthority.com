5346a9c1b63bfe1c7b5a4b03aa37a95ba7ecd753
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-07-06 10:31:16 -0400

    Turn off all the travis-ci nix-based image building stuff
    dockerhub will build the images

--- docker/build.sh
+++ docker/build.sh
@@ -1,42 +1,8 @@
 #!/bin/sh -ex
 
-# Options:
-#
-#   --load      Load the Nix-built Docker images into the local Docker daemon.
-#   --verbose   Dump output from the Nix build process
-
-
 LEASTAUTHORITY=${PWD}/$(dirname $(dirname $0))
 
-NIX_STORE=/nix
-CACHE_VOLUME=nix-cache-implicit-init
-
-# Allow this option to be overridden in environments which have a version of
-# tar too old to support it.  bash -v test is nice but sh only has -z.  Note
-# quotes are not required here because the expansion can only result in "" or
-# "x".
-if [ -z ${EXCLUDE_VCS_IGNORES+x} ]; then
-    EXCLUDE_VCS_IGNORES="--exclude-vcs-ignores"
-fi
-
-build_command="
-mkdir /leastauthority.com
-tar xf - -C /leastauthority.com
-nix-channel --remove nixpkgs
-nix-channel --add https://nixos.org/channels/nixos-17.03 nixpkgs
-nix-channel --update
-/leastauthority.com/docker/_nix-build $@ s4-common-image
-"
-
-# Get a Nix toolchain environment we can use for the next few steps.
-tar cf - -C "${LEASTAUTHORITY}" --exclude-vcs ${EXCLUDE_VCS_IGNORES} . | docker run \
-       --rm \
-       --interactive \
-       --volume ${CACHE_VOLUME}:${NIX_STORE} \
-       --volume /var/run/docker.sock:/var/run/docker.sock \
-       numtide/nix-builder /bin/env /bin/sh -exc "${build_command}"
-
-images="base tahoe-base foolscap-base foolscap-gatherer"
+images="base s4-common-image tahoe-base foolscap-base foolscap-gatherer"
 tahoe_images="tahoe-introducer tahoe-storage"
 
 for image in ${images}; do
