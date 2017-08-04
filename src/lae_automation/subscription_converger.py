2fcb6a937a3066279322679af6720f517af384e0
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-08-04 15:45:13 -0400

    Add a metric for the health of the convergence loop.

--- src/lae_automation/subscription_converger.py
+++ src/lae_automation/subscription_converger.py
@@ -201,6 +201,7 @@ def makeService(options):
             options,
             subscription_client,
             stripe_client,
+            reactor,
         )
         return d
 
@@ -212,8 +213,15 @@ def makeService(options):
 
 
 
+_CONVERGE_COMPLETE = Gauge(
+    u"s4_last_convergence_succeeded",
+    u"The time at which the convergence loop last succeeded.",
+)
+
+
+
 def _finish_convergence_service(
-    k8s_client, options, subscription_client, stripe_client
+    k8s_client, options, subscription_client, stripe_client, reactor,
 ):
     k8s = KubeClient(k8s=k8s_client)
 
@@ -249,9 +257,21 @@ def _finish_convergence_service(
         stats_gatherer_furl=options["stats-gatherer-furl"],
     )
 
+    def monitorable_converge(*a, **kw):
+        d = converge(*a, **kw)
+        def finished(passthrough):
+            _CONVERGE_COMPLETE.set(reactor.seconds())
+            return passthrough
+        d.addCallback(finished)
+        return d
+
+    safe_converge = divert_errors_to_log(
+        monitorable_converge, u"subscription_converger",
+    )
+
     return TimerService(
         options["interval"],
-        divert_errors_to_log(converge, u"subscription_converger"),
+        safe_converge,
         config,
         subscription_client,
         k8s,
