1dbf2e4ab02ec8629dd2de129c46c68606e771f4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-01-17 13:00:52 -0500

    perhaps it belongs here

--- src/lae_automation/containers.py
+++ src/lae_automation/containers.py
@@ -246,10 +246,9 @@ def _deployment_template(model):
                                 u"httpGet": {
                                     u"path": u"/",
                                     u"port": 9001,
-                                    u"initialDelaySeconds": 5,
-                                    u"periodSeconds": 90,
-
                                 },
+                                u"initialDelaySeconds": 5,
+                                u"periodSeconds": 90,
                             },
 
                             # https://kubernetes.io/docs/concepts/configuration/manage-compute-resources-container/
