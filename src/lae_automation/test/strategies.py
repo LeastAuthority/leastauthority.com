8c4341bfc6bb49a2c138266871c44211b6d0a0bf
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-02-05 09:58:03 -0500

    Guide domain name generation towards shorter names
    Longer names may still be generated but average length of generated
    names will now be much lower (and closer to realistic).

--- src/lae_automation/test/strategies.py
+++ src/lae_automation/test/strategies.py
@@ -177,7 +177,7 @@ def domains():
 
     return strategies.builds(
         build_domain,
-        complete=idna_text(min_size=1, max_size=255),
+        complete=idna_text(min_size=1, average_size=12, max_size=255),
         random=strategies.randoms(),
     ).map(
         lambda parts: u".".join(parts)
