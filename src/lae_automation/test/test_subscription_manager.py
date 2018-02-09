f91801013baebc204970ea0015b6e0440fdc3f04
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-02-09 08:33:39 -0500

    Go back to regular FilePath
    I love the idea of MemoryPath but that's not where the performance
    issues with this code are.  Let's not introduce an untested fake (right
    now) if there isn't even any practical benefit.

--- src/lae_automation/test/test_subscription_manager.py
+++ src/lae_automation/test/test_subscription_manager.py
@@ -32,7 +32,6 @@ from lae_util.testtools import TestCase
 
 from .strategies import subscription_id, subscription_details
 from .matchers import AttrsEquals, GoodEquals
-from .memorypath import MemoryPath
 
 
 def partial_subscription_details():
@@ -223,9 +222,7 @@ class SubscriptionManagerTestMixin(object):
 
 class SubscriptionManagerTests(SubscriptionManagerTestMixin, TestCase):
     def get_client(self):
-        root = MemoryPath(FilePath(u"/somewhere"))
-        root.makedirs()
-        return self._get_client_for_path(root)
+        return self._get_client_for_path(FilePath(mkdtemp().decode("utf-8")))
 
 
     def _get_client_for_path(self, path):
